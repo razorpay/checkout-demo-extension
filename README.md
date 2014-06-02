7d67e2e61a30a4d78e8a1ee7292fc361bd9a8300
Abhay Rana <capt.n3m0@gmail.com>
2014-06-02 13:16:30 +0530

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+Setup instructions
+
+1. Copy layout.html to index.html
+2. Replace value in line 9 of index.html with provided merchant key
+3. Optionally replace api.razorpay.com with your local rzp.api.dev URL in case you want to test it locally.
+4. Open index.html in your browser over http(s), not file://
---
+++ checkout.js
@@ -0,0 +1,96 @@
+//Start by creating a new button to press
+var button = document.createElement('button');
+
+var RazorPayScript = document.currentScript || (function() {
+    var scripts = document.getElementsByTagName('script');
+    return scripts[scripts.length - 1];
+})();
+
+var RazorPayForm = $(RazorPayScript).parent();
+
+button.onclick = createLightBox('./template.html');
+
+
+
+$.fn.center = function () {
+   this.css("position","absolute");
+   this.css("top", ( $(window).height() - this.height() ) / 2  + "px");
+   this.css("left", ( $(window).width() - this.width() ) / 2 + "px");
+   this.css('margin-left','auto');
+   this.css('margin-top','auto');
+   return this;
+}
+
+function createLightBox(template_url){
+    //Make an ajax request to template_url, fetch the template
+    //replace the contents
+
+    //Create the overlay
+    $('<div class="ow-overlay ow-closed"></div> ').appendTo("body");
+
+    
+
+    $.get(template_url, function(template){
+        var html = $.tmpl(template, $(RazorPayScript).data());
+        html.appendTo('body');
+        
+        $modal = $('div.modal').omniWindow()
+        $modal.trigger('show');
+
+
+        //Attach some evens to the modal
+
+        $('.close-button').click(function(e){
+            e.preventDefault();
+            $modal.trigger('hide');
+        });
+
+        $('form.body').submit(formsubmit);
+    });
+}
+
+function formsubmit(e){
+    var merchant_key = $(this).find('input[name="key"]').val();
+    var data = $(this).serialize();
+    $.getJSON('https://'+merchant_key+'@api.razorpay.com/transactions/jsonp?callback=?', data, function(response){
+        if(response.callbackUrl){
+            $('div.modal').html('<iframe></iframe>');
+
+            var autosubmit_form_template = '<!doctype html> \
+                <html> \
+                    <head>\
+                    </head>\
+                    <body>\
+                        <form method="POST" action = "${data.url}" id="rzp-dcform">\
+                            <input type="hidden" name="PaReq" value="${data.PAReq}">\
+                            <input type="hidden" name="MD" value="${data.paymentid}">\
+                            <input type="hidden" name="TermUrl" value="${callbackUrl}">\
+                            <input type="submit" value="Submit">\
+                        </form>\
+                        <script>\
+                            var form = document.getElementById(\'rzp-dcform\');\
+                            form.submit();\
+                        </script>\
+                    </body>\
+                </html>'
+            var div = document.createElement('div');
+            $.tmpl(autosubmit_form_template, response).appendTo(div);
+            $('div.modal iframe').get()[0].contentWindow.document.write(div.innerHTML);
+            //This form should autosubmit
+            //Now we need to resize the modal box so as to accomodate 3dsecure.
+            $('div.modal, div.modal iframe').width('800px').height('500px');
+            $('div.modal').center();
+            XD.receiveMessage(successCall);
+        }else{
+            successCall(data);
+        }
+    });
+    e.preventDefault();
+}
+
+function successCall(data){
+    $(RazorPayForm).html(
+        '<input type="hidden" name="transaction_id" value="'+data.data.id+'">'
+    );
+    $(RazorPayForm).submit();
+}
