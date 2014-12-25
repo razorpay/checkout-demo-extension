78bc9bb22113bcff3a3a287d7cb9a4259bb37d4b
Shashank Mehta <me@shashankmehta.in>
2014-12-25 23:52:50 +0530

    Adds jquery plugin sendkeys for autofill and test

--- app/assets/js/autofill.js
+++ app/assets/js/autofill.js
@@ -4,9 +4,9 @@
   (function(){
     window.setTimeout(function(){
       var $form = $('.rzp-body');
-      $form.find('input[name="card[number]"]').val('4012001037141112');//Debit Card with 3dsecure
-      $form.find('input[name="card[expiry]"]').val('05 / 19');
-      $form.find('input[name="card[cvv]"]').val('888');
+      $form.find('input[name="card[number]"]').sendkeys('4012001037141112');//Debit Card with 3dsecure
+      $form.find('input[name="card[expiry]"]').sendkeys('05 / 19');
+      $form.find('input[name="card[cvv]"]').sendkeys('888');
     },1000);
   })();
 })();
