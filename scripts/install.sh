d743da6b77c6d9a8660cabc5f140ca3a1c1a5d61
Harshil Mathur <harshilmathur@gmail.com>
2015-01-11 14:40:39 -0800

    Fixes insall script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Deployment Script
 
+SCRIPTPATH=$(dirname "$SCRIPT")
+
 # Install new version
-rsync -avz --force --delete --progress ./ /home/ubuntu/checkout/
+rsync -avz --force --delete --progress $SCRIPTPATH/../app/dist/ /home/ubuntu/checkout/app/dist/
+
+# Fix permissions
+cd /home/ubuntu/checkout/app/dist/ && sudo chmod 775 -R ./
