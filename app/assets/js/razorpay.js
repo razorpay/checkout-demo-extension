e6c0326ef308d807aed95b56d0516579bc1d502d
Shashank Mehta <me@shashankmehta.in>
2015-01-30 16:42:58 +0530

    Fixes handling of rollbar in built js and for tests

--- app/assets/js/razorpay.js
+++ app/assets/js/razorpay.js
@@ -307,7 +307,7 @@
     // }
   };
 
-  discreet.Rollbar = {
+  Razorpay.prototype.Rollbar = {
     state: false,
 
     _check: function(){
@@ -320,18 +320,18 @@
     },
 
     start: function(){
-      if(discreet.Rollbar._check() === false){
+      if(this._check() === false){
         return;
       }
-      discreet.Rollbar.state = true;
+      this.state = true;
       Rollbar.configure({enabled: true});
     },
 
     stop: function(){
-      if(discreet.Rollbar._check() === false){
+      if(this._check() === false){
         return;
       }
-      discreet.Rollbar.state = false;
+      this.state = false;
       Rollbar.configure({enabled: false})
     }
   }
