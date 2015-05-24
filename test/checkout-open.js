eb4fcc6e766a9f72a1115ca7d9d7bd679c09e859
Shashank Mehta <me@shashankmehta.in>
2015-05-24 18:24:57 +0530

    Fixes tabbing to 2 spaces in test file

--- test/checkout-open.js
+++ test/checkout-open.js
@@ -1,30 +1,30 @@
 var options = {
-	'key': 'key_id',
-	'amount': '5100',
+  'key': 'key_id',
+  'amount': '5100',
   'name': 'Merchant Name',
-	'netbanking': 'true',
-	'prefill': {
-	  'name': 'Shashank Mehta',
-	  'email': 'sm@razorpay.com',
-	  'contact': '9999999999'
-	}
+  'netbanking': 'true',
+  'prefill': {
+    'name': 'Shashank Mehta',
+    'email': 'sm@razorpay.com',
+    'contact': '9999999999'
+  }
 }
 
 var cc = {
-	number: '4012001037141112',
-	expiry: '05 / 19',
-	cvv: '888'
+  number: '4012001037141112',
+  expiry: '05 / 19',
+  cvv: '888'
 }
 
 describe("Checkout should handle frame message", function(){
-	var co, co_opts;
+  var co, co_opts;
 //   var co;
 //   var custom = $.extend(true, {}, coData.options);
 //   custom.unwanted = 'fake';
 
-	beforeEach(function(){
-		co_opts = $.extend(true, {}, options);
-	});
+  beforeEach(function(){
+    co_opts = $.extend(true, {}, options);
+  });
 })
 
 /**
@@ -34,35 +34,35 @@ describe("Checkout should handle frame message", function(){
 describe("checkout validate", function(){
   var init_options, errors, field;
 
-	describe("should throw error if", function(){
-		beforeEach(function(){
-		   init_options = jQuery.extend(true, {}, options);
-		});
+  describe("should throw error if", function(){
+    beforeEach(function(){
+       init_options = jQuery.extend(true, {}, options);
+    });
 
-		afterEach(function(){
+    afterEach(function(){
       expect(errors.length).toBe(1);
       expect(errors[0].field).toBe(field);
-		});
+    });
 
-		it("display_currency is present and not USD", function(){
-			init_options.display_currency = 'YEN';
-		  field = 'display_currency';
-		  errors = Razorpay.prototype.validateOptions(init_options, false);
-		})
+    it("display_currency is present and not USD", function(){
+      init_options.display_currency = 'YEN';
+      field = 'display_currency';
+      errors = Razorpay.prototype.validateOptions(init_options, false);
+    })
 
-		it("display_currency is USD and  display_amount is not there", function(){
-		  field = 'display_amount';
-			init_options.display_currency = 'USD';
-		  errors = Razorpay.prototype.validateOptions(init_options, false);
-		})
+    it("display_currency is USD and  display_amount is not there", function(){
+      field = 'display_amount';
+      init_options.display_currency = 'USD';
+      errors = Razorpay.prototype.validateOptions(init_options, false);
+    })
 
-		it("display_currency is USD and display_amount is invalid", function(){
-		  field = 'display_amount';
-			init_options.display_currency = 'USD';
-			init_options.display_amount = 'swag';
-		  errors = Razorpay.prototype.validateOptions(init_options, false);
-		})
-	})
+    it("display_currency is USD and display_amount is invalid", function(){
+      field = 'display_amount';
+      init_options.display_currency = 'USD';
+      init_options.display_amount = 'swag';
+      errors = Razorpay.prototype.validateOptions(init_options, false);
+    })
+  })
 })
 
 
