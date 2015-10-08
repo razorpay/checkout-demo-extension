238a2f8f5b52e2096959e8ff2ec11c6193c348b1
Pranav Gupta <pranavguptarulz@gmail.com>
2015-10-08 21:11:19 +0530

    less->css conversion with autoprefixer and concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 // Include gulp
+var fs = require('fs');
 var gulp = require('gulp');
 var dot = require('dot');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
 
 function assetPath(path){
-  return 'app/assets/' + path;
+  return 'app/src/' + path;
 }
 
-gulp.task('watch', function() {
+gulp.task('watch', ['buildDev'], function() {
   gulp.watch(assetPath('templates/*.dot'), ['compileTemplates']);
-  gulp.watch(assetPath('css/*.styl'), ['compileStyles']);
+  gulp.watch(assetPath('css/*.less'), ['compileStyles']);
 });
 
 gulp.task('compileTemplates', function(){
@@ -20,5 +24,19 @@ gulp.task('compileTemplates', function(){
 });
 
 gulp.task('compileStyles', function(){
-  
+  gulp.src(assetPath('css/*.less'))
+    .pipe(less())
+    .pipe(concat('checkout.css'))
+    .pipe(autoprefixer({browsers: ['last 10 versions']}))
+    .pipe(gulp.dest(assetPath('_css')));
 });
+
+gulp.task('dirStructure', function(){
+  ['_css', '_templates'].forEach(function(path){
+    if(!fs.existsSync(assetPath(path))){
+      fs.mkdirSync(assetPath(path));
+    }
+  })
+})
+
+gulp.task('buildDev', ['dirStructure', 'compileTemplates', 'compileStyles']);
