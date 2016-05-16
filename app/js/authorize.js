8b70e32c36ea4f31a6d3e267d296ed3ad3ea4d85
Pranav Gupta <pranavguptarulz@gmail.com>
2016-05-16 13:34:14 +0530

    removed basicauth from cancel request

--- app/js/authorize.js
+++ app/js/authorize.js
@@ -284,10 +284,7 @@ Request.prototype = {
       var payment_id = this.payment_id;
       if(payment_id){
         $.ajax({
-          url: discreet.makeUrl() + 'payments/' + payment_id + '/cancel',
-          headers: {
-            Authorization: 'Basic ' + _btoa(this.get('key') + ':')
-          }
+          url: discreet.makeUrl() + 'payments/' + payment_id + '/cancel?key_id=' + this.get('key'),
         })
       }
       this.complete(errorObj || discreet.defaultError());
