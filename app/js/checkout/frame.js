104db826f70a7e99461a8389ec56701b4d0dde61
Pranav Gupta <pranavguptarulz@gmail.com>
2016-05-25 15:59:20 +0530

    wrapped checkout loader code in try/catch

--- app/js/checkout/frame.js
+++ app/js/checkout/frame.js
@@ -182,15 +182,17 @@ function setTestRibbonInvisible(){
 var loader;
 function appendLoader($parent, parent){
   if(!loader){
-    loader = document.createElement('div');
-    var style = "margin:-25px 0 0 -25px;height:50px;width:50px;animation:rzp-rot 1s infinite linear;-webkit-animation:rzp-rot 1s infinite linear;border: 1px solid rgba(255, 255, 255, 0.2);border-top-color: rgba(255, 255, 255, 0.7);border-radius: 50%;"
-    if(parent){
-      style += 'margin: 100px auto -150px;border: 1px solid rgba(0, 0, 0, 0.2);border-top-color: rgba(0, 0, 0, 0.7);';
-    } else {
-      style += 'position:absolute;left:50%;top:50%;';
-    }
-    loader.setAttribute('style', style);
-    $parent.append(loader);
+    try{
+      loader = document.createElement('div');
+      var style = "margin:-25px 0 0 -25px;height:50px;width:50px;animation:rzp-rot 1s infinite linear;-webkit-animation:rzp-rot 1s infinite linear;border: 1px solid rgba(255, 255, 255, 0.2);border-top-color: rgba(255, 255, 255, 0.7);border-radius: 50%;"
+      if(parent){
+        style += 'margin: 100px auto -150px;border: 1px solid rgba(0, 0, 0, 0.2);border-top-color: rgba(0, 0, 0, 0.7);';
+      } else {
+        style += 'position:absolute;left:50%;top:50%;';
+      }
+      loader.setAttribute('style', style);
+      $parent.append(loader);
+    } catch(e){}
   }
 }
 
