2450a2e3f0590b13a6678f3b99fbc8499bcf2495
Pranav Gupta <pronav@users.noreply.github.com>
2016-06-17 00:52:33 +0530

    Ensured api url ends with slash

--- app/js/checkout-frame/init.js
+++ app/js/checkout-frame/init.js
@@ -4,5 +4,9 @@ discreet.isFrame = true;
 (function(){
   var a = document.createElement('a');
   a.href = RazorpayConfig.frameApi;
-  RazorpayConfig.api = a.href;
+  var href = a.href;
+  if (href.slice(-1) !== '/') {
+    href += '/';
+  }
+  RazorpayConfig.api = href;
 })();
