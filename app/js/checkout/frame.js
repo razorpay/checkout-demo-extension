3e4cde5aa0355a0fa9b940745aa53c1442317a3e
Pranav Gupta <pranavguptarulz@gmail.com>
2016-07-07 22:00:50 +0530

    stylint fixes

--- app/js/checkout/frame.js
+++ app/js/checkout/frame.js
@@ -157,6 +157,7 @@ function appendLoader($parent, parent){
   if(!loader){
     try{
       loader = document.createElement('div');
+      loader.className = 'razorpay-loader';
       var style = "margin:-25px 0 0 -25px;height:50px;width:50px;animation:rzp-rot 1s infinite linear;-webkit-animation:rzp-rot 1s infinite linear;border: 1px solid rgba(255, 255, 255, 0.2);border-top-color: rgba(255, 255, 255, 0.7);border-radius: 50%;"
       if(parent){
         style += 'margin: 100px auto -150px;border: 1px solid rgba(0, 0, 0, 0.2);border-top-color: rgba(0, 0, 0, 0.7);';
--- stylelint.config.js
+++ stylelint.config.js
@@ -3,6 +3,7 @@ module.exports = {
   rules: {
     // override anyrules if required. Tag the team for review before whitelisting any extra rule (as this affects the style guide we follow)
     "no-missing-eof-newline": null,
-    "color-hex-case": null
+    "color-hex-case": null,
+    "at-rule-no-unknown": null
   }
 };
