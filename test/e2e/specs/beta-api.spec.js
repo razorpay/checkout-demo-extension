b00cd4b08f143924fcb8184c2bfb020e53de4836
selvagsz <selvaganeshbsg@gmail.com>
2016-12-04 00:53:39 +0530

    Assert whether the bank page has loaded

--- test/e2e/specs/beta-api.spec.js
+++ test/e2e/specs/beta-api.spec.js
@@ -2,16 +2,27 @@ const utils = require('../helpers/utils')
 const CheckoutForm = require('../pageobjects/checkout-form')
 const checkoutForm = new CheckoutForm()
 
-beforeEach(() => {
-  checkoutForm.loadFrame()
-})
-
 function payWithCard(card) {
   checkoutForm.fillCommonFields()
   browser.click('#payment-options > [tab=card]')
   checkoutForm.fillCardFields(card)
   browser.click('#footer')
 }
+
+function switchToCheckout() {
+  let tabIds = browser.getTabIds()
+  browser.switchTab(tabIds[0])
+}
+
+function switchToPopup() {
+  let tabIds = browser.getTabIds()
+  browser.switchTab(tabIds[1])
+}
+
+beforeEach(() => {
+  switchToCheckout()
+  checkoutForm.loadFrame()
+})
 
 describe('Beta Testing', () => {
   it('CyberSource', () => {
@@ -26,10 +37,15 @@ describe('Beta Testing', () => {
 
     assert.equal(browser.getTabIds().length, 2, 'Popup is opened')
 
+    switchToPopup()
+    browser.waitUntil(() => {
+      return browser.elements('form[name=PAReq]').value.length
+    }, 50000, 'Awaiting for bank page', 3000)
+    assert.equal(browser.elements('form[name=PAReq]').value.length, 1, 'Bank page is loaded')
+
     browser.waitUntil(() => {
       return (browser.getTabIds() || []).length === 1
-    }, 50000, 'Awaiting for server\'s response', 3000)
-
+    }, 50000, 'Awaiting for bank page to close', 3000)
     assert.equal(browser.getTabIds().length, 1, 'Popup is closed')
   })
 
@@ -38,7 +54,6 @@ describe('Beta Testing', () => {
       card_number: '341111111111111',
       card_cvv: '8888'
     })
-    let tabIds = browser.getTabIds()
 
     browser.waitUntil(() => {
       return (browser.getTabIds() || []).length === 2
@@ -46,7 +61,11 @@ describe('Beta Testing', () => {
 
     assert.equal(browser.getTabIds().length, 2, 'Popup is opened')
 
-    browser.switchTab(tabIds[1])
+    switchToPopup()
+    browser.waitUntil(() => {
+      return browser.elements('form[name=PAReq]').value.length
+    }, 50000, 'Awaiting for bank page', 3000)
+    assert.equal(browser.elements('form[name=PAReq]').value.length, 1, 'Bank page is loaded')
 
     browser.waitUntil(() => {
       return !!browser.elements('input[type=submit]').value.length
