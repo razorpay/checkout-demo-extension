741f81ea571ea4939b1796f60d66e3c9b307a972
Pranav Gupta <pranavguptarulz@gmail.com>
2017-03-22 14:52:48 +0530

    clear popup track interval

--- app/js/authorize.js
+++ app/js/authorize.js
@@ -283,14 +283,14 @@ Payment.prototype = {
     this.off();
   },
 
-  clear: function(){
+  clear: function() {
     try {
       this.popup.onClose = null;
       this.popup.close();
     } catch(e){}
     this.done = true;
     Razorpay.popup_delay = null;
-    clearInterval(Razorpay.popup_track);
+    clearInterval(self.popup_track_interval);
     clearTimeout(this.ajax_delay);
 
     // unbind listener
@@ -374,13 +374,13 @@ Payment.prototype = {
         try {
           noop(self.popup.window.document);
         } catch(e) {
-          clearInterval(Razorpay.popup_track);
+          clearInterval(self.popup_track_interval);
           track(self.r, 'popup_redirect', {
             duration: new Date() - nowTime
           });
         }
       }
-      setInterval(Razorpay.popup_track, 99);
+      self.popup_track_interval = setInterval(Razorpay.popup_track, 99);
       popup.onClose = this.r.emitter('payment.cancel');
     }
     this.popup = popup;
