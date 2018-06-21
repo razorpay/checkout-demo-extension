5e18318c4846172dbf3e3214cb5786cd5230d822
Pranav Gupta <pranavguptarulz@gmail.com>
2018-06-21 11:27:45 +0530

    added concurrency for e2e tests

--- blackbox/index.js
+++ blackbox/index.js
@@ -2,18 +2,49 @@ require('./api');
 const glob = require('glob').sync;
 const puppeteer = require('puppeteer');
 
-glob(__dirname + '/sites/*.js').forEach(async site => {
+// currently running tests;
+let running = 0;
+let fulfilled = 0;
+
+const concurrency = 10;
+
+let singleTest = process.argv[2];
+
+if (singleTest) {
+  tests = [__dirname + '/sites/' + singleTest + '.js'];
+} else {
+  tests = glob(__dirname + '/sites/*.js');
+}
+
+const run = async site => {
   const browser = await puppeteer.launch({
     executablePath: process.env.CHROME_BIN || '/usr/bin/chromium',
     args: ['--no-sandbox'],
-    headless: false
+    headless: !singleTest
     // devtools: true,
   });
   const page = await browser.newPage();
-  await require(site)(page);
   await page.goto('file://' + __dirname + '/index.html');
-  setTimeout(() => {
+  let timeout = setTimeout(() => {
     console.error('Payment not completed in 5s');
     process.exit(1);
   }, 5000);
-});
+  await require(site)(page);
+  clearTimeout(timeout);
+  await browser.close();
+  fulfilled++;
+};
+
+while (running < concurrency && running < tests.length) {
+  runNext();
+}
+
+async function runNext() {
+  await run(tests[running++]);
+  if (running < tests.length) {
+    runNext();
+  }
+  if (fulfilled === tests.length) {
+    process.exit(0);
+  }
+}
--- blackbox/sites/default.js
+++ blackbox/sites/default.js
@@ -1,43 +1,44 @@
 const { delay, loadCheckoutFrame } = require('../util');
 
-module.exports = async page => {
-  await page.exposeFunction('completeHandler', data => {
-    if (JSON.parse(data).razorpay_payment_id) {
-      console.log('wallet payment passed');
-      process.exit();
-    }
-  });
-  await page.exposeFunction('renderHandler', async () => {
-    await page.click('label[for=wallet-radio-mobikwik]');
-    await delay(250);
-    await page.click('.pay-btn');
-    await delay(1000);
-    await page.click('#otp');
-    await page.keyboard.type('123456');
-    await page.click('.otp-btn');
-  });
-  page.on('load', async () => {
+module.exports = async page =>
+  new Promise(async (resolve, reject) => {
+    await page.exposeFunction('renderHandler', async () => {
+      await page.click('label[for=wallet-radio-mobikwik]');
+      await delay(250);
+      await page.click('.pay-btn');
+      await delay(1000);
+      await page.click('#otp');
+      await page.keyboard.type('123456');
+      await page.click('.otp-btn');
+    });
+    await page.exposeFunction('completeHandler', data => {
+      if (JSON.parse(data).razorpay_payment_id) {
+        console.log('wallet payment passed');
+        resolve();
+      } else {
+        reject();
+      }
+    });
     await page.evaluate(`
-      CheckoutBridge = {
-        oncomplete: completeHandler,
-        onrender: renderHandler
+    CheckoutBridge = {
+      oncomplete: completeHandler,
+      onrender: renderHandler
+    }
+    Razorpay = {
+      config: {
+        frameApi: 'http://localhost:3000',
       }
-      Razorpay = {
-        config: {
-          frameApi: 'http://localhost:3000',
-        }
-      }
-    `);
+    }
+  `);
     await loadCheckoutFrame(page);
     await page.evaluate(`handleMessage({
-      options: {
-        key: 'm1key',
-        prefill: {
-          contact: '8879524924',
-          email: 'pranav@rzp.com',
-          method: 'wallet'
-        }
+    options: {
+      key: 'm1key',
+      prefill: {
+        contact: '8879524924',
+        email: 'pranav@rzp.com',
+        method: 'wallet'
       }
-    })`);
+    }
+  })`);
   });
-};
