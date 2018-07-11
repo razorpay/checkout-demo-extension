58842b3b80ee2fdeebd6c617179918698e65cf5b
Pranav Gupta <pranavguptarulz@gmail.com>
2018-07-11 17:34:36 +0530

    bank and wallet data separated

--- app/modules/authorize.js
+++ app/modules/authorize.js
@@ -1,7 +1,10 @@
-import getFingerprint from './fingerprint';
-import * as Tez from './tez';
 import * as cookie from 'lib/cookie';
 import * as Formatter from './formatter';
+import { processPaymentCreate, processCoproto } from 'payment/coproto';
+import Track from 'tracker';
+
+import { formatPayment } from 'payment/validator';
+
 import jsonp from 'lib/jsonp';
 
 var pollingInterval;
@@ -24,23 +27,6 @@ function setCommunicator() {
   }
 }
 setCommunicator();
-
-function submitPopup(payment) {
-  var popup = payment.popup;
-  var data = payment.data;
-
-  // fix long notes
-  _Obj.loop(data, (val, key) => {
-    if (/^notes/.test(key) && _.lengthOf(val) > 200) {
-      data[key] = val.replace(/\n/g, ' ');
-    }
-  });
-
-  // no ajax route was available
-  if (popup) {
-    _Doc.submitForm(makeRedirectUrl(payment.fees), data, 'post', popup.name);
-  }
-}
 
 function onPaymentCancel(metaParam) {
   if (!this.done) {
@@ -209,81 +195,9 @@ Payment.prototype = {
     }
   },
 
-  format: function() {
-    var data = this.data;
-
-    // Set view for fees.
-    if (this.fees) {
-      data.view = 'html';
-    }
-
-    // fill data from options if empty
-    var getOption = this.r.get;
-    each(
-      [
-        'amount',
-        'currency',
-        'signature',
-        'description',
-        'order_id',
-        'account_id',
-        'notes',
-        'subscription_id',
-        'payment_link_id',
-        'customer_id',
-        'recurring',
-        'subscription_card_change',
-        'recurring_token.max_amount',
-        'recurring_token.expire_by',
-      ],
-      function(i, field) {
-        if (!(field in data)) {
-          var val = getOption(field);
-          if (val) {
-            data[field.replace(/\.(\w+)/g, '[$1]')] = val;
-          }
-        }
-      }
-    );
-
-    var key_id = getOption('key');
-    if (!data.key_id && key_id) {
-      data.key_id = key_id;
-    }
-
-    // api needs this flag to decide between redirect/otp
-    if (this.powerwallet && data.method === 'wallet') {
-      data['_[source]'] = 'checkoutjs';
-    }
-
-    let fingerprint = getFingerprint();
-    if (fingerprint) {
-      data['_[shield][fhash]'] = fingerprint;
-    }
-
-    data['_[shield][tz]'] = new Date().getTimezoneOffset();
-
-    // flatten notes, card
-    // notes.abc -> notes[abc]
-    flattenProp(data, 'notes', '[]');
-    flattenProp(data, 'card', '[]');
-
-    var expiry = data['card[expiry]'];
-    if (isString(expiry)) {
-      data['card[expiry_month]'] = expiry.slice(0, 2);
-      data['card[expiry_year]'] = expiry.slice(-2);
-      delete data['card[expiry]'];
-    }
-
-    // add tracking data
-    data._ = Track.common();
-    // make it flat
-    flattenProp(data, '_', '[]');
-  },
-
   generate: function(data) {
     this.data = clone(data || this.data);
-    this.format();
+    formatPayment(this);
 
     if (this.shouldPopup() && !this.popup && this.r.get('callback_url')) {
       this.r.set('redirect', true);
@@ -298,7 +212,7 @@ Payment.prototype = {
     this.writePopup();
 
     if (!this.tryAjax()) {
-      submitPopup(this);
+      this.trySubmit();
     }
 
     // adding listeners
@@ -349,9 +263,8 @@ Payment.prototype = {
       var errorObj = data.error;
       if (!_.isNonNullObject(errorObj) || !errorObj.description) {
         if (data.request) {
-          var func = responseTypes[data.type];
-          if (typeof func === 'function') {
-            return func.call(this, data.request);
+          if (processCoproto.call(this, data)) {
+            return;
           }
         }
         data = discreet.error('Payment failed');
@@ -369,9 +282,6 @@ Payment.prototype = {
     } catch (e) {}
 
     this.done = true;
-    Razorpay.popup_delay = null;
-    clearInterval(this.popup_track_interval);
-    clearTimeout(this.ajax_delay);
 
     // unbind listener
     if (this.offmessage) {
@@ -420,21 +330,31 @@ Payment.prototype = {
     // else make ajax request
 
     var razorpayInstance = this.r;
-    var ajax_delay_timeout = 1e4; // 10s
 
-    this.ajax_delay = setTimeout(function() {
-      Track(razorpayInstance, 'ajax_delay', {
-        delay: ajax_delay_timeout,
-      });
-    }, ajax_delay_timeout);
-
-    Track(razorpayInstance, 'ajax');
     this.ajax = fetch.post({
       url: makeUrl('payments/create/ajax'),
       data,
-      callback: _Func.bind(ajaxCallback, this),
+      callback: _Func.bind(processPaymentCreate, this),
     });
     return 1;
+  },
+
+  trySubmit: function() {
+    var payment = this;
+    var popup = payment.popup;
+
+    // no ajax route was available
+    if (popup) {
+      var data = payment.data;
+
+      // fix long notes
+      _Obj.loop(data, (val, key) => {
+        if (/^notes/.test(key) && _.lengthOf(val) > 200) {
+          data[key] = val.replace(/\n/g, ' ');
+        }
+      });
+      _Doc.submitForm(makeRedirectUrl(payment.fees), data, 'post', popup.name);
+    }
   },
 
   makePopup: function() {
@@ -445,24 +365,6 @@ Payment.prototype = {
     }
 
     if (popup) {
-      var timer = _.timer();
-
-      Razorpay.popup_delay = () =>
-        Track(this.r, 'popup_delay', {
-          duration: timer(),
-        });
-
-      Razorpay.popup_track = () => {
-        try {
-          noop(this.popup.window.document);
-        } catch (e) {
-          error: e;
-          clearInterval(this.popup_track_interval);
-          Track(this.r, 'popup_acs', { duration: timer() });
-        }
-      };
-
-      this.popup_track_interval = setInterval(Razorpay.popup_track, 99);
       popup.onClose = this.r.emitter('payment.cancel');
     }
     this.popup = popup;
@@ -486,46 +388,6 @@ Payment.prototype = {
     }
   },
 };
-
-function ajaxCallback(response) {
-  clearTimeout(this.ajax_delay);
-  var payment_id = response.payment_id;
-  if (payment_id) {
-    this.payment_id = payment_id;
-  }
-
-  this.magicCoproto = response.magic || false;
-
-  Track(this.r, 'ajax_response', response);
-
-  var errorResponse = response.error;
-  var popup = this.popup;
-
-  // race between popup close poll and ajaxCallback. don't continue if payment has been canceled
-  if (popup && popup.checkClose()) {
-    return; // return if it's already closed
-  }
-  // if ajax call is blocked by ghostery or some other reason, fall back to redirection in popup
-  if (errorResponse && response.xhr && response.xhr.status === 0) {
-    if (popup) {
-      submitPopup(this);
-    }
-    return Track(this.r, 'no_popup');
-  }
-
-  if (response.razorpay_payment_id || errorResponse) {
-    this.complete(response);
-  } else {
-    var request = response.request;
-    if (request && request.url && RazorpayConfig.frame) {
-      request.url = request.url.replace(/^.+v1\//, makeUrl());
-    }
-    var func = responseTypes[response.type];
-    if (typeof func === 'function') {
-      func.call(this, request, response);
-    }
-  }
-}
 
 function pollPaymentData(onComplete) {
   clearPollingInterval(true);
@@ -552,201 +414,6 @@ function makeRedirectUrl(fees) {
   return makeUrl('payments/create/' + (fees ? 'fees' : 'checkout'));
 }
 
-var responseTypes = {
-  // this === payment
-  first: function(request, fullResponse) {
-    var direct = request.method === 'direct';
-    var content = request.content;
-    var popup = this.popup;
-    var coprotoMagic = fullResponse.magic ? fullResponse.magic : false;
-
-    if (this.isMagicPayment) {
-      this.r._payment.emit('magic.init');
-
-      var popupOptions = {
-        focus: !coprotoMagic,
-        magic: coprotoMagic,
-        otpelf: true,
-      };
-
-      if (direct) {
-        popupOptions.content = content;
-      } else {
-        var url =
-          "javascript: submitForm('" +
-          request.url +
-          "', " +
-          _Obj.stringify(request.content) +
-          ", '" +
-          request.method +
-          "')";
-        popupOptions.url = url;
-      }
-
-      global.CheckoutBridge.invokePopup(_Obj.stringify(popupOptions));
-    } else if (popup) {
-      if (direct) {
-        // direct is true for payzapp
-        popup.write(content);
-      } else {
-        _Doc.submitForm(
-          request.url,
-          request.content,
-          request.method,
-          popup.window.name
-        );
-      }
-      // popup blocking addons close popup once we set a url
-      setTimeout(() => {
-        if (popup.window.closed && this.r.get('callback_url')) {
-          this.r.set('redirect', true);
-          this.checkRedirect();
-        }
-      });
-    }
-  },
-
-  async: function(request, fullResponse) {
-    this.ajax = fetch({
-      url: request.url,
-      callback: response => this.complete(response),
-    }).till(response => response && response.status);
-
-    this.emit('upi.pending', fullResponse.data);
-  },
-
-  tez: function(coprotoRequest, fullResponse) {
-    Tez.pay(
-      fullResponse.data,
-      instrument => {
-        this.emit('upi.intent_response', {
-          response: instrument.details,
-        });
-      },
-      error => {
-        if (error.code && error.code === error.ABORT_ERR) {
-          this.emit('upi.intent_response', {});
-        }
-
-        Track(this.r, 'tez_error', error);
-      }
-    );
-  },
-
-  intent: function(request, fullResponse) {
-    var self = this;
-    var url = request.url;
-
-    var upiBackCancel = {
-      '_[method]': 'upi',
-      '_[flow]': 'intent',
-      '_[reason]': 'UPI_INTENT_BACK_BUTTON',
-    };
-
-    var ra = () =>
-      fetch({
-        url: request.url,
-        callback: response => this.complete(response),
-      }).till(response => response && response.status);
-
-    this.emit('upi.coproto_response', request);
-
-    var intent_url = (fullResponse.data || {}).intent_url;
-    this.on('upi.intent_response', function(data) {
-      if (isEmptyObject(data)) {
-        return self.emit('cancel', upiBackCancel);
-      } else if (data.response) {
-        var response = {};
-
-        if (_.isNonNullObject(data.response)) {
-          response = data.response;
-        } else {
-          // Convert the string response into a JSON object.
-          var split = data.response.split('&');
-          for (var i = 0; i < split.length; i++) {
-            var pair = split[i].split('=');
-            if (
-              pair[1] === '' ||
-              pair[1] === 'undefined' ||
-              pair[1] === 'null'
-            ) {
-              response[pair[0]] = null;
-            } else {
-              response[pair[0]] = pair[1];
-            }
-          }
-        }
-
-        if (!response.txnId) {
-          return self.emit('cancel', upiBackCancel);
-        }
-      } else {
-        self.emit('upi.pending', { flow: 'upi-intent', response: data });
-      }
-      self.ajax = ra();
-    });
-
-    var CheckoutBridge = window.CheckoutBridge;
-    if (CheckoutBridge && CheckoutBridge.callNativeIntent) {
-      // If there's a UPI App specified, use it.
-      if (this.upi_app) {
-        CheckoutBridge.callNativeIntent(intent_url, this.upi_app);
-      } else {
-        CheckoutBridge.callNativeIntent(intent_url);
-      }
-    } else if (ua_android_browser) {
-      if (this.tez) {
-        return responseTypes['tez'].call(this, request, fullResponse);
-      }
-
-      // Start Timeout
-      var drawerTimeout = setTimeout(function() {
-        /**
-         * If upi app selection drawer not happened (technically,
-         * checkout is not blurred until 3 sec)
-         */
-        self.emit('cancel', {
-          '_[method]': 'upi',
-          '_[flow]': 'intent',
-          '_[reason]': 'UPI_INTENT_WEB_NO_APPS',
-        });
-        self.emit('upi.noapp');
-      }, 3000);
-
-      var blurHandler = function() {
-        /**
-         * If upi app selection drawer opened before 3 sec, clear timeout
-         */
-        clearTimeout(drawerTimeout);
-        self.emit('upi.selectapp');
-      };
-
-      var focHandler = function() {
-        self.emit('upi.pending', { flow: 'upi-intent' });
-
-        window.removeEventListener('blur', blurHandler);
-        window.removeEventListener('focus', focHandler);
-        ra();
-      };
-
-      window.addEventListener('blur', blurHandler);
-      window.addEventListener('focus', focHandler);
-
-      window.location = fullResponse.data.intent_url;
-    }
-  },
-
-  otp: function(request) {
-    this.otpurl = request.url;
-    this.emit('otp.required');
-  },
-
-  // prettier-ignore
-  'return': function(request) {
-    discreet.redirect(request);
-  }
-};
-
 function otpCallback(response) {
   var error = response.error;
   if (error) {
@@ -757,7 +424,7 @@ function otpCallback(response) {
     }
     this.complete(response);
   }
-  ajaxCallback.call(this, response);
+  processCoproto.call(this, response);
 }
 
 var razorpayProto = Razorpay.prototype;
@@ -800,7 +467,7 @@ razorpayProto.resendOTP = function(callback) {
     data: {
       '_[source]': 'checkoutjs',
     },
-    callback: _Func.bind(ajaxCallback, payment),
+    callback: _Func.bind(processCoproto, payment),
   });
 };
 
@@ -826,7 +493,7 @@ razorpayProto.topupWallet = function() {
           method: request.method || 'post',
         });
       } else {
-        ajaxCallback.call(payment, response);
+        processCoproto.call(payment, response);
       }
     },
   });
---
+++ app/modules/payment/coproto.js
@@ -0,0 +1,234 @@
+import * as Tez from './tez';
+import Track from 'tracker';
+
+export const processPaymentCreate = function(response) {
+  var payment = this;
+
+  payment.payment_id = response.payment_id;
+  payment.magicCoproto = response.magic || false;
+
+  var popup = payment.popup;
+
+  // race between popup close poll and ajaxCallback. don't continue if payment has been canceled
+  if (popup && popup.checkClose()) {
+    return; // return if it's already closed
+    // otherwise subsequesnt code may lead to redirection in main window
+  }
+
+  // if ajax call is blocked by ghostery or some other reason, fall back to redirection in popup
+  var errorResponse = response.error;
+  if (errorResponse && response.xhr && response.xhr.status === 0) {
+    payment.trySubmit();
+    return Track(payment.r, 'no_popup');
+  }
+
+  if (response.razorpay_payment_id || errorResponse) {
+    payment.complete(response);
+  } else {
+    processCoproto.call(payment, response);
+  }
+};
+
+export const processCoproto = function(response) {
+  var func = responseTypes[response.type];
+  var isFunction = _.isFunction(func);
+  if (isFunction) {
+    func.call(this, response.request, response);
+  }
+  return isFunction;
+};
+
+var responseTypes = {
+  // this === payment
+  first: function(request, fullResponse) {
+    var direct = request.method === 'direct';
+    var content = request.content;
+    var popup = this.popup;
+    var coprotoMagic = fullResponse.magic ? fullResponse.magic : false;
+
+    if (this.isMagicPayment) {
+      this.emit('magic.init');
+
+      var popupOptions = {
+        focus: !coprotoMagic,
+        magic: coprotoMagic,
+        otpelf: true,
+      };
+
+      if (direct) {
+        popupOptions.content = content;
+      } else {
+        var url =
+          "javascript: submitForm('" +
+          request.url +
+          "', " +
+          _Obj.stringify(request.content) +
+          ", '" +
+          request.method +
+          "')";
+        popupOptions.url = url;
+      }
+
+      global.CheckoutBridge.invokePopup(_Obj.stringify(popupOptions));
+    } else if (popup) {
+      if (direct) {
+        // direct is true for payzapp
+        popup.write(content);
+      } else {
+        _Doc.submitForm(
+          request.url,
+          request.content,
+          request.method,
+          popup.window.name
+        );
+      }
+      // popup blocking addons close popup once we set a url
+      setTimeout(() => {
+        if (popup.window.closed && this.r.get('callback_url')) {
+          this.r.set('redirect', true);
+          this.checkRedirect();
+        }
+      });
+    }
+  },
+
+  async: function(request, fullResponse) {
+    this.ajax = fetch({
+      url: request.url,
+      callback: response => this.complete(response),
+    }).till(response => response && response.status);
+
+    this.emit('upi.pending', fullResponse.data);
+  },
+
+  tez: function(coprotoRequest, fullResponse) {
+    Tez.pay(
+      fullResponse.data,
+      instrument => {
+        this.emit('upi.intent_response', {
+          response: instrument.details,
+        });
+      },
+      error => {
+        if (error.code && error.code === error.ABORT_ERR) {
+          this.emit('upi.intent_response', {});
+        }
+
+        Track(this.r, 'tez_error', error);
+      }
+    );
+  },
+
+  intent: function(request, fullResponse) {
+    var url = request.url;
+
+    var upiBackCancel = {
+      '_[method]': 'upi',
+      '_[flow]': 'intent',
+      '_[reason]': 'UPI_INTENT_BACK_BUTTON',
+    };
+
+    var ra = () =>
+      fetch({
+        url: request.url,
+        callback: response => this.complete(response),
+      }).till(response => response && response.status);
+
+    this.emit('upi.coproto_response', request);
+
+    var intent_url = (fullResponse.data || {}).intent_url;
+
+    this.on('upi.intent_response', data => {
+      if (_Obj.isEmpty(data)) {
+        return this.emit('cancel', upiBackCancel);
+      } else if (data.response) {
+        var response = {};
+
+        if (_.isNonNullObject(data.response)) {
+          response = data.response;
+        } else {
+          // Convert the string response into a JSON object.
+          var split = data.response.split('&');
+          for (var i = 0; i < split.length; i++) {
+            var pair = split[i].split('=');
+            if (
+              pair[1] === '' ||
+              pair[1] === 'undefined' ||
+              pair[1] === 'null'
+            ) {
+              response[pair[0]] = null;
+            } else {
+              response[pair[0]] = pair[1];
+            }
+          }
+        }
+
+        if (!response.txnId) {
+          return this.emit('cancel', upiBackCancel);
+        }
+      } else {
+        this.emit('upi.pending', { flow: 'upi-intent', response: data });
+      }
+      this.ajax = ra();
+    });
+
+    var CheckoutBridge = window.CheckoutBridge;
+    if (CheckoutBridge && CheckoutBridge.callNativeIntent) {
+      // If there's a UPI App specified, use it.
+      if (this.upi_app) {
+        CheckoutBridge.callNativeIntent(intent_url, this.upi_app);
+      } else {
+        CheckoutBridge.callNativeIntent(intent_url);
+      }
+    } else if (ua_android_browser) {
+      if (this.tez) {
+        return responseTypes['tez'].call(this, request, fullResponse);
+      }
+
+      // Start Timeout
+      var drawerTimeout = setTimeout(() => {
+        /**
+         * If upi app selection drawer not happened (technically,
+         * checkout is not blurred until 3 sec)
+         */
+        this.emit('cancel', {
+          '_[method]': 'upi',
+          '_[flow]': 'intent',
+          '_[reason]': 'UPI_INTENT_WEB_NO_APPS',
+        });
+        this.emit('upi.noapp');
+      }, 3000);
+
+      var blurHandler = () => {
+        /**
+         * If upi app selection drawer opened before 3 sec, clear timeout
+         */
+        clearTimeout(drawerTimeout);
+        this.emit('upi.selectapp');
+      };
+
+      var focHandler = () => {
+        this.emit('upi.pending', { flow: 'upi-intent' });
+
+        window.removeEventListener('blur', blurHandler);
+        window.removeEventListener('focus', focHandler);
+        ra();
+      };
+
+      window.addEventListener('blur', blurHandler);
+      window.addEventListener('focus', focHandler);
+
+      window.location = fullResponse.data.intent_url;
+    }
+  },
+
+  otp: function(request) {
+    this.otpurl = request.url;
+    this.emit('otp.required');
+  },
+
+  // prettier-ignore
+  'return': function(request) {
+    discreet.redirect(request);
+  }
+};
