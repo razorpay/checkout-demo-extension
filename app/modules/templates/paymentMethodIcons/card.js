863d30d3ede7806574b83e5d9a249b8d214c3049
rzpamidi <prashanth.pamidi@razorpay.com>
2018-07-19 14:35:35 +0530

    Simplifies svg icons

--- app/modules/templates/paymentMethodIcons/card.js
+++ app/modules/templates/paymentMethodIcons/card.js
@@ -1,16 +1,5 @@
 export default (foregroundColor, backgroundColor) =>
-  `<svg width="27" height="22" viewBox="0 0 27 22" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <defs>
-      <path id="a" d="M0 0h28v28H0z"/>
-    </defs>
-    <g transform="translate(0 -3)" fill="none" fill-rule="evenodd">
-      <mask id="b" fill="#fff">
-        <use xlink:href="#a"/>
-      </mask>
-      <path d="M2 10v13h18v-5H7v-8H2zm0-2h5v10h15v5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V10a2 2 0 0 1 2-2z" fill="${backgroundColor}" fill-rule="nonzero" mask="url(#b)"/>
-      <g mask="url(#b)" fill="${foregroundColor}" fill-rule="nonzero">
-        <path d="M7 5v13h18V5H7zm0-2h18a2 2 0 0 1 2 2v13a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z"/>
-        <path d="M10.004 16.003a1 1 0 1 1 0-2h2a1 1 0 0 1 0 2h-2zM6.718 10.005a1 1 0 1 1 0-2h19a1 1 0 0 1 0 2h-19z"/>
-      </g>
-    </g>
+  `<svg width="27" height="22" viewBox="0 0 27 22" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2 7v13h18v-5H7V7H2zm0-2h5v10h15v5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2z" fill="${backgroundColor}"/>
+    <path d="M10.004 13.003a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2h-2zM7 0h18a2 2 0 0 1 2 2v13a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zm0 2v13h18V2H7zm-.282 5.005a1 1 0 1 1 0-2h19a1 1 0 0 1 0 2h-19z" fill="${foregroundColor}"/>
   </svg>`;
--- app/modules/templates/paymentMethodIcons/wallet.js
+++ app/modules/templates/paymentMethodIcons/wallet.js
@@ -1,13 +1,8 @@
 export default (foregroundColor, backgroundColor) =>
   `<svg width="25" height="23" viewBox="0 0 25 23" xmlns="http://www.w3.org/2000/svg">
-    <g fill="none" fill-rule="evenodd">
-      <path d="M-2-2h28v28H-2z"/>
-      <g transform="translate(0 5)">
-        <path d="M2 2v14h19v-5h2.011V5H21V2H2zm0-2h19a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z" fill="${foregroundColor}" fill-rule="nonzero"/>
-        <circle fill="${foregroundColor}" cx="17" cy="9" r="1"/>
-      </g>
-      <g fill="${backgroundColor}" fill-rule="nonzero">
-        <path d="M7.326 5L16 2.11V5h2V2.11A2 2 0 0 0 15.368.211L1 5h6.326zM15 12v4h8v-4h-8zm0-2h8a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-8a2 2 0 0 1-2-2v-4a2 2 0 0 1 2-2z"/>
-      </g>
+    <g transform="translate(0 5)">
+      <path d="M2 2v14h19v-5h2.011V5H21V2H2zm0-2h19a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z" fill="${foregroundColor}" fill-rule="nonzero"/>
+      <circle fill="${foregroundColor}" cx="17" cy="9" r="1"/>
     </g>
+    <path fill="${backgroundColor}" d="M7.326 5L16 2.11V5h2V2.11A2 2 0 0 0 15.368.211L1 5h6.326zM15 12v4h8v-4h-8zm0-2h8a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-8a2 2 0 0 1-2-2v-4a2 2 0 0 1 2-2z"/>
   </svg>`;
