30a7d30a6b6b1c7626739885e38508ef6bc0e414
Umang Galaiya <umang.galaiya@razorpay.com>
2018-11-20 11:42:32 +0530

    Update provider names.

--- app/modules/common/cardlessemi.js
+++ app/modules/common/cardlessemi.js
@@ -6,8 +6,8 @@ const prefix = cdnUrl + 'cardless_emi/';
 const sqPrefix = cdnUrl + 'cardless_emi-sq/';
 
 const list = {
-  zestmoney: ['Zest Money'],
-  earlysalary: ['Early Salary'],
+  zestmoney: ['ZestMoney'],
+  earlysalary: ['EarlySalary'],
 };
 
 export const providers = _Obj.map(list, (details, code) => ({
