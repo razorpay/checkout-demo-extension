252a7a1786c5a02f0b016f732ba930f39492b953
Ankit <ankitstarski@gmail.com>
2018-11-20 21:11:19 +0530

    add Object.entries polyfill

--- app/modules/entry/checkout-frame.js
+++ app/modules/entry/checkout-frame.js
@@ -2,6 +2,7 @@ import Razorpay, { RazorpayConfig } from 'common/Razorpay';
 import 'checkoutjs/options';
 import 'payment';
 import Track from 'tracker';
+import 'lib/polyfill';
 
 Track.props.library = 'checkoutjs';
 RazorpayConfig.api = _Doc.resolveUrl(RazorpayConfig.frameApi);
