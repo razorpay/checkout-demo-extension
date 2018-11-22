1517550f1ae5246dd8a20b6e1f4db9022e6449a9
Umang Galaiya <umang.galaiya@razorpay.com>
2018-11-22 13:50:26 +0530

    Add option svelte components.

---
+++ app/modules/templates/views/ui/options/NextOption.svelte
@@ -0,0 +1,47 @@
+<Option
+  attributes={attributes}
+  {data}
+  type="next-option"
+
+  on:select="select(event)"
+>
+  <div class="option-title">{title}</div>
+  {#if arrowText}
+    <div class="option-arrow-text theme-highlight">{arrowText}</div>
+  {/if}
+</Option>
+
+<script>
+  export default {
+    components: {
+      Option: 'templates/views/ui/options/Option.svelte',
+    },
+
+    computed: {
+      attributes: function ({ icon }) {
+        const attribs = {};
+
+        if (icon) {
+          attribs.style = `background-image: url('${icon}');`;
+        }
+
+        return attribs;
+      }
+    },
+
+    data: function () {
+      return {
+        icon: null,
+        arrowText: null,
+      };
+    },
+
+    methods: {
+      select: function (e) {
+        e.option = this.get().data;
+
+        this.fire('select', e);
+      }
+    },
+  }
+</script>
