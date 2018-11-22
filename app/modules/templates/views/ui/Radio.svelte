4c1081e540f0925593ec239a77713ae808f40222
Umang Galaiya <umang.galaiya@razorpay.com>
2018-11-23 03:07:12 +0530

    Add Radio component.

---
+++ app/modules/templates/views/ui/Radio.svelte
@@ -0,0 +1,36 @@
+<div class="flow input-radio">
+  <input
+    type="radio"
+    {checked}
+    class={inputClass}
+    {id}
+    {name}
+    {value}
+
+    on:change="fire('change', event)"
+  >
+  <label for={id}>
+    <div class="radio-display"></div>
+    <div class="label-content">{@html label}</div>
+  </label>
+</div>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        checked: false,
+        containerClass: '',
+        id: '',
+        inputClass: '',
+        name: '',
+        value: '',
+      };
+    },
+
+    computed: {
+      containerClass: ({ containerClass }) => `input-radio ${containerClass}`,
+      inputClass: ({ inputClass }) => ``
+    }
+  }
+</script>
