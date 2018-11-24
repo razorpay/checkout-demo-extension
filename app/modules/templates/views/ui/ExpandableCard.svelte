f4ff507fb4dfb7fa234692b8a0ee3bcedf89e5c8
Umang Galaiya <umang.galaiya@razorpay.com>
2018-11-24 20:37:23 +0530

    Add expandable card.

---
+++ app/modules/templates/views/ui/ExpandableCard.svelte
@@ -0,0 +1,29 @@
+<div
+  class={elementClass}
+
+  on:click="fire('click', event)"
+>
+  <div class="expandable-card-title">
+    {@html title}
+    {#if badge}
+      <div class="badge">{badge}</div>
+    {/if}
+  </div>
+  <div class="expandable-card-detail">{@html detail}</div>
+</div>
+
+<script>
+  export default {
+    computed: {
+      elementClass: ({ expanded }) => {
+        const list = ['expandable-card'];
+
+        if (expanded) {
+          list.push('expandable-card--expanded');
+        }
+
+        return list.join(' ');
+      },
+    },
+  }
+</script>
