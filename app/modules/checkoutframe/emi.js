e774b00e09cdf23f8bcd145aa3ee0b6ba95c2c74
Umang Galaiya <umang.galaiya@razorpay.com>
2018-11-27 19:06:26 +0530

    Remove extra code.

--- app/modules/checkoutframe/emi.js
+++ app/modules/checkoutframe/emi.js
@@ -1,25 +1,19 @@
 /* global templates, showOverlay, hideEmi, Event */
 import EmiView from 'templates/views/emi.svelte';
-import Razorpay from 'common/Razorpay';
-import { AMEX_EMI_MIN, EMI_HELP_TEXT } from 'common/constants';
-import * as OptionsList from 'components/OptionsList';
+import { AMEX_EMI_MIN } from 'common/constants';
 
 function hideEMIDropdown() {
   const body = _Doc.querySelector('#body');
-  const parent = _Doc.querySelector('#emi-check-label');
 
   if (body |> _El.hasClass('emi-focus')) {
     body |> _El.removeClass('emi-focus');
-    parent |> _El.removeClass('focus');
   }
 }
 
 function showEMIDropdown() {
   const body = _Doc.querySelector('#body');
-  const parent = _Doc.querySelector('#emi-check-label');
 
   body |> _El.addClass('emi-focus');
-  parent |> _El.addClass('focus');
 }
 
 export default function emiView(session) {
@@ -73,34 +67,6 @@ emiView.prototype = {
   },
 
   bind() {
-    let session = this.session;
-
-    this.on('change', '#emi_bank', e => {
-      let bank = e.target.value;
-      let text = '';
-
-      if (bank) {
-        text = this.opts.banks[bank].name;
-      }
-
-      if (this.prevBank !== bank) {
-        _Doc.querySelector('#emi_duration').value = '';
-      }
-
-      this.prevBank = bank;
-    });
-
-    this.on(
-      'click',
-      '#container',
-      function(e) {
-        if (e.target.id !== 'emi-check-label') {
-          hideEMIDropdown();
-        }
-      },
-      true
-    );
-
     this.on('click', '#view-emi-plans', function() {
       // TODO: Update showOverlay once session.js is refactored.
       showOverlay({ 0: _Doc.querySelector('#emi-wrap') });
