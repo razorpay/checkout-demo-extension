372c37277e12dd1ef74855f52d83ef9cb98070e1
Umang Galaiya <umang.galaiya@razorpay.com>
2018-12-03 16:30:00 +0530

    Add inputs.

--- app/modules/templates/screens/cardlessemi.svelte
+++ app/modules/templates/screens/cardlessemi.svelte
@@ -1,4 +1,7 @@
 <div class="tab-content showable screen pad collapsible" id="form-cardless_emi">
+  <input type="hidden" name="emi_duration">
+  <input type="hidden" name="provider">
+  <input type="hidden" name="ott">
   <h3>Select an Option</h3>
   <div class="options">
     {#each providers as provider}
