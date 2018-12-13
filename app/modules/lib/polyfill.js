12e22a662c029c7d03e92d0d48ae96e41904c1b7
Umang Galaiya <umang.galaiya@razorpay.com>
2018-12-13 18:15:55 +0530

    Add classlist.toggle polyfill.

--- app/modules/lib/polyfill.js
+++ app/modules/lib/polyfill.js
@@ -1,3 +1,5 @@
+/* global DOMTokenList */
+
 /**
  * Object.entries polyfill
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
@@ -14,3 +16,18 @@ if (!Object.entries) {
     return resArray;
   };
 }
+
+/**
+ * Because classList.toggle is broken in IE10 and IE11.
+ * https://caniuse.com/#feat=classlist
+ */
+DOMTokenList.prototype.toggle = function(val) {
+  if (arguments.length > 1) {
+    return this[arguments[1] ? 'add' : 'remove'](val), !!arguments[1];
+  }
+  var oldValue = this.value;
+  return (
+    this.remove(oldValue),
+    oldValue === this.value && (this.add(val), true) /*|| false*/
+  );
+};
