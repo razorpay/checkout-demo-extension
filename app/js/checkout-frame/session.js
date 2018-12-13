0657a8dad275935d1ef481ed95d0e99bb70c00c5
Umang Galaiya <umang.galaiya@razorpay.com>
2018-12-13 16:01:04 +0530

    Add analytics for Cardless EMI and EMI tab separation.

--- app/js/checkout-frame/session.js
+++ app/js/checkout-frame/session.js
@@ -1528,6 +1528,13 @@ Session.prototype = {
           select: function(event) {
             var providerCode = event.option.code;
 
+            Analytics.track('cardless_emi:provider:select', {
+              type: AnalyticsTypes.BEHAV,
+              data: {
+                provider: providerCode,
+              },
+            });
+
             // User selected EMI on Cards
             if (providerCode === 'cards') {
               self.switchTab('emi');
@@ -1599,6 +1606,12 @@ Session.prototype = {
       return;
     }
 
+    Analytics.track('cardless_emi:plans:view', {
+      data: {
+        provider: providerCode,
+      },
+    });
+
     var plansList = this.getCardlessEmiPlans(plans);
 
     this.emiPlansView.setPlans({
@@ -1629,6 +1642,14 @@ Session.prototype = {
           $('#form-cardless_emi input[name=ott]').val(
             CardlessEmiStore.ott[CardlessEmiStore.providerCode]
           );
+
+          Analytics.track('cardless_emi:plan:select', {
+            type: AnalyticsTypes.BEHAV,
+            data: {
+              provider: providerCode,
+              value: value,
+            },
+          });
 
           self.preSubmit();
         },
@@ -1667,6 +1688,9 @@ Session.prototype = {
           }
 
           self.showLoadError(errorDesc, true);
+
+          Analytics.track('cardless_emi:plans:fetch:error');
+
           return;
         }
 
@@ -1688,6 +1712,12 @@ Session.prototype = {
         amount: self.get('amount'),
       }
     );
+
+    Analytics.track('cardless_emi:plans:fetch:start', {
+      data: {
+        provider: providerCode,
+      },
+    });
   },
 
   setOtpScreen: function() {
@@ -2404,18 +2434,26 @@ Session.prototype = {
     if (enabledMethods.emi) {
       this.on('click', '#add-card-container', 'emi-plans-trigger', function(e) {
         var $target = $(e.delegateTarget);
+        var eventName = 'emi:plans:';
+        var eventData = {
+          from: self.tab,
+        };
 
         self.removeAndCleanupOffers();
 
         if ($target.$('.emi-plan-unselected:not(.hidden)')[0]) {
           self.showEmiPlans('new')(e);
+          eventName += 'view';
         } else if ($target.$('.emi-plan-selected:not(.hidden)')[0]) {
           self.showEmiPlans('new')(e);
+          eventName += 'edit';
         } else if ($target.$('.emi-pay-without:not(.hidden)')[0]) {
           if (enabledMethods.card) {
             self.setScreen('card');
             self.switchTab('card');
             self.offers && self.renderOffers(this.tab);
+
+            eventName = 'emi:pay_without';
           }
         } else if ($target.$('.emi-plan-unavailable:not(.hidden)')[0]) {
           if (enabledMethods.card) {
@@ -2423,8 +2461,15 @@ Session.prototype = {
             self.switchTab('card');
             self.toggleSavedCards(false);
             self.offers && self.renderOffers(this.tab);
+
+            eventName = 'emi:pay_without';
           }
         }
+
+        Analytics.track(eventName, {
+          type: AnalyticsTypes.BEHAV,
+          data: eventData,
+        });
       });
 
       this.on('click', '#form-card', 'saved-card-pay-without-emi', function(e) {
@@ -3395,8 +3440,22 @@ Session.prototype = {
     var self = this;
     var emi_options = this.emi_options;
     var amount = this.get('amount');
-    var viewAllPlans = function() {
-      showOverlay($('#emi-wrap'));
+
+    var trackEmi = function(name, data) {
+      Analytics.track(name, {
+        type: AnalyticsTypes.BEHAV,
+        data: data,
+      });
+    };
+
+    var viewAllPlans = function(tab) {
+      trackEmi('emi:plans:all:view', {
+        from: tab,
+      });
+
+      return function() {
+        showOverlay($('#emi-wrap'));
+      };
     };
 
     if (type === 'new') {
@@ -3429,6 +3488,10 @@ Session.prototype = {
             }),
 
             payWithoutEmi: function() {
+              trackEmi('emi:pay_without', {
+                from: prevTab,
+              });
+
               $('#emi_duration').val('');
 
               self.switchTab('card');
@@ -3442,6 +3505,11 @@ Session.prototype = {
               var plan = plans[value];
               var text = getEmiText(amount, plan).short || '';
 
+              trackEmi('emi:plan:select', {
+                from: prevTab,
+                value: value,
+              });
+
               $('#emi_duration').val(value);
               $trigger.$(
                 '.emi-plan-selected .emi-plans-text'
@@ -3453,10 +3521,11 @@ Session.prototype = {
               self.processOffersOnEmiPlanSelection(plan);
 
               $('.select-plan-btn').addClass('invisible');
+
               self.preSubmit();
             },
 
-            viewAll: viewAllPlans,
+            viewAll: viewAllPlans(prevTab),
           },
 
           actions: {
@@ -3504,6 +3573,10 @@ Session.prototype = {
             },
 
             payWithoutEmi: function() {
+              trackEmi('emi:pay_without', {
+                from: prevTab,
+              });
+
               $trigger.$('.emi_duration').val('');
               toggleEmiPlanDetails($trigger.parent().parent(), false);
 
@@ -3517,6 +3590,11 @@ Session.prototype = {
             select: function(value) {
               var plan = plans[value];
               var text = getEmiText(amount, plan).short || '';
+
+              trackEmi('emi:plan:select', {
+                from: prevTab,
+                value: value,
+              });
 
               $trigger.$('.emi_duration').val(value);
               $trigger.$(
@@ -3540,7 +3618,7 @@ Session.prototype = {
               }
             },
 
-            viewAll: viewAllPlans,
+            viewAll: viewAllPlans(prevTab),
           },
 
           actions: {
@@ -3622,7 +3700,16 @@ Session.prototype = {
         this.savedCardsView.setCards({
           cards: this.transformedTokens,
           on: {
-            viewPlans: this.showEmiPlans('saved'),
+            viewPlans: function(e) {
+              Analytics.track('saved_card:emi:plans:view', {
+                type: AnalyticsTypes.BEHAV,
+                data: {
+                  from: self.tab,
+                },
+              });
+
+              self.showEmiPlans('saved')(e);
+            },
           },
         });
 
@@ -4962,9 +5049,9 @@ Session.prototype = {
     if (amount < 300000) {
       methods.cardless_emi = null;
     } else if (methods.cardless_emi instanceof Array) {
-    /**
-     * methods.cardless_emi will be [] when there are no providers enabled.
-     */
+      /**
+       * methods.cardless_emi will be [] when there are no providers enabled.
+       */
       if (methods.cardless_emi.length === 0) {
         methods.cardless_emi = null;
       }
