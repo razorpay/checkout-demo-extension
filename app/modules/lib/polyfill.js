c137f987ce4ebbb243f1c44d554eab3974c43518
Umang Galaiya <umang.galaiya@razorpay.com>
2018-12-14 13:44:08 +0530

    Add IE condition for classList.toggle polyfill.

--- app/modules/lib/polyfill.js
+++ app/modules/lib/polyfill.js
@@ -1,3 +1,5 @@
+import { internetExplorer } from 'common/useragent';
+
 /* global DOMTokenList */
 
 /**
@@ -21,13 +23,15 @@ if (!Object.entries) {
  * Because classList.toggle is broken in IE10 and IE11.
  * https://caniuse.com/#feat=classlist
  */
-DOMTokenList.prototype.toggle = function(val) {
-  if (arguments.length > 1) {
-    return this[arguments[1] ? 'add' : 'remove'](val), !!arguments[1];
-  }
-  var oldValue = this.value;
-  return (
-    this.remove(oldValue),
-    oldValue === this.value && (this.add(val), true) /*|| false*/
-  );
-};
+if (internetExplorer && DOMTokenList) {
+  DOMTokenList.prototype.toggle = function(val) {
+    if (arguments.length > 1) {
+      return this[arguments[1] ? 'add' : 'remove'](val), !!arguments[1];
+    }
+    var oldValue = this.value;
+    return (
+      this.remove(oldValue),
+      oldValue === this.value && (this.add(val), true) /*|| false*/
+    );
+  };
+}
