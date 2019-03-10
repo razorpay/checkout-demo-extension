aa6ac25734c0a6e6cb1aed54340c92870e7cfa85
Siddharth Goswami <vistax86live@gmail.com>
2019-03-10 05:41:26 +0530

    Add Fee Bearer support for all methods. (#859)
    * Adds fee breakup overlay.
    
    * Adds fee breakup overlay css.
    
    * Makes overlay hiding generic.
    
    * Adds AsyncLoading component.
    
    * Adds FeeBearer component.
    
    * Adds FeeBearerView
    
    * Adds FeeBearerView to discreet
    
    * Uses FeeBearer component in QR
    
    * Uses FeeBearer component in UPI
    
    * Adds flag for checking fee redirection.
    
    * Fix fee breakup view in mobile
    
    * Fix QR callback argument
    
    * Uses getFormData() instead of payload.
    
    * Refactors fees to feesRedirect
    
    * Remove unused methods.
    
    * Destroy feeBearer after submit.
    
    * Handles error properly.
    
    * Use return in create fees callback.
    
    * Moves fee bearer logic to a better place.
    
    * Breaks createFees function into two parts (even though it causes circular dependency).
    
    * Reuse fee bearer view.
    
    * Fixes async loading style.
    
    * Fixes FeeBearerView import.
    
    * Refactors feebearer component.
    
    * Respects ES5 syntax.
    
    * Nullifies feeBearerView from session.
    
    * Fixes payload refresh issue.
    
    * Fixes card expiry issue.
    
    * Moves makeFeesTable into feebearer.svelte
    
    * Fixes comments.
    
    * Removes destroy for fee bearer view.
    
    * Uses default export for FeeBearerView.
    
    * create a more generic payment payload formatter
    
    * Makes svelte happy.
    
    * Revert "disable upi intent on customer fee_bearer (#842)"
    
    This reverts commit 72ccd0eff189afdcf40500590f70d25cede7ee73.
    
    * Uses clone instead of Object.assign
    
    * Fixes amount symbol.

--- app/modules/checkoutframe/discreet.js
+++ app/modules/checkoutframe/discreet.js
@@ -15,6 +15,7 @@ import * as OtpService from 'common/otpservice';
 import * as strings from 'common/strings';
 import * as UserAgent from 'common/useragent';
 import emiView from 'checkoutframe/emi';
+import FeeBearerView from 'checkoutframe/feebearer';
 import SavedCardsView from 'checkoutframe/savedcards';
 import emandateView from 'checkoutframe/emandate';
 import emiOptionsView from 'checkoutframe/emioptions';
@@ -97,6 +98,8 @@ export default {
   emiScreenView,
   emiPlansView,
   SavedCardsView,
+
+  FeeBearerView,
 
   otpView,
   Curtain,
