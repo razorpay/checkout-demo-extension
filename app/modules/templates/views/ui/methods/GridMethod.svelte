4aa55d106a8c94fd89716652dcfa160bbd06be8c
Umang Galaiya <umang.galaiya@razorpay.com>
2019-04-03 00:47:45 +0530

    Create GridMethod from GridMethods

---
+++ app/modules/templates/views/ui/methods/GridMethod.svelte
@@ -0,0 +1,18 @@
+<div
+  class="payment-option item"
+  tab="{method.method}"
+
+  on:click="fire('click', event)"
+>
+
+  <label>
+    <i>{@html method.icon}</i>
+    <span class="title">{method.title}</span>
+    <!-- <span class="desc"> EMI via Credit & Debit Cards </span> -->
+  </label>
+</div>
+
+<script>
+  export default {
+  }
+</script>
--- app/modules/templates/views/ui/methods/GridMethods.svelte
+++ app/modules/templates/views/ui/methods/GridMethods.svelte
@@ -1,19 +1,22 @@
 <div id="payment-options" class="grid clear count-{methods.length}">
   {#each methods as method}
-    <div class="payment-option item" tab="{method.method}"
-      on:click="switchTab(method.method)">
-      <label>
-        <i>{@html method.icon}</i> {method.title}
-        <!-- <span class="desc"> EMI via Credit & Debit Cards </span> -->
-      </label>
-    </div>
+    <GridMethod
+      method={method}
+
+      on:click="switchTab(method.method)"
+    />
   {/each}
 </div>
 
 <script>
   import { getSession } from 'sessionmanager';
+  import Store from 'checkoutframe/store';
 
   export default {
+    components: {
+      GridMethod: './GridMethod.svelte',
+    },
+
     computed: {
       methods: ({ session, avail_methods }) => {
         const methods = session.methods;
@@ -21,6 +24,9 @@
         const icons = session.themeMeta.icons;
         let AVAIL_METHODS = _Obj.clone(avail_methods);
         let retMethods = [];
+
+        const downtimes = Store.get().downtimes || {};
+        const down = downtimes.disabled || [];
 
         if (o('theme.debit_card')) {
           AVAIL_METHODS = _Arr.remove(AVAIL_METHODS, 'card');
@@ -30,14 +36,17 @@
         _Arr.loop(AVAIL_METHODS, method => {
           if (methods[method]) {
             let icon = icons[method];
+            let isDown = _Arr.contains(down, method);
             if (/card$/.test(method)) {
               icon = icons['card'];
+              isDown = _Arr.contains(down, 'card');
             }
 
             retMethods.push({
               method,
               icon: icon,
               title: session.tab_titles[method],
+              down: isDown,
               /* TODO: add descriptions for each tab */
             });
           }
