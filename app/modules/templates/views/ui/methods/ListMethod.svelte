64424da7e7fdae23601eb7deeda3ca9a2dae3c17
Umang Galaiya <umang.galaiya@razorpay.com>
2019-04-04 03:44:04 +0530

    Introduce ListMethod

---
+++ app/modules/templates/views/ui/methods/ListMethod.svelte
@@ -0,0 +1,68 @@
+<NextOption
+  {attributes}
+  data={method}
+  {icon}
+  {classes}
+
+  on:select="selectMethod(event)"
+>
+  <span class="title">{title}</span>
+  {#if down}
+    <span class="downtime">
+      i
+      <Tooltip
+        bindTo="#list-options"
+        class="downtime-tooltip"
+        align={['top', 'right']}
+      >
+        {downMessage}
+      </Tooltip>
+    </span>
+  {/if}
+</NextOption>
+
+<script>
+
+  export default {
+    components: {
+      NextOption: 'templates/views/ui/options/NextOption.svelte',
+      Tooltip: 'templates/views/ui/Tooltip.svelte',
+    },
+
+    computed: {
+      attributes: function ({ method, down }) {
+        return {
+          tab: method,
+          down,
+        };
+      },
+
+      classes: function ({ down }) {
+        const classes = [];
+
+        if (down) {
+          classes.push('has-tooltip');
+        }
+
+        return classes;
+      }
+    },
+
+    methods: {
+      selectMethod: function (event) {
+        const {
+          down,
+          method,
+        } = this.get();
+
+        event.data = {
+          down,
+          method,
+        };
+
+        this.fire('select', event);
+      }
+    }
+  }
+
+</script>
