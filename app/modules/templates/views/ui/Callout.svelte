f8ab1c3766ef1e67d15b0d89e8cdf4c4cc7556da
Umang Galaiya <umang.galaiya@razorpay.com>
2019-04-05 00:42:26 +0530

    Introduce Callout.

---
+++ app/modules/templates/views/ui/Callout.svelte
@@ -0,0 +1,26 @@
+<div
+  class={allClasses}
+  class:drishy={visible}
+>
+  {#if showIcon}
+    <span></span>&nbsp;
+  {/if}
+  <slot></slot>
+</div>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        visible: true,
+        showIcon: true,
+        classes: [],
+        icon: '&#x2139',
+      };
+    },
+
+    computed: {
+      allClasses: ({ classes = [] }) => ['callout'].concat(classes).join(' '),
+    },
+  }
+</script>
