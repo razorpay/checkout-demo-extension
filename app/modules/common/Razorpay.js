6cb339f78f8a4c9d4caa3c31f8adbdd66f8d6ea8
Umang Galaiya <umang.galaiya@razorpay.com>
2019-05-21 12:52:18 +0530

    Send currency in preferences call

--- app/modules/common/Razorpay.js
+++ app/modules/common/Razorpay.js
@@ -203,9 +203,29 @@ export function makePrefParams(rzp) {
   if (rzp) {
     var getter = rzp.get;
     var params = {};
+
+    /**
+     * Set Key
+     */
     var key_id = getter('key');
     if (key_id) {
       params.key_id = key_id;
+    }
+
+    /**
+     * Set currency
+     */
+    var currency = getter('currency');
+    var display_currency = getter('display_currency');
+    var display_amount = getter('display_amount');
+
+    // Display currency is only valid when a display amount is present
+    if (display_currency && `${display_amount}`.length) {
+      currency = display_currency;
+    }
+
+    if (currency) {
+      params.currency = currency;
     }
 
     _Arr.loop(
