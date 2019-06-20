c5144b71dfd6c76e7d7b737e4c9b2dc2699dab9c
Umang Galaiya <umang.galaiya@razorpay.com>
2019-06-20 20:07:17 +0530

    Validate only if BIN length <= 6

--- scripts/emi/index.js
+++ scripts/emi/index.js
@@ -57,16 +57,20 @@ getOptimizedRegex(baseRegex)
     regex = new RegExp(regexString);
 
     const length = bankBins[0].length;
-    const supported = Array(Math.pow(10, length))
-      .fill(null)
-      .map((_, i) => String(i).padStart(length, '0'))
-      .filter(i => regex.test(i));
-
-    supported.sort();
-    bankBins.sort();
+    const shouldValidate = length <= 6;
 
     try {
-      chai.assert.deepEqual(supported, bankBins);
+      if (shouldValidate) {
+        const supported = Array(Math.pow(10, length))
+          .fill(null)
+          .map((_, i) => String(i).padStart(length, '0'))
+          .filter(i => regex.test(i));
+
+        supported.sort();
+        bankBins.sort();
+
+        chai.assert.deepEqual(supported, bankBins);
+      }
       console.log(regexString);
     } catch (err) {
       console.log('Generated regex does not match all BINs');
