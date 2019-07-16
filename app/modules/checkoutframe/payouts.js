6d1a14c93a768b27c119b5bd5278bdac47de8c53
Chintan Acharya <chintan.acharya@razorpay.com>
2019-07-16 21:39:40 +0530

    Remove mocked API responses

--- app/modules/checkoutframe/payouts.js
+++ app/modules/checkoutframe/payouts.js
@@ -5,50 +5,8 @@ const _headers = {
   'Content-Type': 'application/x-www-form-urlencoded',
 };
 
-const mock = false;
-
-const FETCH_MOCK_RESPONSE = {
-  id: 'cont_BSQxC46dCKUs97',
-  name: 'John Doe',
-  fund_accounts: [
-    {
-      account_type: 'bank_account',
-      id: 'fa_00000000000002',
-      bank_account: {
-        name: 'John Doe',
-        ifsc: 'SBIN0007105',
-        bank_name: 'State Bank of India',
-        account_number: '111XXXXXX4567',
-      },
-    },
-    {
-      account_type: 'vpa',
-      id: 'fa_00000000000003',
-      vpa: {
-        address: 'john*****@upi',
-      },
-    },
-  ],
-};
-
-const SAVE_MOCK_RESPONSE = {
-  id: 'fa_CuLw0crMkC5eeb',
-  account_type: 'bank_account',
-  bank_account: {
-    ifsc: 'SBIN0000012',
-    bank_name: 'State Bank of India',
-    name: 'Test',
-    account_number: 'X2345',
-  },
-};
-
 export function fetchFundAccounts(contactId) {
   const { r } = getSession();
-  if (mock) {
-    return new Promise(resolve => {
-      setTimeout(_ => resolve(FETCH_MOCK_RESPONSE), 2000);
-    });
-  }
   return new Promise((resolve, reject) =>
     fetch({
       url: makeAuthUrl(r, `contacts/${contactId}/public`),
@@ -65,11 +23,6 @@ export function fetchFundAccounts(contactId) {
 
 export function createFundAccount(fundAccount) {
   const { r } = getSession();
-  if (mock) {
-    return new Promise(resolve => {
-      setTimeout(_ => resolve(SAVE_MOCK_RESPONSE), 2000);
-    });
-  }
   return new Promise((resolve, reject) =>
     fetch({
       url: makeAuthUrl(r, 'fund_accounts/public'),
