46cf3586b921ff9891023a08f617530ccd2ec443
Chintan Acharya <chintan.acharya@razorpay.com>
2019-09-06 12:49:54 +0530

    Add padding to tab if it has message

--- app/modules/templates/tabs/Tab.svelte
+++ app/modules/templates/tabs/Tab.svelte
@@ -1,8 +1,25 @@
 {#if methodSupported}
-  <div class="tab-content showable screen" id='form-{method}' class:pad {down}>
+  <div
+    class="tab-content showable screen"
+    id='form-{method}'
+    class:pad
+    class:hasMessage
+
+    {down}
+
+    ref:tab
+  >
     <slot></slot>
   </div>
 {/if}
+
+<style>
+
+ref:tab.hasMessage {
+  padding-bottom: 56px;
+}
+
+</style>
 
 <script>
   import { getSession } from 'sessionmanager';
