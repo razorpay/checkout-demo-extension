2d6e1b0597eacd6edc3c800caea98c92e159b37f
Chintan Acharya <chintan.acharya@razorpay.com>
2019-09-06 12:50:39 +0530

    Add Downtime message component

---
+++ app/modules/templates/views/ui/Downtime.svelte
@@ -0,0 +1,29 @@
+<Callout
+  type={calloutType}
+  classes={['downtime-callout']}
+  showIcon={false}
+>
+  <b>{issuer}</b> {message}
+</Callout>
+
+<script>
+
+const DowntimeMessages = {
+  error: ' accounts are temporarily unavailable right now. Please select another bank.',
+  warning: ' accounts are experiencing low success rates.'
+};
+
+export default {
+
+  components: {
+    Callout: 'templates/views/ui/Callout.svelte'
+  },
+
+  computed: {
+    calloutType: ({ isHighSeverity }) => isHighSeverity ? 'error' : 'warning',
+    message: ({ calloutType }) => DowntimeMessages[calloutType]
+  }
+
+}
+
+</script>
