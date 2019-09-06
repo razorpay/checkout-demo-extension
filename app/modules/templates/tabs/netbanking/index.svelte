bf4a565cc52a45d03bac0f27f9e127c372a1056b
Chintan Acharya <chintan.acharya@razorpay.com>
2019-09-06 12:56:43 +0530

    Make netbanking screen scrollable when there is a downtime

--- app/modules/templates/tabs/netbanking/index.svelte
+++ app/modules/templates/tabs/netbanking/index.svelte
@@ -18,50 +18,67 @@
       {/each}
     </div>
 
-  <div class="elem-wrap pad">
-    <div id="nb-elem" class="elem select" class:invalid>
-      <i class="select-arrow"></i>
-      <div class="help">Please select a bank</div>
-      <select
-        id="bank-select"
-        name="bank"
-        required
-        class="input no-refresh no-validate"
-        pattern="[\w]+"
+    <div class="elem-wrap pad">
+      <div id="nb-elem" class="elem select" class:invalid>
+        <i class="select-arrow"></i>
+        <div class="help">Please select a bank</div>
+        <select
+          id="bank-select"
+          name="bank"
+          required
+          class="input no-refresh no-validate"
+          pattern="[\w]+"
 
-        bind:value=selectedBankCode
+          bind:value=selectedBankCode
 
-        use:focus
-        use:blur
-        use:input
+          use:focus
+          use:blur
+          use:input
+        >
+          <option value="">Select a different Bank</option>
+          {#each banksArr as bank}
+            <option value={bank.code}>{bank.name}</option>
+          {/each}
+        </select>
+      </div>
+    </div>
+
+    {#if showCorporateRadio}
+      <div class="pad"
+        ref:radioContainer
+        transition:fade="{duration: 100}"
+        class:scrollFix=selectedBankDowntime
       >
-        <option value="">Select a different Bank</option>
-        {#each banksArr as bank}
-          <option value={bank.code}>{bank.name}</option>
-        {/each}
-      </select>
-    </div>
+        <label>Complete Payment Using</label>
+        <div class="input-radio">
+          <input
+            type="radio"
+            id="nb_type_retail"
+            value="retail"
+            checked={!corporateSelected}
+            on:click=setRetailOption()
+          >
+          <label for="nb_type_retail">
+            <div class="radio-display"></div>
+            <div class="label-content">Retail</div>
+          </label>
+        </div>
+        <div class="input-radio">
+          <input
+            type="radio"
+            id="nb_type_corporate"
+            value="corporate"
+            checked={corporateSelected}
+            on:click=setCorporateOption()
+          >
+          <label for="nb_type_corporate">
+            <div class="radio-display"></div>
+            <div class="label-content">Corporate</div>
+          </label>
+        </div>
+      </div>
+    {/if}
   </div>
-
-  {#if showCorporateRadio}
-    <div class="pad" ref:radioContainer transition:fade="{duration: 100}">
-      <label>Complete Payment Using</label>
-      <div class="input-radio">
-        <input type="radio" id="nb_type_retail" value="retail" checked={!corporateSelected} on:click=setRetailOption()>
-        <label for="nb_type_retail">
-          <div class="radio-display"></div>
-          <div class="label-content">Retail</div>
-        </label>
-      </div>
-      <div class="input-radio">
-        <input type="radio" id="nb_type_corporate" value="corporate" checked={corporateSelected} on:click=setCorporateOption()>
-        <label for="nb_type_corporate">
-          <div class="radio-display"></div>
-          <div class="label-content">Corporate</div>
-        </label>
-      </div>
-    </div>
-  {/if}
 
   <!-- Show recurring message for recurring payments -->
   {#if recurring}
@@ -71,21 +88,34 @@
   {/if}
 
   <!-- Show downtime message if the selected bank is down -->
-  {#if showDown}
-    <div class="down">
-      <svg xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24">
-        <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.351 6.493c-.08-.801.55-1.493 1.351-1.493s1.431.692 1.351 1.493l-.801 8.01c-.029.282-.266.497-.55.497s-.521-.215-.55-.498l-.801-8.009zm1.351 12.757c-.69 0-1.25-.56-1.25-1.25s.56-1.25 1.25-1.25 1.25.56 1.25 1.25-.56 1.25-1.25 1.25z" />
-      </svg>
-      <span class="text">{banks[selectedBankCode]}</span> is currently facing issues.
-    </div>
+  {#if selectedBankDowntime}
+    <Downtime
+      issuer={banks[selectedBankCode]}
+      isHighSeverity={isHighSeverityDowntime}
+    />
   {/if}
 
 </Tab>
 
 <style>
 
+#netb-banks {
+  overflow: hidden;
+}
+
+ref:screenContent {
+  height: 100%;
+  overflow: auto;
+}
+
 ref:radioContainer {
   margin-top: -6px;
+  margin-bottom: 18px;
+}
+
+/* Add extra space at the bottom to prevent callout message from overlapping radios */
+ref:radioContainer.scrollFix {
+  margin-bottom: 36px;
 }
 
 .input-radio:first-of-type {
@@ -104,6 +134,9 @@ import { iPhone } from 'common/useragent';
 import DowntimesStore from 'checkoutstore/downtimes';
 import { groupNetbankingDowntimesByBank } from 'checkoutframe/downtimes';
 
+import { getPreferredBanks } from 'common/bank';
+import { getSession } from 'sessionmanager';
+
 import * as InputActions from 'actions/input';
 
 import {
@@ -120,7 +153,8 @@ export default {
   components: {
     Tab: 'templates/tabs/Tab.svelte',
     GridItem: 'templates/tabs/netbanking/GridItem.svelte',
-    Callout: 'templates/views/ui/Callout.svelte'
+    Callout: 'templates/views/ui/Callout.svelte',
+    Downtime: 'templates/views/ui/Downtime.svelte'
   },
 
   data() {
@@ -129,7 +163,8 @@ export default {
       showCorporateRadio: false,
       corporateOption: '',
       retailOption: '',
-      downtimes: {}
+      downtimes: {},
+      session: getSession()
     }
   },
 
@@ -144,6 +179,9 @@ export default {
       if (corporateOption) {
         this.set({ selectedBankCode: corporateOption });
       }
+    },
+    onSwitch() {
+      this.hidePayButtonIfSevereDowntime();
     },
     setRetailOption() {
       const { selectedBankCode, banks } = this.get();
@@ -161,6 +199,10 @@ export default {
     },
     deselectBank() {
       this.set({ selectedBankCode: '' });
+    },
+    hidePayButtonIfSevereDowntime() {
+      const { isHighSeverityDowntime, session } = this.get();
+      session.body.toggleClass('sub', !isHighSeverityDowntime);
     }
   },
 
@@ -178,14 +220,15 @@ export default {
             bank: selectedBankCode,
           },
         });
+
         this.fire('bankSelected', { code: selectedBankCode });
+        this.hidePayButtonIfSevereDowntime();
       }
     }
   },
 
   oncreate() {
     const { banksArr } = this.get();
-
 
     const downtimes = DowntimesStore.get();
     const netbankingDowntimes = groupNetbankingDowntimesByBank(downtimes.netbanking);
@@ -196,8 +239,6 @@ export default {
     if (banksArr.length === 1) {
       this.setSelectedBank(banksArr[0].code);
     }
-
-
   },
 
   actions: {
@@ -219,10 +260,19 @@ export default {
     banksArr: ({ banks, downtimes }) => _Obj.entries(banks)
         .map((entry) => ({ code: entry[0], name: entry[1], downtime: downtimes[entry[0]] })),
 
-    // // Do not show invalid for emandate as the screen changes as soon as bank is selected.
-    invalid: ({ method, selectedBankCode }) => !selectedBankCode,
+    // Do not show invalid for emandate as the screen changes as soon as bank is selected. // TODO: Fix this
+    invalid: ({ method, selectedBankCode }) => method !== 'emandate' && !selectedBankCode,
 
     showDown: ({ down, selectedBankCode }) => down && down.indexOf(selectedBankCode) !== -1,
+
+    selectedBankDowntime: ({ downtimes, selectedBankCode }) => downtimes && downtimes[selectedBankCode],
+
+    netbanks: ({ banks, bankOptions, maxGridCount }) => getPreferredBanks(banks, bankOptions).slice(0, maxGridCount),
+
+    // Hide pay button only if the selected bank's downtime severity is high or scheduled.
+    // TODO: figure out a better way of doing this. (i.e. return computed object from downtime module)
+    isHighSeverityDowntime: ({ selectedBankDowntime }) => selectedBankDowntime
+         && _Arr.contains(['high', 'scheduled'], selectedBankDowntime.severity)
 
   }
 
