0c0b583d245c6f1bf9a756654699fb8b8197df6e
Umang Galaiya <umang.galaiya@razorpay.com>
2019-09-09 19:52:12 +0530

    Revert padding changes to Screen

--- app/modules/templates/layouts/Screen.svelte
+++ app/modules/templates/layouts/Screen.svelte
@@ -1,29 +1,14 @@
-<div ref:content class:screen-pad="pad" class:screen-pad-twice="padTwice">
+<div ref:content>
   <slot></slot>
 </div>
 
 <style>
 
   ref:content {
-    &.screen-pad {
-      padding: 0 12px 12px 12px;
-      &.screen-pad-twice {
-        padding-left: 24px;
-        padding-right: 24px;
-      }
-    }
+    padding: 0 12px 12px 12px;
     height: 100%;
     overflow: auto;
     box-sizing: border-box;
   }
 
 </style>
-
-<script>
-  export default {
-    data: () => ({
-      pad: true,
-      padTwice: false,
-    })
-  }
-</script>
