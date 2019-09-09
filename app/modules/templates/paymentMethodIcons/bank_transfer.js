2a65e3500582825ff1653571d7fa20d10664211c
Kevin Jose <kevin.jose@razorpay.com>
2019-09-09 13:52:23 +0530

    fix for broken bank_transfer svg

--- app/modules/templates/paymentMethodIcons/bank_transfer.js
+++ app/modules/templates/paymentMethodIcons/bank_transfer.js
@@ -1,5 +1,5 @@
 export default (foregroundColor, backgroundColor) =>
-  `<svg viewBox="0 0 30 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+  `<svg viewBox="0 0 32 34" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M7.288 17.143a1.071 1.071 0 1 1 2.143 0V22.5a1.071 1.071 0 0 1-2.143 0v-5.357zM13.716 17.143a1.071 1.071 0 1 1 2.143 0V22.5a1.071 1.071 0 0 1-2.143 0v-5.357zM20.145 17.143a1.071 1.071 0 1 1 2.143 0V22.5a1.071 1.071 0 0 1-2.143 0v-5.357zM4.073 27.857a1.071 1.071 0 0 1 0-2.143h21.429a1.071 1.071 0 0 1 0 2.143H4.073z" fill="${foregroundColor}"/>
     <path d="M14.49 5.222L25.38 13H3.21L14.49 5.222z" stroke="${foregroundColor}" stroke-width="2"/>
     <circle cx="23.5" cy="10.5" r="7.6" fill="#fff" stroke="${backgroundColor}" stroke-width="1.8"/>
