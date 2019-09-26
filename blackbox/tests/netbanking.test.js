d07069fad6c561fa2deda1bddb44ebd893121640
Sakshi Jain <sakshi.jain@razorpay.com>
2019-09-26 19:09:22 +0530

    WIP - added test case fgor netbanking with partial payment

--- blackbox/tests/netbanking.test.js
+++ blackbox/tests/netbanking.test.js
@@ -69,4 +69,62 @@ describe('Netbanking tests', () => {
     );
     expect(messageText).toEqual(expectedErrorMeassage);
   });
+  test('perform netbanking transaction with partial payments', async () => {
+    const options = {
+      key: 'rzp_test_1DP5mmOlF5G5ag',
+      amount: 20000,
+      personalization: false,
+    };
+    const preferences = makePreferences({
+      order: {
+        amount: 20000,
+        amount_due: 20000,
+        amount_paid: 0,
+        currency: 'INR',
+        first_payment_min_amount: null,
+        partial_payment: true,
+      },
+    });
+    const context = await openCheckout({ page, options, preferences });
+    await page.type('[name=contact]', '9999988888');
+    await page.type('[name=email]', 'pro@rzp.com');
+
+    const makePartialCheckBox = await page.waitForSelector('.checkbox');
+    await makePartialCheckBox.click();
+    const amountValue = await page.waitForSelector('#amount-value');
+    await amountValue.type('100');
+    const nextButton = await page.waitForSelector('#next-button');
+    await nextButton.click();
+    await delay(500);
+
+    await page.click('[tab=netbanking]');
+    await page.select('#bank-select', 'SBIN');
+    const orignalAmount = await page.waitForSelector('.original-amount');
+    const otpAmount = await page.evaluate(
+      orignalAmount => orignalAmount.textContent,
+      orignalAmount
+    );
+    expect(otpAmount).toEqual('₹ 100');
+
+    await delay(200);
+    await page.click('#footer');
+
+    // await handleFeeBearer(context, page);
+
+    context.popup();
+
+    let req = await context.expectRequest();
+    expect(req.method).toEqual('POST');
+
+    const expectedErrorMeassage = 'some error';
+    await context.respondJSON({
+      error: { description: expectedErrorMeassage },
+    });
+    const messageDiv = await page.waitForSelector('#fd-t');
+    const messageText = await page.evaluate(
+      messageDiv => messageDiv.textContent,
+      messageDiv
+    );
+    expect(messageText).toEqual(expectedErrorMeassage);
+  });
 });
