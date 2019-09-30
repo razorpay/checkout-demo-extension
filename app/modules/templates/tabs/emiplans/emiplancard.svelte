71e31af163573279c4ad3cf0cb7baa119f9565a5
Chintan Acharya <chintan.acharya@razorpay.com>
2019-09-30 19:27:17 +0530

    Wrap condition inside brackets

--- app/modules/templates/tabs/emiplans/emiplancard.svelte
+++ app/modules/templates/tabs/emiplans/emiplancard.svelte
@@ -48,7 +48,7 @@
           );
         }
       },
-      noCostEmi: ({ plan, provider }) => plan.subvention === 'merchant' || provider === 'zestmoney' && plan.duration === 3,
+      noCostEmi: ({ plan, provider }) => plan.subvention === 'merchant' || (provider === 'zestmoney' && plan.duration === 3),
       badge: ({ noCostEmi }) => noCostEmi ? 'No cost EMI' : false,
       isCardEmi: ({ provider }) => !provider,
       showInterest: ({ provider, isCardEmi }) => !isCardEmi || !_Arr.contains(['zestmoney', 'earlysalary'], provider),
