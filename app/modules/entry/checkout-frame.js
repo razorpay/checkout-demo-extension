d7142d3b624b854847c122a0a12c095f8db08841
Umang Galaiya <umang.galaiya@razorpay.com>
2019-10-17 10:47:51 +0530

    Add canary to Track.props

--- app/modules/entry/checkout-frame.js
+++ app/modules/entry/checkout-frame.js
@@ -5,4 +5,9 @@ import Track from 'tracker';
 import 'lib/polyfill';
 
 Track.props.library = 'checkoutjs';
+
+if (Boolean(_.query2obj(global.location.search.replace('?', '')).canary)) {
+  Track.props.env = 'canary';
+}
+
 RazorpayConfig.api = _Doc.resolveUrl(RazorpayConfig.frameApi);
