cab296d720424155b2522f121f0016ad9ca1bf82
Umang Galaiya <umang.galaiya@razorpay.com>
2019-10-22 21:10:38 +0530

    Use highlightColor for LinkButton's border

--- app/modules/components/LinkButton.svelte
+++ app/modules/components/LinkButton.svelte
@@ -7,10 +7,9 @@
     padding: 3px 1px;
     font-size: 12px;
     border-bottom: 1px solid;
-    border-color: #528ff0;
   }
 </style>
 
-<button {type} {...$$props} on:click>
+<button {type} {...$$props} on:click class="hc_border">
   <slot />
 </button>
