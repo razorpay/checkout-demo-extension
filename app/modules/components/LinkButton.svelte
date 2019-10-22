5c957b947e7d8e02a0d1bb364ed05a5a7cab9b83
Umang Galaiya <umang.galaiya@razorpay.com>
2019-10-22 18:52:33 +0530

    Use buttons instead of anchors on OTP screen

---
+++ app/modules/components/LinkButton.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let type = 'button';
+</script>
+
+<style>
+  button {
+    padding: 3px 1px;
+    font-size: 12px;
+    border-bottom: 1px solid;
+    border-color: #528ff0;
+  }
+</style>
+
+<button {type} {...$$props} on:click>
+  <slot />
+</button>
