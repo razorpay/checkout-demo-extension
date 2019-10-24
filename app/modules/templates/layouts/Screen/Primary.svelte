3e9cf8b538406a704ec20dc73b2b1760818d28e4
Umang Galaiya <umang.galaiya@razorpay.com>
2019-10-24 17:24:20 +0530

    Move padding from Screen to PrimaryScreen

--- app/modules/templates/layouts/Screen/Primary.svelte
+++ app/modules/templates/layouts/Screen/Primary.svelte
@@ -1,12 +1,18 @@
+<script>
+  export let pad = true;
+</script>
+
 <style>
   .screen-primary {
     flex-grow: 1;
     overflow: auto;
+  }
 
+  .screen-primary.pad {
     padding: 0 12px 12px;
   }
 </style>
 
-<div class="screen-primary">
+<div class="screen-primary" class:pad>
   <slot />
 </div>
--- app/modules/templates/layouts/Screen/index.svelte
+++ app/modules/templates/layouts/Screen/index.svelte
@@ -1,19 +1,16 @@
-<script>
-  export let pad = true;
-</script>
-
 <style>
   div {
     height: 100%;
     overflow: auto;
     box-sizing: border-box;
-  }
 
-  .pad {
-    padding: 0 12px 12px 12px;
+    padding: 0;
+
+    display: flex;
+    flex-direction: column;
   }
 </style>
 
-<div class:pad>
+<div>
   <slot />
 </div>
