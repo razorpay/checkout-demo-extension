b97ed8458a7e2a7dc102c632b9f9223aa1c4f9b3
Umang Galaiya <umang.galaiya@razorpay.com>
2019-10-24 17:19:17 +0530

    Introduce Primary and Bottom screen components

---
+++ app/modules/templates/layouts/Screen/Bottom.svelte
@@ -0,0 +1,9 @@
+<style>
+  .screen-bottom {
+    flex-shrink: 0;
+  }
+</style>
+
+<div class="screen-bottom">
+  <slot />
+</div>
---
+++ app/modules/templates/layouts/Screen/Primary.svelte
@@ -0,0 +1,12 @@
+<style>
+  .screen-primary {
+    flex-grow: 1;
+    overflow: auto;
+
+    padding: 0 12px 12px;
+  }
+</style>
+
+<div class="screen-primary">
+  <slot />
+</div>
