95886ffb7693db5bf0fe5d9671bfa3e6e0172298
Umang Galaiya <umang.galaiya@razorpay.com>
2019-10-25 15:29:28 +0530

    Move p13n utils to a separate file

--- app/modules/checkoutframe/personalization/index.js
+++ app/modules/checkoutframe/personalization/index.js
@@ -5,6 +5,7 @@ import Track from 'tracker';
 import Analytics from 'analytics';
 import { isMobile } from 'common/useragent';
 import { filterInstruments } from './filters';
+import { hashFnv32a, set, get } from './utils';
 
 const PREFERRED_INSTRUMENTS = 'rzp_preffered_instruments';
 
@@ -20,48 +21,6 @@ const INSTRUMENT_PROPS = {
   netbanking: 'bank',
   upi: ['_[flow]', 'vpa', 'upi_app', '_[upiqr]'],
   paypal: [],
-};
-
-const set = (key, data) => {
-  try {
-    global.localStorage.setItem(key, data);
-  } catch (e) {}
-};
-
-const get = key => {
-  try {
-    return global.localStorage.getItem(key);
-  } catch (e) {}
-
-  return '[]';
-};
-
-/**
- * Calculate a 32 bit FNV-1a hash
- * Found here: https://gist.github.com/vaiorabbit/5657561
- * Ref.: http://isthe.com/chongo/tech/comp/fnv/
- *
- * @param {string} str the input value
- * @param {boolean} [asString] set to true to return the hash value as
- *     8-digit hex string instead of an integer
- * @param {integer} [seed] optionally pass the hash of the previous chunk
- * @returns {integer | string}
- */
-const hashFnv32a = (str = '', asString = true, seed = 0xdeadc0de) => {
-  let i,
-    l,
-    hval = seed;
-
-  for (i = 0, l = str.length; i < l; i++) {
-    hval ^= str.charCodeAt(i);
-    hval +=
-      (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
-  }
-  if (asString) {
-    // Convert to 8 digit hex string
-    return ('0000000' + (hval >>> 0).toString(16)).substr(-8);
-  }
-  return hval >>> 0;
 };
 
 let currentUid = null;
---
+++ app/modules/checkoutframe/personalization/utils.js
@@ -0,0 +1,41 @@
+/**
+ * Calculate a 32 bit FNV-1a hash
+ * Found here: https://gist.github.com/vaiorabbit/5657561
+ * Ref.: http://isthe.com/chongo/tech/comp/fnv/
+ *
+ * @param {string} str the input value
+ * @param {boolean} [asString] set to true to return the hash value as
+ *     8-digit hex string instead of an integer
+ * @param {integer} [seed] optionally pass the hash of the previous chunk
+ * @returns {integer | string}
+ */
+export function hashFnv32a(str = '', asString = true, seed = 0xdeadc0de) {
+  let i,
+    l,
+    hval = seed;
+
+  for (i = 0, l = str.length; i < l; i++) {
+    hval ^= str.charCodeAt(i);
+    hval +=
+      (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+  }
+  if (asString) {
+    // Convert to 8 digit hex string
+    return ('0000000' + (hval >>> 0).toString(16)).substr(-8);
+  }
+  return hval >>> 0;
+}
+
+export function set(key, data) {
+  try {
+    global.localStorage.setItem(key, data);
+  } catch (e) {}
+}
+
+export function get(key) {
+  try {
+    return global.localStorage.getItem(key);
+  } catch (e) {}
+
+  return '[]';
+}
