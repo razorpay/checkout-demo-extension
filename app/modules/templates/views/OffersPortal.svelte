fdd1711dcef65deac3d8e7af9707731a447adb54
Umang Galaiya <umang.galaiya@razorpay.com>
2019-11-09 20:26:54 +0530

    Add Offers portal

---
+++ app/modules/templates/views/OffersPortal.svelte
@@ -0,0 +1,10 @@
+<style>
+  :global(.has-offers) .offers-portal {
+    display: block !important;
+    position: unset;
+    transform: none !important;
+    bottom: unset;
+  }
+</style>
+
+<div class="offers-container offers-portal" />
