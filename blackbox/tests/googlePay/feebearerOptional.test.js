ac6d342ff272edab6c144f5e09e042dddca2816a
Baala <baalamurugan.sc@razorpay.com>
2019-11-12 13:08:54 +0530

    GooglePay base test case

---
+++ blackbox/tests/googlePay/feebearerOptional.test.js
@@ -0,0 +1,46 @@
+const { openSdkCheckout } = require('../../actions/checkout-sdk');
+const { makePreferences } = require('../../actions/preferences');
+const {
+  assertHomePage,
+  fillUserDetails,
+  assertPaymentMethods,
+  selectPaymentMethod,
+  submit,
+  respondAndVerifyIntentRequest,
+  handleFeeBearer,
+  selectUPIApp,
+} = require('../../actions/common');
+
+describe('Basic GooglePay payment', () => {
+  test('Perform GooglePay transaction with feebearer and contact optional enabled', async () => {
+    const options = {
+      key: 'rzp_test_1DP5mmOlF5G5ag',
+      amount: 60000,
+      personalization: false,
+    };
+    const preferences = makePreferences({
+      fee_bearer: true,
+      optional: ['contact'],
+    });
+    preferences.methods.upi = true;
+    const context = await openSdkCheckout({
+      page,
+      options,
+      preferences,
+      apps: [
+        {
+          package_name: 'com.google.android.apps.nbu.paisa.user',
+          app_name: 'Google Pay (Tez)',
+        },
+      ],
+    });
+    await assertHomePage(context, true, true);
+    await fillUserDetails(context, false);
+    await assertPaymentMethods(context);
+    await selectPaymentMethod(context, 'upi');
+    await selectUPIApp(context, '1');
+    await submit(context);
+    await handleFeeBearer(context, page);
+    await respondAndVerifyIntentRequest(context);
+  });
+});
