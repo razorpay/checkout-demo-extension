4bff9d6cec84382f7b32f5cf0c65d325a0756506
Umang Galaiya <umang.galaiya@razorpay.com>
2019-11-12 22:43:08 +0530

    Move SlottedOption

---
+++ app/modules/templates/views/ui/options/SlottedOption.svelte
@@ -0,0 +1,32 @@
+<script>
+  // UI imports
+  import Stack from 'templates/layouts/Stack.svelte';
+
+  // Props
+  export let className = '';
+</script>
+
+<style>
+  button {
+    background: #ffffff;
+    border: 1px solid #e6e7e8;
+    display: block;
+    width: 100%;
+    text-align: left;
+    transition: border-color 0.15s linear;
+  }
+
+  div {
+    flex-grow: 1;
+  }
+</style>
+
+<button type="button" class={className}>
+  <Stack horizontal>
+    <slot name="icon" />
+    <div>
+      <slot name="title" />
+      <slot name="subtitle" />
+    </div>
+  </Stack>
+</button>
