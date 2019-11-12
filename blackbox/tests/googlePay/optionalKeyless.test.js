e2c503e2ec66f9e1bdb2065a8612a3d34fcf3460
Baala <baalamurugan.sc@razorpay.com>
2019-11-12 13:08:54 +0530

    GooglePay base test case

---
+++ blackbox/tests/googlePay/optionalKeyless.test.js
@@ -0,0 +1,41 @@
+const { openSdkCheckout } = require('../../actions/checkout-sdk');
+const { makePreferences } = require('../../actions/preferences');
+const {
+  assertHomePage,
+  fillUserDetails,
+  assertPaymentMethods,
+  selectPaymentMethod,
+  submit,
+  respondAndVerifyIntentRequest,
+  selectUPIApp,
+} = require('../../actions/common');
+
+describe('Basic GooglePay payment', () => {
+  test('Perform keyless GooglePay transaction with contact optional', async () => {
+    const options = {
+      order_id: 'rzp_test_1DP5mmOlF5G5ag',
+      amount: 200,
+      personalization: false,
+    };
+    const preferences = makePreferences({ optional: ['contact'] });
+    preferences.methods.upi = true;
+    const context = await openSdkCheckout({
+      page,
+      options,
+      preferences,
+      apps: [
+        {
+          package_name: 'com.google.android.apps.nbu.paisa.user',
+          app_name: 'Google Pay (Tez)',
+        },
+      ],
+    });
+    await assertHomePage(context, true, true);
+    await fillUserDetails(context, false);
+    await assertPaymentMethods(context);
+    await selectPaymentMethod(context, 'upi');
+    await selectUPIApp(context, '1');
+    await submit(context);
+    await respondAndVerifyIntentRequest(context);
+  });
+});
