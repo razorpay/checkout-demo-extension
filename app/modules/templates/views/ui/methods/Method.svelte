c634b83f2571ff2e1a2145f5d7072a4d11af163e
Umang Galaiya <umang.galaiya@razorpay.com>
2019-11-12 22:44:22 +0530

    Add Method component

---
+++ app/modules/templates/views/ui/methods/Method.svelte
@@ -0,0 +1,69 @@
+<script>
+  // Props
+  export let method = null;
+  export let icon = null;
+  export let title = null;
+  export let subtitle = null;
+
+  // UI imports
+  import SlottedOption from 'templates/views/ui/option/SlottedOption.svelte';
+
+  // Utils imports
+  import { getSession } from 'sessionmanager';
+  import {
+    getMethodNameForPaymentOption,
+    getMethodDescription,
+  } from 'checkoutframe/paymentmethods';
+
+  const session = getSession();
+
+  // Items to display
+  const _icon = icon || session.themeMeta.icons[method];
+  const _title = title || getMethodNameForPaymentOption(method, { session });
+  const _subtitle = subtitle || getMethodDescription(method, { session });
+</script>
+
+<style>
+  /* Container styles */
+  :global(.new-method) {
+    padding: 16px;
+  }
+
+  /* Icon styles */
+  i {
+    display: flex;
+    margin-right: 16px;
+    width: 24px;
+    text-align: center;
+  }
+
+  i :global(svg) {
+    height: 24px;
+    width: auto;
+  }
+
+  /* Content styles */
+  div[slot='title'] {
+    margin: 0;
+    font-weight: bold;
+    font-size: 1rem;
+    line-height: 1.1rem;
+    color: #4f4f4f;
+    text-transform: none;
+  }
+
+  div[slot='subtitle'] {
+    margin: 4px 0 0 0;
+    font-size: 0.8rem;
+    line-height: 1rem;
+    color: #828282;
+  }
+</style>
+
+<SlottedOption className="new-method">
+  <i slot="icon">
+    {@html _icon}
+  </i>
+  <div slot="title">{_title}</div>
+  <div slot="subtitle">{_subtitle}</div>
+</SlottedOption>
