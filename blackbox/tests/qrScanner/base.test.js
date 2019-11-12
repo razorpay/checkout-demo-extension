be14f4522b518c02732ef95bdd9b38c26d3b2943
Baala <baalamurugan.sc@razorpay.com>
2019-11-12 16:42:32 +0530

    QR Code base test case

---
+++ blackbox/tests/qrScanner/base.test.js
@@ -0,0 +1,36 @@
+const { makePreferences } = require('../../actions/preferences');
+const { openCheckout } = require('../../actions/checkout');
+
+const {
+  assertHomePage,
+  fillUserDetails,
+  assertPaymentMethods,
+  selectPaymentMethod,
+  selectUPIApp,
+  respondToUPIPaymentStatus,
+  respondToUPIAjax,
+} = require('../../actions/common');
+
+describe('Basic QR Code payment', () => {
+  test('Perform QR Code transaction', async () => {
+    const options = {
+      key: 'rzp_test_1DP5mmOlF5G5ag',
+      amount: 20000,
+      personalization: false,
+    };
+    const preferences = makePreferences();
+    preferences.methods.upi = true;
+    const context = await openCheckout({
+      page,
+      options,
+      preferences,
+    });
+    await assertHomePage(context, true, true);
+    await fillUserDetails(context, true);
+    await assertPaymentMethods(context);
+    await selectPaymentMethod(context, 'upi');
+    await selectUPIApp(context, '1');
+    await respondToUPIAjax(context);
+    await respondToUPIPaymentStatus(context);
+  });
+});
---
+++ blackbox/tests/qrScanner/feebearer.test.js
@@ -0,0 +1,37 @@
+const { openCheckout } = require('../../actions/checkout');
+const { makePreferences } = require('../../actions/preferences');
+const {
+  assertHomePage,
+  fillUserDetails,
+  assertPaymentMethods,
+  selectPaymentMethod,
+  handleFeeBearer,
+  selectUPIApp,
+  respondToUPIAjax,
+  respondToUPIPaymentStatus,
+} = require('../../actions/common');
+
+describe('Basic QR Code Payment', () => {
+  test('Perform QR Code transaction with feebearer enabled', async () => {
+    const options = {
+      key: 'rzp_test_1DP5mmOlF5G5ag',
+      amount: 60000,
+      personalization: false,
+    };
+    const preferences = makePreferences({ fee_bearer: true });
+    preferences.methods.upi = true;
+    const context = await openCheckout({
+      page,
+      options,
+      preferences,
+    });
+    await assertHomePage(context, true, true);
+    await fillUserDetails(context, true);
+    await assertPaymentMethods(context);
+    await selectPaymentMethod(context, 'upi');
+    await selectUPIApp(context, '1');
+    await handleFeeBearer(context, page);
+    await respondToUPIAjax(context);
+    await respondToUPIPaymentStatus(context);
+  });
+});
