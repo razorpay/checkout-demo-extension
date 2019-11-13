5c2df9aac411e0a3f4c5dede31c706e0feb5fec3
Umang Galaiya <umang.galaiya@razorpay.com>
2019-11-13 14:52:32 +0530

    Add Slotted/RadioOption

---
+++ app/modules/templates/views/ui/options/Slotted/RadioOption.svelte
@@ -0,0 +1,56 @@
+<script>
+  // UI imports
+  import Stack from 'templates/layouts/Stack.svelte';
+  import Radio from 'templates/views/ui/Radio.svelte';
+
+  // Props
+  export let className = '';
+  export let name;
+  export let value;
+  export let selected = false;
+  export let reverse = false;
+  export let radio = true;
+
+  let radioClasses;
+  $: {
+    const _classes = ['slotted'];
+
+    if (!radio) {
+      _classes.push('invisible');
+    }
+
+    radioClasses = _classes.join(' ');
+  }
+</script>
+
+<style>
+  button {
+    background: #ffffff;
+    border: 1px solid #e6e7e8;
+    display: block;
+    width: 100%;
+    text-align: start;
+    transition: border-color 0.15s linear;
+  }
+
+  div {
+    flex-grow: 1;
+  }
+
+  button :global(.input-radio.slotted .radio-display) {
+    position: unset;
+    top: unset;
+  }
+</style>
+
+<button class={className} class:selected on:click>
+  <Stack horizontal {reverse}>
+    <slot name="icon" />
+    <div>
+      <slot name="title" />
+      <slot name="subtitle" />
+    </div>
+    <Radio {name} {value} checked={selected} classes={radioClasses} />
+    <slot name="extra" />
+  </Stack>
+</button>
