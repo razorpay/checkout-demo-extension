4482abc593851a80aa8c5e4568da2f2b88a4c48a
saranshgupta1995 <saranshgupta1995@gmail.com>
2019-11-14 16:56:50 +0530

    added options to support keyless

--- blackbox/actions/index.js
+++ blackbox/actions/index.js
@@ -2,27 +2,57 @@ const { makePreferences, makePreferencesLogged } = require('./preferences');
 const { makeOptions } = require('./options');
 const { openCheckout } = require('./checkout');
 
+const getDataUpdatedForKeyless = d => {
+  return {
+    ...d,
+    title: d.title + ' - keyless',
+    options: {
+      ...d.options,
+      ...{
+        order_id: 'rzp_test_1DP5mmOlF5G5ag',
+        amount: 200,
+        personalization: false,
+      },
+    },
+  };
+};
+
 /**
  *
  * @param {String} title Title of the test
- * @param {Object} { preferences = {}, loggedIn = true, anon = true}
+ * @param {Object} { preferences = {}, options = {}, loggedIn = true, anon = true}
  */
 const getTestData = (
   title,
-  { preferences = {}, loggedIn = true, anon = true } = {}
+  {
+    preferences = {},
+    options = {},
+    loggedIn = true,
+    anon = true,
+    keyless = true,
+  } = {}
 ) => {
   let tests = [];
+  prefernecesLoggedIn = makePreferencesLogged(preferences);
+  preferences = makePreferences(preferences);
+  options = makeOptions(options);
   if (loggedIn) {
-    tests.push({
-      preferences: makePreferencesLogged(preferences),
+    let loggedInData = {
+      preferences,
       title: title + ' - when logged in',
-    });
+      options,
+    };
+    tests.push(loggedInData);
+    if (keyless) tests.push(getDataUpdatedForKeyless(loggedInData));
   }
   if (anon) {
-    tests.push({
-      preferences: makePreferences(preferences),
+    let anonymousUserData = {
+      preferences: prefernecesLoggedIn,
       title: title + ' - with anonymous user',
-    });
+      options,
+    };
+    tests.push(anonymousUserData);
+    if (keyless) tests.push(getDataUpdatedForKeyless(anonymousUserData));
   }
   return tests;
 };
