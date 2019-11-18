6954fb86d19cd9e27d7ce8e5182fbfd36d14f1eb
Sakshi Jain <sakshi.jain@razorpay.com>
2019-11-18 17:26:49 +0530

    Reduced number of workers

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,5 @@ module.exports = {
   globalTeardown: './blackbox/teardown',
   testEnvironment: './blackbox/jest-environment',
   testTimeout: 60 * 1000 * (isProd ? 1 : 10),
-  maxWorkers: 10,
+  maxWorkers: 4,
 };
