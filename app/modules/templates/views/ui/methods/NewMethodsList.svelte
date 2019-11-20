f089d40ef9f0ef025dfd99b32a1bd654c161a578
Umang Galaiya <umang.galaiya@razorpay.com>
2019-11-20 14:24:34 +0530

    Remove dummy instruments and use customer directly

--- app/modules/templates/views/ui/methods/NewMethodsList.svelte
+++ app/modules/templates/views/ui/methods/NewMethodsList.svelte
@@ -4,12 +4,17 @@
   import CardInstrument from 'templates/views/ui/Personalization/CardInstrument.svelte';
   import Instrument from 'templates/views/ui/Personalization/Instrument.svelte';
 
+  // Utils imports
   import { AVAILABLE_METHODS } from 'common/constants';
   import { getSession } from 'sessionmanager';
   import { isMobile } from 'common/useragent';
   import { doesAppExist } from 'common/upi';
   import Analytics from 'analytics';
   import * as AnalyticsTypes from 'analytics-types';
+  import { getInstrumentsForCustomer } from 'checkoutframe/personalization';
+
+  // Store
+  import { contact } from 'checkoutstore/screens/home';
 
   const session = getSession();
   let visibleMethods = [];
@@ -50,190 +55,11 @@
   }
 
   function setInstruments() {
-    let instruments = [
-      {
-        wallet: 'freecharge',
-        method: 'wallet',
-        timestamp: 1566990934739,
-        success: false,
-        frequency: 4,
-        id: 'BXFgSdmlXqscTs',
-      },
-      {
-        method: 'card',
-        token_id: 'token_9AT28Pxxe0Npi9',
-        type: 'credit',
-        issuer: 'ICIC',
-        network: 'MasterCard',
-        timestamp: 1571742071478,
-        success: true,
-        frequency: 58,
-        id: 'BYn4088DoGT4ND',
-      },
-      {
-        method: 'card',
-        token_id: 'token_69ATx28Pxxe0Npi9',
-        type: 'credit',
-        issuer: 'ICIC',
-        network: 'RuPay',
-        timestamp: 1571742071478,
-        success: true,
-        frequency: 58,
-        id: '6BYn4088xDoGT4ND',
-      },
-      {
-        method: 'card',
-        token_id: 'token_59ATx28Pxxe0Npi9',
-        type: 'credit',
-        issuer: 'ICIC',
-        network: 'American Express',
-        timestamp: 1571742071478,
-        success: true,
-        frequency: 58,
-        id: '5BYn4088xDoGT4ND',
-      },
-      {
-        method: 'card',
-        token_id: 'token_49ATx28Pxxe0Npi9',
-        type: 'credit',
-        issuer: 'ICIC',
-        network: 'Diners Club',
-        timestamp: 1571742071478,
-        success: true,
-        frequency: 58,
-        id: '4BYn4088xDoGT4ND',
-      },
-      {
-        method: 'card',
-        token_id: 'token_39ATx28Pxxe0Npi9',
-        type: 'credit',
-        issuer: 'ICIC',
-        network: 'Visa',
-        timestamp: 1571742071478,
-        success: true,
-        frequency: 58,
-        id: '3BYn4088xDoGT4ND',
-      },
-      {
-        method: 'card',
-        token_id: 'token_29ATx28Pxxe0Npi9',
-        type: 'credit',
-        issuer: 'ICIC',
-        network: 'Bajaj Finserv',
-        timestamp: 1571742071478,
-        success: true,
-        frequency: 58,
-        id: '2BYn4088xDoGT4ND',
-      },
-      {
-        method: 'card',
-        token_id: 'token_19ATx28Pxxe0Npi9',
-        type: 'credit',
-        issuer: 'ICIC',
-        network: 'unknown',
-        timestamp: 1571742071478,
-        success: true,
-        frequency: 58,
-        id: '1BYn4088xDoGT4ND',
-      },
-      {
-        '_[flow]': 'intent',
-        '_[upiqr]': '1',
-        method: 'upi',
-        timestamp: 1573540387023,
-        success: true,
-        frequency: 25,
-        id: 'CzrbrLSryKJRqT',
-      },
-      {
-        bank: 'KKBK',
-        method: 'netbanking',
-        timestamp: 1564737032060,
-        success: false,
-        frequency: 3,
-        id: 'D0HgIpdHcGaJZH',
-      },
-      {
-        bank: 'HDFC',
-        method: 'netbanking',
-        timestamp: 1567500711224,
-        success: false,
-        frequency: 6,
-        id: 'D0HgiQA7UG5Ykd',
-      },
-      {
-        '_[flow]': 'directpay',
-        vpa: 'umanghome123@ybl',
-        method: 'upi',
-        timestamp: 1564646237158,
-        success: false,
-        frequency: 2,
-        id: 'D0apTaxfmKwEkz',
-      },
-      {
-        bank: 'ALLA',
-        method: 'netbanking',
-        timestamp: 1564735856210,
-        success: false,
-        frequency: 1,
-        id: 'D10HRixVyoaFuI',
-      },
-      // {
-      //   "_[flow]": "intent",
-      //   "method": "upi",
-      //   "timestamp": 1567088580069,
-      //   "success": false,
-      //   "frequency": 2,
-      //   "id": "DBmM8zc92WEfer"
-      // },
-      {
-        '_[flow]': 'directpay',
-        vpa: 'umanghome@okicici',
-        method: 'upi',
-        timestamp: 1567690358160,
-        success: false,
-        frequency: 4,
-        id: 'DBmMbrqGe096p7',
-      },
-      {
-        bank: 'PMCB',
-        method: 'netbanking',
-        timestamp: 1568021807837,
-        success: false,
-        frequency: 1,
-        id: 'DG3MT1zKlc2Zu1',
-      },
-      {
-        '_[flow]': 'directpay',
-        vpa: 'umangrajeshgalaiya@icici',
-        method: 'upi',
-        timestamp: 1568712465764,
-        success: false,
-        frequency: 1,
-        id: 'DJDTt4vN1krVBx',
-      },
-    ];
-    // TODO
-    // let instruments = getInstrumentsForCustomer(customer, {
-    //   methods: session.methods
-    // });
+    let customer = session.getCustomer($contact);
 
-    // TODO: Move this to p13n filters
-    /* Filter out any app that's in user's list but not currently installed */
-    instruments = _Arr.filter(instruments, instrument => {
-      if (instrument.method === 'upi' && instrument['_[flow]'] === 'intent') {
-        if (instrument['_[upiqr]'] === '1' && !isMobile()) {
-          return true;
-        }
-
-        if (doesAppExist(instrument.upi_app, session.upi_intents_data)) {
-          return true;
-        }
-
-        return false;
-      }
-
-      return true;
+    let instruments = getInstrumentsForCustomer(customer, {
+      methods: session.methods,
+      upiApps: session.upi_intents_data,
     });
 
     visibleInstruments = instruments.slice(0, 3);
