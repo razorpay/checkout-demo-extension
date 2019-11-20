2fbddd1e787013c7fa5e285e0b422ba4de2d007b
sidgo <siddharth.goswami@razorpay.com>
2019-11-20 22:00:15 +0530

    [tests] Fix card-actions for Native OTP

--- blackbox/actions/card-actions.js
+++ blackbox/actions/card-actions.js
@@ -1,6 +1,22 @@
 const { delay } = require('../util');
 
-async function handleCardValidation(
+async function handleCardValidation(context, { urlShouldContain } = {}) {
+  const req = await context.expectRequest();
+  expect(req.url).toContain(urlShouldContain || 'create/ajax');
+  await context.respondJSON({
+    type: 'first',
+    request: {
+      url: 'http://localhost:9008',
+      method: 'get',
+      content: [],
+    },
+    payment_id: 'pay_DLXKaJEF1T1KxC',
+    amount: '\u20b9 51',
+    image: 'https://cdn.razorpay.com/logos/D3JjREAG8erHB7_medium.jpg',
+  });
+}
+
+async function handleCardValidationForNativeOTP(
   context,
   { coproto, urlShouldContain } = {}
 ) {
@@ -17,28 +33,23 @@ async function handleCardValidation(
       payment_id: 'pay_DevaXGbxgAKHE9',
       next: ['otp_submit', 'otp_resend'],
       gateway: 'eyJpdiI6InZyajhcLzFyZnppVEhtRkJHN0dKN',
-      submit_url:
-        'https://api.razorpay.com/v1/gateway/mocksharp/payment/otp_submit/c8aafb01f805301c93f9607c0e02679b01d479ff?key_id=rzp_live_ILgsfZCZoFIKMb',
-      resend_url:
-        'https://api.razorpay.com/v1/gateway/mocksharp/payment/otp/resend',
+      submit_url: 'https://api.razorpay.com/otp_submit',
+      resend_url: 'https://api.razorpay.com/otp_resend',
       metadata: {
         issuer: 'HDFC',
         network: 'MC',
         last4: '9275',
         iin: '512967',
       },
-      redirect:
-        'https://api.razorpay.com/v1/gateway/mocksharp/payment?key_id=rzp_test_1DP5mmOlF5G5ag&action=authorize&amount=5100&method=card&payment_id=DLXKaJEF1T1KxC&callback_url=https%3A%2F%2Fapi.razorpay.com%2Fv1%2Fpayments%2Fpay_DLXKaJEF1T1KxC%2Fcallback%2F10b9b52d2b5974f35acfec916f3785eab0c98325%2Frzp_test_1DP5mmOlF5G5ag&recurring=0&card_number=eyJpdiI6ImdnUm9BbnZucTRMU09VWiswMHQ1WFE9PSIsInZhbHVlIjoiSkpwZjJOd2htQlcza2dzYnNiRjJFb3ZqUlVaNGw4WEtLWDgyOVVxYnN4ST0iLCJtYWMiOiIxZDg2YTBlYWY3MGEyNzE5NWQ1NzNhNTRiMjc4ZTZhZTFlYTQxNDUyNWU1NjkzOTNlYTEzYjljZmM0YWY1NGIyIn0%3D&encrypt=1',
-      submit_url_private:
-        'https://api.razorpay.com/v1/gateway/mocksharp/payment/otp/submit',
-      resend_url_private:
-        'https://api.razorpay.com/v1/gateway/mocksharp/payment/otp/resend',
+      redirect: 'http://localhost:9008',
+      submit_url_private: 'https://api.razorpay.com/otp_submit',
+      resend_url_private: 'https://api.razorpay.com/otp_resend',
     });
   } else {
     await context.respondJSON({
       type: 'first',
       request: {
-        url: 'http://localhost:9008',
+        url: 'https://api.razorpay.com/bank',
         method: 'get',
         content: [],
       },
@@ -47,6 +58,12 @@ async function handleCardValidation(
       image: 'https://cdn.razorpay.com/logos/D3JjREAG8erHB7_medium.jpg',
     });
   }
+}
+
+async function handleBankRequest(context) {
+  const req = await context.expectRequest();
+  expect(req.url).toContain('api.razorpay.com/bank');
+  await context.respondHTML(`<html></html>`);
 }
 
 async function enterCardDetails(context, { cardType, nativeOtp = false } = {}) {
@@ -76,5 +93,7 @@ async function retryCardTransaction(context) {
 module.exports = {
   enterCardDetails,
   handleCardValidation,
+  handleCardValidationForNativeOTP,
+  handleBankRequest,
   retryCardTransaction,
 };
