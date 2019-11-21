0d6bfa644c88b77d2ef141af1464e44641e2e5b3
Chintan Acharya <chintan.acharya@razorpay.com>
2019-11-21 17:36:45 +0530

    Split address into fields

--- app/modules/templates/views/address.svelte
+++ app/modules/templates/views/address.svelte
@@ -1,12 +1,15 @@
 <script>
+  // UI Imports
+  import AddressField from 'templates/views/ui/fields/AddressField.svelte';
+  import PinCodeField from 'templates/views/ui/fields/PinCodeField.svelte';
+  import StateField from 'templates/views/ui/fields/StateField.svelte';
+
+  // Props
   export let states;
 
   export let address;
   export let pincode;
   export let state;
-
-  const ADDRESS_PATTERN = '[\\s\\S]{10}';
-  const PINCODE_PATTERN = '^\\\\d{6}$';
 </script>
 
 <style>
@@ -17,44 +20,12 @@
 
 <div class="wrap">
   <div class="elem-wrap" id="elem-wrap-address">
-    <div class="elem elem-address">
-      <div class="help">Address should be at least 10 characters long</div>
-      <label>Address</label>
-      <textarea
-        class="input"
-        name="address"
-        type="text"
-        id="address"
-        required
-        pattern={ADDRESS_PATTERN}
-        maxlength="200"
-        rows="2"
-        bind:value={address} />
-    </div>
+    <AddressField bind:value={address} />
   </div>
   <div class="elem-wrap">
-    <div class="elem">
-      <div class="help">Enter 6 digit pincode</div>
-      <label>PIN Code</label>
-      <input
-        class="input"
-        required
-        pattern={PINCODE_PATTERN}
-        maxlength="6"
-        id="pincode"
-        bind:value={pincode} />
-    </div>
+    <PinCodeField bind:value={pincode} />
   </div>
   <div class="elem-wrap">
-    <div class="elem select elem-state">
-      <div class="help">Select a value from list of states</div>
-      <i class="select-arrow">&#xe601;</i>
-      <select class="input" required id="state" bind:value={state}>
-        <option value="">Select State</option>
-        {#each states as [code, text] (code)}
-          <option value={code}>{text}</option>
-        {/each}
-      </select>
-    </div>
+    <StateField {states} bind:value={state} />
   </div>
 </div>
---
+++ app/modules/templates/views/ui/fields/PinCodeField.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Field from 'templates/views/ui/Field.svelte';
+
+  export let value;
+
+  const PINCODE_PATTERN = '^\\d{6}$';
+
+  function handleInput({ target }) {
+    value = target.value;
+  }
+</script>
+
+<Field
+  name="pincode"
+  required={true}
+  pattern={PINCODE_PATTERN}
+  maxlength={6}
+  helpText="Enter 6 digit pincode"
+  label="PIN Code"
+  on:input={handleInput}
+  {value}
+  handleFocus={true}
+  handleBlur={true}
+  handleInput={true} />
