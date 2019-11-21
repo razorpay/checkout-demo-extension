43d7fa59c79b150a94a82db6d82a7ec203d15141
Chintan Acharya <chintan.acharya@razorpay.com>
2019-11-21 11:40:17 +0530

    Add address fields to home screen store

--- app/modules/checkoutstore/screens/home.js
+++ app/modules/checkoutstore/screens/home.js
@@ -3,3 +3,7 @@ import { writable } from 'svelte/store';
 export const contact = writable('');
 export const email = writable('');
 export const selectedInstrumentId = writable(null);
+
+export const address = writable('');
+export const pincode = writable('');
+export const state = writable('');
