bb91964dfc48bd926b348ea54922663fedbf0bdc
Sakshi Jain <sakshi.jain@razorpay.com>
2019-11-21 12:03:25 +0530

    Added test cases for callbackURL for EMI

---
+++ blackbox/tests/EMI/callbackURLPartialPayments.test.js
@@ -0,0 +1,50 @@
+const { openCheckout } = require('../../actions/checkout');
+const { makePreferences } = require('../../actions/preferences');
+const {
+  assertHomePage,
+  fillUserDetails,
+  assertPaymentMethods,
+  selectPaymentMethod,
+  enterCardDetails,
+  submit,
+  selectEMIPlanWithoutOffer,
+  verifyEMIPlansWithoutOffers,
+  expectRedirectWithCallback,
+  handlePartialPayment,
+  verifyPartialAmount,
+} = require('../../actions/common');
+
+describe('EMI tests', () => {
+  test('perform EMI transaction with partial payments enabled', async () => {
+    const options = {
+      key: 'rzp_test_1DP5mmOlF5G5ag',
+      amount: 500000,
+      personalization: false,
+      callback_url: 'http://www.merchanturl.com/callback?test1=abc&test2=xyz',
+      redirect: true,
+    };
+    const preferences = makePreferences({
+      order: {
+        amount: 300000,
+        amount_due: 300000,
+        amount_paid: 0,
+        currency: 'INR',
+        first_payment_min_amount: null,
+        partial_payment: true,
+      },
+    });
+    const context = await openCheckout({ page, options, preferences });
+    await assertHomePage(context, true, true);
+    await fillUserDetails(context, true);
+    await handlePartialPayment(context, '3000');
+    await assertPaymentMethods(context);
+    await selectPaymentMethod(context, 'emi');
+    await enterCardDetails(context);
+    await submit(context);
+    await verifyEMIPlansWithoutOffers(context, '6');
+    await selectEMIPlanWithoutOffer(context, '2');
+    await verifyPartialAmount(context, '₹ 3,000');
+    await submit(context);
+    await expectRedirectWithCallback(context, { method: 'emi' });
+  });
+});
