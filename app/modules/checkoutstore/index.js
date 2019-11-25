117c6b9f2b64745feee34c3a7659eaa710603311
Umang Galaiya <umang.galaiya@razorpay.com>
2019-11-25 17:33:52 +0530

    Add hidden fields to CheckoutStore

--- app/modules/checkoutstore/index.js
+++ app/modules/checkoutstore/index.js
@@ -1,5 +1,6 @@
 import Preferences from 'checkoutstore/preferences';
 import Downtimes from 'checkoutstore/downtimes';
+import { getSession } from 'sessionmanager';
 
 const defaultState = {};
 
@@ -11,23 +12,33 @@ function CheckoutStore(base) {
   };
 
   this.get = function() {
+    const session = getSession();
+
     const storeState = _Obj.extend({}, checkoutStoreState);
     const preferences = Preferences.get();
     const downtimes = Downtimes.get();
     const optionalFields = {};
     const optionalFieldsList = preferences.optional || [];
+    const hiddenFields = {};
 
     if (optionalFieldsList) {
       optionalFields.contact = optionalFieldsList |> _Arr.contains('contact');
       optionalFields.email = optionalFieldsList |> _Arr.contains('email');
     }
 
+    hiddenFields.contact =
+      optionalFields.contact && session.get('hidden.contact');
+    hiddenFields.email = optionalFields.email && session.get('hidden.email');
+
     storeState.optional = optionalFields;
+    storeState.hidden = hiddenFields;
     storeState.isPartialPayment =
       preferences.order && preferences.order.partial_payment;
 
     storeState.contactEmailOptional =
       storeState.optional.contact && storeState.optional.email;
+    storeState.contactEmailHidden =
+      storeState.hidden.contact && storeState.hidden.email;
     storeState.verticalMethods =
       storeState.contactEmailOptional || storeState.isPartialPayment;
 
