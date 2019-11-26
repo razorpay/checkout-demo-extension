4715efd97da52821bd624546530c3e148df43fca
Umang Galaiya <umang.galaiya@razorpay.com>
2019-11-26 23:10:31 +0530

    Add homescreen experiment

--- app/modules/experiments/current.js
+++ app/modules/experiments/current.js
@@ -1,1 +1,20 @@
-export default [];
+/**
+ * Experiment format:
+ * {
+ *    name: 'string',
+ *    evaluator: function that returns a segment
+ * }
+ */
+
+/**
+ * Homescreen 2019 Experiment
+ * Segments:
+ *  - 0: Old homescreen
+ *  - 1: New homescreen
+ */
+const HOMESCREEN_2019 = {
+  name: 'home_2019',
+  evaluator: () => (Math.random() >= 0.5 ? 0 : 1),
+};
+
+export default [HOMESCREEN_2019];
