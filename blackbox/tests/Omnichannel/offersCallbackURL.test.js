56fd23b59d861292160406f79685687621fdfb0f
Sakshi Jain <sakshi.jain@razorpay.com>
2019-12-02 16:29:32 +0530

    Added onmichannel test cases

---
+++ blackbox/tests/Omnichannel/offersCallbackURL.test.js
@@ -0,0 +1,63 @@
+const { openCheckout } = require('../../actions/checkout');
+const { makePreferences } = require('../../actions/preferences');
+const {
+  assertHomePage,
+  fillUserDetails,
+  assertPaymentMethods,
+  selectPaymentMethod,
+  selectUPIMethod,
+  submit,
+  verifyOmnichannelPhoneNumber,
+  expectRedirectWithCallback,
+  viewOffers,
+  selectOffer,
+  verifyOfferApplied,
+  verifyDiscountPaybleAmount,
+  verifyDiscountAmountInBanner,
+  verifyDiscountText,
+} = require('../../actions/common');
+describe('Basic Omnichannel payment', () => {
+  test('Perform Omnichannel transaction with offers and callbackURL applied', async () => {
+    const options = {
+      key: 'rzp_live_rFalxzSoQIEcFH',
+      amount: 200000,
+      personalization: false,
+      callback_url: 'http://www.merchanturl.com/callback?test1=abc&test2=xyz',
+      redirect: true,
+    };
+    const preferences = makePreferences({
+      features: { google_pay_omnichannel: true },
+      offers: [
+        {
+          original_amount: 200000,
+          amount: 198000,
+          id: 'offer_Dcad1sICBaV2wI',
+          name: 'UPI Offer Name',
+          payment_method: 'upi',
+          display_text: 'UPI Offer Display Text',
+        },
+      ],
+    });
+    preferences.methods.upi = true;
+    preferences.mode = 'live';
+    const context = await openCheckout({
+      page,
+      options,
+      preferences,
+    });
+    await assertHomePage(context, true, true);
+    await fillUserDetails(context);
+    await assertPaymentMethods(context);
+    await selectPaymentMethod(context, 'upi');
+    await selectUPIMethod(context, 'Google Pay');
+    await verifyOmnichannelPhoneNumber(context);
+    await viewOffers(context);
+    await selectOffer(context, '1');
+    await verifyOfferApplied(context);
+    await verifyDiscountPaybleAmount(context, '₹ 1,980');
+    // await verifyDiscountAmountInBanner(context, '₹ 1,980'); /* Issue reported CE-963*/
+    await verifyDiscountText(context, 'You save ₹ 20');
+    await submit(context);
+    await expectRedirectWithCallback(context, { method: 'upi' });
+  });
+});
