8a0d500ba6173579aad1a9dc2a5198997bcfb654
Baala <baalamurugan.sc@razorpay.com>
2019-12-02 11:52:09 +0530

    Remaining Test Cases for GooglePay

---
+++ blackbox/tests/qrScanner/personalizationFeebearer.test.js
@@ -0,0 +1,43 @@
+const {
+  openCheckoutForPersonalization,
+} = require('../../actions/checkout-personalization');
+const { makePreferences } = require('../../actions/preferences');
+const {
+  assertHomePage,
+  fillUserDetails,
+  verifyPersonalizationPaymentMethodsText,
+  submit,
+  respondToUPIPaymentStatus,
+  respondToUPIAjax,
+  responseWithQRImage,
+  validateQRImage,
+  selectPersonalizationPaymentMethod,
+  handleFeeBearer,
+} = require('../../actions/common');
+
+describe('QR Scanner with Personalization  payment', () => {
+  test('Perform QR Scanner with Personalization transaction', async () => {
+    const options = {
+      key: 'rzp_test_VwsqHDsQPoVQi6',
+      amount: 60000,
+    };
+    const preferences = makePreferences({ fee_bearer: true });
+    preferences.methods.upi = true;
+    const context = await openCheckoutForPersonalization({
+      page,
+      options,
+      preferences,
+      method: 'QR',
+    });
+    await assertHomePage(context, true, true);
+    await fillUserDetails(context, '8888888885');
+    await verifyPersonalizationPaymentMethodsText(context, 'QR', 'UPI QR');
+    await selectPersonalizationPaymentMethod(context, '1');
+    await submit(context);
+    await handleFeeBearer(context);
+    await respondToUPIAjax(context, { method: 'qr' });
+    await responseWithQRImage(context);
+    await validateQRImage(context);
+    await respondToUPIPaymentStatus(context);
+  });
+});
