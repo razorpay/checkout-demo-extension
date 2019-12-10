0ca4c688c40b6696fa1a6cdc57eec968cee682a8
Sakshi Jain <sakshi.jain@razorpay.com>
2019-12-10 10:43:57 +0530

    updated UPI intent test cases with new homescreen changes

---
+++ blackbox/tests/homescreen/upiIntent/feebearerDowntimeHigh.test.js
@@ -0,0 +1,68 @@
+const { makePreferences } = require('../../../actions/preferences');
+const { openSdkCheckoutWithNewHomeScreen } = require('../open');
+
+const {
+  submit,
+  respondAndVerifyIntentRequest,
+  selectUPIApp,
+} = require('../../../actions/common');
+
+const {
+  assertBasicDetailsScreen,
+  fillUserDetails,
+  proceed,
+  assertUserDetails,
+  assertPaymentMethods,
+  selectPaymentMethod,
+  assertEditUserDetailsAndBack,
+  verifyHighDowntime,
+} = require('../actions');
+
+describe('Basic upi payment', () => {
+  test('Verify UPI intent downtime - High with customer feebearer enabled', async () => {
+    const options = {
+      key: 'rzp_test_1DP5mmOlF5G5ag',
+      amount: 200,
+      personalization: false,
+    };
+    const preferences = makePreferences({
+      fee_bearer: true,
+      payment_downtime: {
+        entity: 'collection',
+        count: 1,
+        items: [
+          {
+            id: 'down_DEW7D9S10PEsl1',
+            entity: 'payment.downtime',
+            method: 'upi',
+            begin: 1567686386,
+            end: null,
+            status: 'started',
+            scheduled: false,
+            severity: 'high',
+            created_at: 1567686387,
+            updated_at: 1567686387,
+          },
+        ],
+      },
+    });
+    preferences.methods.upi = true;
+    const context = await openSdkCheckoutWithNewHomeScreen({
+      page,
+      options,
+      preferences,
+      apps: [{ package_name: 'in.org.npci.upiapp', app_name: 'BHIM' }],
+    });
+    await assertBasicDetailsScreen(context);
+    await fillUserDetails(context);
+    await proceed(context);
+    await assertUserDetails(context);
+    await assertEditUserDetailsAndBack(context);
+    await assertPaymentMethods(context);
+    await verifyHighDowntime(
+      context,
+      'upi',
+      'UPI is facing temporary issues right now.'
+    );
+  });
+});
---
+++ blackbox/tests/homescreen/upiIntent/partialPaymentOffers.test.js
@@ -0,0 +1,94 @@
+const { makePreferences } = require('../../../actions/preferences');
+const { openSdkCheckoutWithNewHomeScreen } = require('../open');
+const {
+  submit,
+  respondAndVerifyIntentRequest,
+  selectUPIApp,
+  verifyPartialAmount,
+  viewOffers,
+  selectOffer,
+  verifyOfferApplied,
+  verifyDiscountPaybleAmount,
+  verifyDiscountText,
+  verifyDiscountAmountInBanner,
+} = require('../../../actions/common');
+
+const {
+  assertBasicDetailsScreen,
+  fillUserDetails,
+  assertUserDetails,
+  assertPaymentMethods,
+  selectPaymentMethod,
+  assertEditUserDetailsAndBack,
+  handlePartialPayment,
+} = require('../actions');
+
+describe.skip('Basic upi payment', () => {
+  test('Perform upi intent transaction with partial payments enabled and offers applied', async () => {
+    const options = {
+      key: 'rzp_test_1DP5mmOlF5G5ag',
+      amount: 200,
+      personalization: false,
+    };
+    const preferences = makePreferences({
+      order: {
+        amount: 100,
+        amount_due: 100,
+        amount_paid: 0,
+        currency: 'INR',
+        first_payment_min_amount: null,
+        partial_payment: true,
+      },
+      offers: [
+        {
+          original_amount: 200000,
+          amount: 199000,
+          id: 'offer_Dcad1sICBaV2wI',
+          name: 'UPI Offer Name',
+          payment_method: 'upi',
+          display_text: 'UPI Offer Display Text',
+        },
+        {
+          original_amount: 200000,
+          amount: 199000,
+          id: 'offer_DcaetTeD4Gjcma',
+          name: 'UPI Offer Name 2',
+          payment_method: 'upi',
+          display_text: 'UPI Offer Display Text 2',
+        },
+        {
+          original_amount: 200000,
+          amount: 199000,
+          id: 'offer_DcafkxTAseGAtT',
+          name: 'UPI Offer Name 3',
+          payment_method: 'upi',
+          display_text: 'UPI Offer Display Text 3',
+        },
+      ],
+    });
+    preferences.methods.upi = true;
+    const context = await openSdkCheckoutWithNewHomeScreen({
+      page,
+      options,
+      preferences,
+      apps: [{ package_name: 'in.org.npci.upiapp', app_name: 'BHIM' }],
+    });
+    await assertBasicDetailsScreen(context);
+    await fillUserDetails(context);
+    await handlePartialPayment(context, '1');
+    await assertUserDetails(context);
+    await assertEditUserDetailsAndBack(context);
+    await assertPaymentMethods(context);
+    await selectPaymentMethod(context, 'upi');
+    await selectUPIApp(context, '1');
+    await verifyPartialAmount(context, '₹ 1');
+    await viewOffers(context);
+    await selectOffer(context, '1');
+    await verifyOfferApplied(context);
+    await verifyDiscountPaybleAmount(context, '₹ 1,990');
+    await verifyDiscountAmountInBanner(context, '₹ 1,990');
+    await verifyDiscountText(context, 'You save ₹ 10');
+    await submit(context);
+    await respondAndVerifyIntentRequest(context);
+  });
+});
