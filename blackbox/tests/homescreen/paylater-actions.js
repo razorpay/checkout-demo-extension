fefafb4f1d770561abcfc8e5ebdb5760f199156e
Sakshi Jain <sakshi.jain@razorpay.com>
2019-12-16 10:11:07 +0530

    Made recommended changes

--- blackbox/tests/homescreen/paylater-actions.js
+++ blackbox/tests/homescreen/paylater-actions.js
@@ -1,13 +1,11 @@
 async function handlePayLaterOTPOrCustomerCardStatusRequest(context) {
   const req = await context.expectRequest();
   if (req.url.includes('otp/verify')) {
-    expect(req.url).toContain('otp/verify');
     await context.respondJSON({
       ott: '3007d85081c8fb',
       success: 1,
     });
   } else if (req.url.includes('customers/status')) {
-    expect(req.url).toContain('customers/status');
     await context.respondJSON({ saved: true });
   }
 }
