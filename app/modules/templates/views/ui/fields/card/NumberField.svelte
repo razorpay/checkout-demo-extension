c20ba235d2feeb020f95d15f37ea162230beeb7c
Chintan Acharya <chintan.acharya@razorpay.com>
2020-01-03 18:19:26 +0530

    Fix exports from number field

--- app/modules/templates/views/ui/fields/card/NumberField.svelte
+++ app/modules/templates/views/ui/fields/card/NumberField.svelte
@@ -6,36 +6,18 @@
   import { getSession } from 'sessionmanager';
   import { Formatter } from 'formatter';
   import { getCardType, getIin, getCardDigits } from 'common/card';
-  import { DEFAULT_AUTH_TYPE_RADIO, SHOWN_CLASS } from 'common/constants';
-
   import { getIcon } from 'icons/network';
 
   export let value = '';
   export let type = null;
+  export let id = '';
 
   const dispatch = createEventDispatcher();
   const session = getSession();
-  const INVALID_CLASS = 'invalid';
 
   let invalid = false;
   let field = null;
   let formatterObj = null;
-
-  const Flows = {
-    PIN: 'pin',
-    OTP: 'otp',
-    RECURRING: 'recurring',
-  };
-
-  /**
-   * @param {Object} flows
-   * @param {String} flow
-   *
-   * @return {Boolean}
-   */
-  const isFlowApplicable = _.curry2((flows, flow) => Boolean(flows[flow]));
-
-  export function onShown() {}
 
   export function getType() {
     return type;
@@ -43,14 +25,15 @@
 
   function handleInput(e) {
     value = e.target.value;
-    onChange();
+    setCardValidity(sync());
+    dispatch('input', e.detail);
   }
 
   /**
    * Validate the card number.
    * @return {Boolean}
    */
-  function sync() {
+  export function sync() {
     const cardNumber = getCardDigits(value);
     const cardType = getCardType(cardNumber);
 
@@ -85,7 +68,7 @@
     // }
   }
 
-  function setCardValidity(isValid) {
+  export function setCardValidity(isValid) {
     const cardNumber = getCardDigits(value);
     const cardType = getCardType(cardNumber);
     const caretPosition = field.getCaret();
@@ -108,91 +91,6 @@
       invalid = true;
     }
   }
-
-  function setDebitPinRadiosVisibility(visible) {
-    const container = _Doc.querySelector(
-      '#add-card-container .flow-selection-container'
-    );
-    const checkedRadio = _Doc.querySelector(
-      '#add-card-container .flow.input-radio input[type=radio]:checked'
-    );
-
-    if (visible) {
-      // Show container
-      _El.addClass(container, SHOWN_CLASS);
-
-      // Get default radio
-      const defaultRadio = _Doc.querySelector(
-        `#add-card-container .flow.input-radio #flow-${DEFAULT_AUTH_TYPE_RADIO}`
-      );
-
-      // Set default radio to checked if none is checked
-      if (!checkedRadio && defaultRadio) {
-        defaultRadio.checked = true;
-      }
-    } else {
-      // Hide container
-      _El.removeClass(container, SHOWN_CLASS);
-
-      // Uncheck values
-      if (checkedRadio) {
-        checkedRadio.checked = false;
-      }
-    }
-  }
-
-  const showDebitPinRadios = () => setDebitPinRadiosVisibility(true);
-  const hideDebitPinRadios = () => setDebitPinRadiosVisibility(false);
-
-  /**
-   * Checks and performs actions related to card flows
-   * and validate the card input.
-   * @param cardElem {DOMNode} #elem-card or similar
-   * @param cardInput (DOMNode) <input /> of card number
-   * @param {DOMNode} cardExpiry Expiry <input> on which to focus if card is valid
-   */
-  function onChange() {
-    // Perform sync validation initially
-    setCardValidity(sync());
-
-    const cardNumber = getCardDigits(value);
-    const iin = getIin(cardNumber);
-    const isStrictlyRecurring =
-      session.recurring && session.get('recurring') !== 'preferred';
-
-    const flowChecker = (flows = {}) => {
-      const cardNumber = getCardDigits(value);
-      const isIinSame = getIin(cardNumber) === iin;
-
-      // If the card number was changed before response, do nothing
-      if (!isIinSame) {
-        return;
-      }
-
-      let isValid = sync();
-
-      if (isStrictlyRecurring) {
-        isValid = isValid && isFlowApplicable(flows, Flows.RECURRING);
-      } else {
-        // Debit-PIN is not supposed to work in case of recurring
-        if (isFlowApplicable(flows, Flows.PIN)) {
-          showDebitPinRadios();
-        } else {
-          hideDebitPinRadios();
-        }
-      }
-
-      setCardValidity(isValid);
-    };
-
-    if (iin.length < 6) {
-      hideDebitPinRadios();
-    }
-
-    if (iin.length >= 6) {
-      session.r.getCardFlows(iin, flowChecker);
-    }
-  }
 </script>
 
 <style>
@@ -203,7 +101,7 @@
   .icon {
     position: absolute;
     right: 4px;
-    top: 24px;
+    top: 30px;
     width: 24px;
   }
 </style>
@@ -216,7 +114,9 @@
   {/if}
   <!-- TODO: set maxlength based on type or remove from here if already handled by formatter -->
   <!-- TODO: handle prefill and readonly -->
+  <!-- TODO: change help message for AMEX -->
   <Field
+    {id}
     formatter={{ type: 'card', on: { network: handleNetwork } }}
     helpText="Please enter your card number"
     name="card[number]"
@@ -229,7 +129,7 @@
     handleBlur
     handleFocus
     handleInput
-    on:
+    on:blur
     maxlength={19}
     bind:this={field}
     on:input={handleInput} />
