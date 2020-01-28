746537b1d50d4eefe459445dd87e792ba74b9f35
saransh <saransh.gupta@razorpay.com>
2020-01-28 14:50:08 +0530

    added saved VPA token tests

---
+++ blackbox/tests/homescreen/savedVpaTokens/optionalPartialPayment.test.js
@@ -0,0 +1,70 @@
+const { getTestData } = require('../../../actions');
+const { openCheckoutWithNewHomeScreen } = require('../open');
+const {
+  submit,
+  selectUPIMethod,
+  enterUPIAccount,
+  handleUPIAccountValidation,
+  respondToUPIAjax,
+  respondToUPIPaymentStatus,
+  verifyPartialAmount,
+} = require('../../../actions/common');
+
+const {
+  assertBasicDetailsScreen,
+  fillUserDetails,
+  proceed,
+  assertUserDetails,
+  assertPaymentMethods,
+  selectPaymentMethod,
+  assertEditUserDetailsAndBack,
+  handlePartialPayment,
+} = require('../actions');
+
+describe.each(
+  getTestData(
+    'Perform upi collect transaction with contact optional and partial payments enabled',
+    {
+      loggedIn: true,
+      anon: false,
+      options: {
+        amount: 20000,
+        personalization: false,
+      },
+      preferences: {
+        optional: ['contact'],
+        order: {
+          amount: 20000,
+          amount_due: 20000,
+          amount_paid: 0,
+          currency: 'INR',
+          first_payment_min_amount: null,
+          partial_payment: true,
+        },
+      },
+    }
+  )
+)('UPI tests', ({ preferences, title, options }) => {
+  test(title, async () => {
+    preferences.methods.upi = true;
+    const context = await openCheckoutWithNewHomeScreen({
+      page,
+      options,
+      preferences,
+    });
+    // await assertBasicDetailsScreen(context);
+    // await fillUserDetails(context);
+    // await handlePartialPayment(context, '100');
+    // await assertUserDetails(context);
+    // await assertEditUserDetailsAndBack(context);
+    await assertPaymentMethods(context);
+    await selectPaymentMethod(context, 'upi');
+    await selectUPIMethod(context, 'token');
+    // await enterUPIAccount(context, 'BHIM');
+    await verifyPartialAmount(context, '₹ 100');
+    await submit(context);
+    // await handleUPIAccountValidation(context, 'BHIM@upi');
+    await respondToUPIAjax(context);
+    await respondToUPIPaymentStatus(context);
+  });
+});
