6646e71aad46984146fa9814f59cf59fb852ae23
Umang Galaiya <umang.galaiya@razorpay.com>
2020-01-29 16:56:17 +0530

    [Emandate] Show bank account type dropdown in the UI

--- app/modules/checkoutframe/emandate.js
+++ app/modules/checkoutframe/emandate.js
@@ -181,15 +181,6 @@ emandateView.prototype = {
       authTypes = this.session.emandateBanks[bankCode].auth_types;
     }
 
-    /**
-     * Netbanking is allowed only if
-     * 1. netbanking is an auth type, AND
-     * 2. account_type is NOT set in prefill
-     */
-    if (authTypes.indexOf('netbanking') > -1 && this.prefill.account_type) {
-      authTypes.splice(authTypes.indexOf('netbanking'), 1);
-    }
-
     return authTypes;
   },
 
