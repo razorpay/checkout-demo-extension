54596814fb4b92469870eef8d3207061ec0c4e42
saransh <saransh.gupta@razorpay.com>
2020-01-30 17:43:14 +0530

    added list header component

---
+++ app/modules/templates/views/ui/ListHeader.svelte
@@ -0,0 +1,48 @@
+<script>
+  // Svelte imports
+
+  import SlottedOption from 'templates/views/ui/options/Slotted/Option.svelte';
+
+  // Props
+  export let type;
+  export let classes = [];
+  export let reverse = false;
+
+  // Computed
+  export let optionClasses;
+
+  // Refs
+
+  $: {
+    const defaultClasses = ['option'];
+
+    if (type) {
+      defaultClasses.push(type);
+    }
+
+    if (reverse) {
+      defaultClasses.push('reverse');
+    }
+
+    optionClasses = defaultClasses.concat(classes).join(' ');
+  }
+</script>
+
+<style>
+  div[slot='icon'].full-opacity.higher-spec {
+    opacity: 1;
+    margin-right: 0px;
+  }
+  div[slot='subtitle'] {
+    color: rgba(81, 89, 120, 0.7);
+  }
+</style>
+
+<SlottedOption defaultStyles disabled>
+  <div slot="subtitle">
+    <slot name="subtitle" />
+  </div>
+  <div slot="icon" class="full-opacity higher-spec">
+    <slot name="icon" />
+  </div>
+</SlottedOption>
