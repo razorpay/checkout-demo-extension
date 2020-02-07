43b0bf860887da83fb1e17c6baa9d564c604ef6a
AakashRaina <aakashraina9@gmail.com>
2020-02-07 14:19:37 +0530

    [COUNTRYCODES] Adds algorithm

--- app/modules/common/countrycodesutil.js
+++ app/modules/common/countrycodesutil.js
@@ -1,5 +1,22 @@
 import countrycodes from 'countrycodes';
 
+/**
+ * There are 3 possible scenarios for a given number - 
+    I Starts with + 
+    II Starts with 00
+    III Starts without + or 00
+
+  1) For numbers starting with 00, the leading 2 zeros are converted to +, so that cuts it down to final 2 scenarios(I & III).
+  2) Picking scenario(I) - The + is ignored and the first 3 digits are picked up to be checked against the list of country codes. The maximum length of a country code is 3 digits, hence going for a top-to-bottom approach. 
+  3) The 3 digit country code is checked against all 3 digit country codes - If it matches, function returns country code.
+  4) Next step - Pick first 2 digits and check against all 2 digit country codes - If it matches, function returns country code.
+  5) Next step - Pick first 1 digit and check against all 1 digit country codes - If it matches, function returns country code or else undefined.
+  6) Picking scenario(III) - checking for an Indian number is our 1st priority so checking for 10 or 12 digits.
+  7) If the number is a 10 digit one - and starts with [6-9] - It will be classified as an Indian phone number.
+  8) If the number is a 12 digit one - and starts with 91[6-9] - It will be classified as an Indian phone number.
+  9) Other than these scenarios, numbers starting without  a +  or 00 make it difficult to determine country code from a given number.
+ */
+
 const MAX_LENGTH_COUNTRY_CODE = 3;
 
 /**
