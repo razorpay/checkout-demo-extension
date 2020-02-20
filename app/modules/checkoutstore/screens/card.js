6edad1a937e0e1b0c147336cf87138d3c5ebc6d8
Chintan Acharya <chintan.acharya@razorpay.com>
2020-02-20 14:52:56 +0530

    Check remember card by default

--- app/modules/checkoutstore/screens/card.js
+++ app/modules/checkoutstore/screens/card.js
@@ -5,7 +5,7 @@ export const cardNumber = writable('');
 export const cardCvv = writable('');
 export const cardExpiry = writable('');
 export const cardName = writable('');
-export const remember = writable(false);
+export const remember = writable(true);
 export const authType = writable('c3ds');
 
 export const cardType = derived(cardNumber, getCardType);
