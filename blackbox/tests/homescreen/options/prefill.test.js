5e58fe0b43fc49c6a10abf23d553327df4969d50
saransh <saranshgupta1995@gmail.com>
2020-03-04 13:41:49 +0530

    add prefill tests for card and netbanking
    added new action

---
+++ blackbox/tests/homescreen/options/prefill.test.js
@@ -0,0 +1,89 @@
+const { getTestData } = require('../../../actions');
+const { openCheckoutWithNewHomeScreen } = require('../open');
+
+const {
+  assertBasicDetailsScreen,
+  fillUserDetails,
+  proceed,
+  assertUserDetails,
+  assertPaymentMethods,
+  selectPaymentMethod,
+  assertInputValue,
+} = require('../actions');
+
+const nameToBePrefilled = 'Saransh';
+const cardCvvToBePrefilled = '122';
+const cardNumberToBePrefilled = '4242424242424242';
+const cardExpiryToBePrefilled = '12/55';
+
+// describe.each(
+//   getTestData('Check contact and card prefills', {
+//     options: {
+//       prefill: {
+//         contact: '222222222',
+//         name: nameToBePrefilled,
+//         email: 'a@gmail.com',
+//         'card[number]': cardNumberToBePrefilled,
+//         'card[expiry]': cardExpiryToBePrefilled,
+//         'card[cvv]': cardCvvToBePrefilled,
+//       },
+//     },
+//     keyless: false,
+//     preferences: {},
+//   })
+// )('Prefill tests', ({ preferences, title, options }) => {
+//   test(title, async () => {
+//     const context = await openCheckoutWithNewHomeScreen({
+//       page,
+//       options,
+//       preferences,
+//     });
+//     await assertBasicDetailsScreen(context);
+//     await fillUserDetails(context);
+
+//     await proceed(context);
+//     await assertUserDetails(context);
+//     await assertPaymentMethods(context);
+//     await selectPaymentMethod(context, 'card');
+//     await assertInputValue(context, '#card_name', nameToBePrefilled);
+//     await assertInputValue(context, '#card_cvv', cardCvvToBePrefilled);
+//     await assertInputValue(context, '#card_number', '4242 4242 4242 4242');
+//     await assertInputValue(context, '#card_expiry', '12 / 55');
+//   });
+// });
+
+const bankToPreselect = 'SBIN';
+
+describe.each(
+  getTestData('Check amount and netbanking prefill', {
+    options: {
+      amount: 200202,
+      prefill: {
+        bank: bankToPreselect,
+      },
+    },
+    keyless: false,
+    preferences: {},
+  })
+)('Prefill tests', ({ preferences, title, options }) => {
+  test(title, async () => {
+    const context = await openCheckoutWithNewHomeScreen({
+      page,
+      options,
+      preferences,
+    });
+    await assertBasicDetailsScreen(context);
+    const amountSpan = await context.page.waitForSelector(
+      '#amount .original-amount'
+    );
+    const amountText = await context.page.evaluate(
+      amountSpan => amountSpan.textContent,
+      amountSpan
+    );
+    expect(amountText).toContain(`2,002.02`);
+    await fillUserDetails(context);
+
+    await proceed(context);
+    await assertInputValue(context, '#bank-select', bankToPreselect);
+  });
+});
