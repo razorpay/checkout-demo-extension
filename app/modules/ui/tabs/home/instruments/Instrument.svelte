87b83d9e085ccab5b165954b2f56c1b54c142873
Umang Galaiya <umang.galaiya@razorpay.com>
2020-03-05 18:37:26 +0530

    Add base Instrument.svelte

---
+++ app/modules/ui/tabs/home/instruments/Instrument.svelte
@@ -0,0 +1,24 @@
+<script>
+  // Utils imports
+  import { isInstrumentForEntireMethod } from 'configurability/instruments.js';
+
+  // UI imports
+  import MethodInstrument from './MethodInstrument.svelte';
+  import RadioInstrument from './RadioInstrument.svelte';
+  import InputInstrument from './InputInstrument.svelte';
+
+  // Props
+  export let instrument;
+
+  const isMethodInstrument = isInstrumentForEntireMethod(instrument);
+  const isSavedCardInstrument = instrument.method && instrument.token; // TODO
+  const isInputInstrument = isSavedCardInstrument;
+</script>
+
+{#if isMethodInstrument}
+  <MethodInstrument {instrument} />
+{:else if isInputInstrument}
+  <InputInstrument {instrument} />
+{:else}
+  <RadioInstrument {instrument} />
+{/if}
