bdeff2caf60703bcf1bee51dda347d615c24b3c1
Umang Galaiya <umang.galaiya@razorpay.com>
2020-03-05 19:08:38 +0530

    Add SavedCardInstrument.svelte

--- app/modules/ui/tabs/home/instruments/Instrument.svelte
+++ app/modules/ui/tabs/home/instruments/Instrument.svelte
@@ -5,20 +5,19 @@
   // UI imports
   import MethodInstrument from './MethodInstrument.svelte';
   import RadioInstrument from './RadioInstrument.svelte';
-  import InputInstrument from './InputInstrument.svelte';
+  import SavedCardInstrument from './SavedCardInstrument.svelte';
 
   // Props
   export let instrument;
 
   const isMethodInstrument = isInstrumentForEntireMethod(instrument);
-  const isSavedCardInstrument = instrument.method && instrument.token; // TODO
-  const isInputInstrument = isSavedCardInstrument;
+  const isSavedCardInstrument = instrument.method && instrument.token_id;
 </script>
 
 {#if isMethodInstrument}
   <MethodInstrument {instrument} />
-{:else if isInputInstrument}
-  <InputInstrument {instrument} />
+{:else if isSavedCardInstrument}
+  <SavedCardInstrument {instrument} />
 {:else}
   <RadioInstrument {instrument} />
 {/if}
