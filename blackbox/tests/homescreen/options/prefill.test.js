8c1bf04c8a142693c322d19146b7652969b74e17
saransh <saranshgupta1995@gmail.com>
2020-03-06 12:58:26 +0530

    moved test out to avoid intermittent failures

--- blackbox/tests/homescreen/options/prefill.test.js
+++ blackbox/tests/homescreen/options/prefill.test.js
@@ -12,47 +12,6 @@ const {
   assertInputValue,
 } = require('../actions');
 
-const nameToBePrefilled = 'Saransh';
-const cardCvvToBePrefilled = '122';
-const cardNumberToBePrefilled = '4242424242424242';
-const cardExpiryToBePrefilled = '12/55';
-
-describe.each(
-  getTestData('Check contact and card prefills', {
-    options: {
-      prefill: {
-        contact: '222222222',
-        name: nameToBePrefilled,
-        email: 'a@gmail.com',
-        'card[number]': cardNumberToBePrefilled,
-        'card[expiry]': cardExpiryToBePrefilled,
-        'card[cvv]': cardCvvToBePrefilled,
-      },
-    },
-    keyless: false,
-    preferences: {},
-  })
-)('Prefill tests', ({ preferences, title, options }) => {
-  test(title, async () => {
-    const context = await openCheckoutWithNewHomeScreen({
-      page,
-      options,
-      preferences,
-    });
-    await assertBasicDetailsScreen(context);
-    await fillUserDetails(context);
-
-    await proceed(context);
-    await assertUserDetails(context);
-    await assertPaymentMethods(context);
-    await selectPaymentMethod(context, 'card');
-    await assertInputValue(context, '#card_name', nameToBePrefilled);
-    await assertInputValue(context, '#card_cvv', cardCvvToBePrefilled);
-    await assertInputValue(context, '#card_number', '4242 4242 4242 4242');
-    await assertInputValue(context, '#card_expiry', '12 / 55');
-  });
-});
-
 const bankToPreselect = 'SBIN';
 
 describe.each(
