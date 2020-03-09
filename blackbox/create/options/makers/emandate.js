94e78bc664e5fb48713e8ff42760075118470b9c
Baala <baalamurugan.sc@razorpay.com>
2020-03-09 20:37:57 +0530

    emandate tc

---
+++ blackbox/create/options/makers/emandate.js
@@ -0,0 +1,48 @@
+function makeOptions(features, options) {
+  const { callbackUrl } = features;
+
+  if (callbackUrl) {
+    options = {
+      order_id: 'order_DfNAO0KJCH5WNY',
+      amount: 0,
+      personalization: false,
+      recurring: true,
+      prefill: {
+        bank: 'HDFC',
+      },
+      callback_url: 'http://www.merchanturl.com/callback?test1=abc&test2=xyz',
+      redirect: true,
+    };
+  } else {
+    options = {
+      order_id: 'order_DfNAO0KJCH5WNY',
+      amount: 0,
+      personalization: false,
+      recurring: true,
+      prefill: {
+        bank: 'HDFC',
+      },
+    };
+  }
+
+  return options;
+}
+
+function makePreferences(features, preferences) {
+  preferences.order = [
+    {
+      amount: 0,
+      currency: 'INR',
+      payment_capture: true,
+    },
+  ];
+  preferences.order.bank = 'HDFC';
+  preferences.order.method = 'emandate';
+
+  return preferences;
+}
+
+module.exports = {
+  makeOptions,
+  makePreferences,
+};
--- blackbox/create/options/makers/index.js
+++ blackbox/create/options/makers/index.js
@@ -1,7 +1,9 @@
 const netbanking = require('./netbanking');
 const upiQr = require('./upi-qr');
+const emandate = require('./emandate');
 
 module.exports = {
   netbanking: netbanking,
   'upi-qr': upiQr,
+  emandate: emandate,
 };
