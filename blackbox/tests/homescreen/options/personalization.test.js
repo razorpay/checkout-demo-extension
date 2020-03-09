68af8bb474ccac2107e1b7479463f529e6d130f5
saransh <saranshgupta1995@gmail.com>
2020-03-09 16:08:06 +0530

    add negative test for personalization

---
+++ blackbox/tests/homescreen/options/personalization.test.js
@@ -0,0 +1,36 @@
+const { getTestData } = require('../../../actions');
+
+const {
+  assertBasicDetailsScreen,
+  assertSelectorAbsence,
+  fillUserDetails,
+  proceed,
+} = require('../actions');
+
+const { openCheckoutWithNewHomeScreen } = require('../open');
+
+describe.each(
+  getTestData('Negative test for personalization in options', {
+    loggedIn: false,
+    options: {
+      amount: 200,
+      personalization: false,
+      remember_customer: true,
+    },
+    keyless: false,
+  })
+)('Options tests', ({ preferences, title, options }) => {
+  test(title, async () => {
+    const context = await openCheckoutWithNewHomeScreen({
+      page,
+      options,
+      preferences,
+      method: 'Card',
+    });
+
+    await assertBasicDetailsScreen(context);
+    await fillUserDetails(context, '8888888881');
+    await proceed(context);
+    await assertSelectorAbsence(context, '#instruments-list');
+  });
+});
