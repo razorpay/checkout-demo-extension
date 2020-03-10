31e98bca2259e26e2931dea33260bbafa7ca51f5
Umang Galaiya <umang.galaiya@razorpay.com>
2020-03-11 03:59:20 +0530

    Add a way to ungroup instruments

---
+++ app/modules/configurability/ungroup.js
@@ -0,0 +1,41 @@
+import Config from './instrument-config';
+
+/**
+ * Returns individual instruments from an instrument that might contain a group.
+ * @param {Instrument} instrument
+ *
+ * @returns {Array<Instrument>}
+ */
+function getIndividualInstruments(instrument) {
+  const method = instrument.method;
+  const config = Config[method];
+
+  if (config.isIndividual(instrument)) {
+    return [instrument];
+  }
+
+  const individuals = config.groupedToIndividual(instrument);
+
+  return individuals;
+}
+
+/**
+ * Ungroups instruments from a block.
+ * @param {Block} block
+ *
+ * @returns {Block}
+ */
+export function ungroupInstruments(block) {
+  const instruments = block.instruments;
+  const ungrouped = [];
+
+  _Arr.loop(instruments, instrument => {
+    const individuals = getIndividualInstruments(instrument);
+
+    _Arr.mergeWith(ungrouped, individuals);
+  });
+
+  return _Obj.extend(_Obj.clone(block), {
+    instruments: ungrouped,
+  });
+}
