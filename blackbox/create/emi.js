2d9b12675cfd51484bc13b4a9f40a962fb3e4250
Baala <baalamurugan.sc@razorpay.com>
2020-03-11 14:09:41 +0530

    EMI tc

---
+++ blackbox/create/emi.js
@@ -0,0 +1,141 @@
+const makeOptionsAndPreferences = require('./options/index.js');
+const { getTestData } = require('../actions');
+const { openCheckoutWithNewHomeScreen } = require('../tests/homescreen/open');
+const {
+  // Generic
+  verifyTimeout,
+  handleFeeBearer,
+  submit,
+  handleMockSuccessDialog,
+  expectRedirectWithCallback,
+
+  //EMI
+  enterCardDetails,
+  verifyEMIPlansWithoutOffers,
+  selectEMIPlanWithoutOffer,
+  handleEMIValidation,
+  verifyEMIPlansWithOffers,
+  selectEMIPlanWithOffer,
+
+  // Offers
+  verifyOfferApplied,
+  viewOffers,
+  selectOffer,
+  setPreferenceForOffer,
+
+  // Partial Payment
+  verifyPartialAmount,
+} = require('../actions/common');
+
+const {
+  // Generic
+  proceed,
+
+  // Homescreen
+  assertBasicDetailsScreen,
+  fillUserDetails,
+  assertUserDetails,
+  assertPaymentMethods,
+  assertEditUserDetailsAndBack,
+  selectPaymentMethod,
+
+  // Partial Payments
+  handlePartialPayment,
+} = require('../tests/homescreen/actions');
+
+module.exports = function(testFeatures) {
+  const { features, preferences, options, title } = makeOptionsAndPreferences(
+    'emi',
+    testFeatures
+  );
+
+  const {
+    partialPayment,
+    feeBearer,
+    timeout,
+    callbackUrl,
+    offers,
+    optionalContact,
+    optionalEmail,
+  } = features;
+
+  describe.each(
+    getTestData(title, {
+      options,
+      preferences,
+    })
+  )('EMI tests', ({ preferences, title, options }) => {
+    test(title, async () => {
+      await setPreferenceForOffer(preferences);
+
+      const context = await openCheckoutWithNewHomeScreen({
+        page,
+        options,
+        preferences,
+      });
+
+      const missingUserDetails = optionalContact && optionalEmail;
+
+      const isHomeScreenSkipped = missingUserDetails && !partialPayment; // and not TPV
+
+      if (!isHomeScreenSkipped) {
+        await assertBasicDetailsScreen(context);
+      }
+
+      if (!missingUserDetails) {
+        await fillUserDetails(context, '8888888881');
+      }
+
+      if (partialPayment) {
+        await handlePartialPayment(context, '3000');
+      } else if (!isHomeScreenSkipped) {
+        await proceed(context);
+      }
+
+      if (!missingUserDetails) {
+        await assertUserDetails(context);
+        await assertEditUserDetailsAndBack(context);
+      }
+
+      await assertPaymentMethods(context);
+      await selectPaymentMethod(context, 'emi');
+      await enterCardDetails(context);
+      if (offers) {
+        await viewOffers(context);
+        await selectOffer(context, '6');
+        await verifyOfferApplied(context);
+      }
+
+      if (partialPayment) {
+        await verifyPartialAmount(context, '₹ 3,000');
+      }
+
+      if (callbackUrl && timeout) {
+        await verifyTimeout(context, 'netbanking');
+
+        return;
+      }
+
+      await submit(context);
+      if (offers) {
+        await verifyEMIPlansWithOffers(context, '2');
+        await selectEMIPlanWithOffer(context, '2');
+      } else {
+        await verifyEMIPlansWithoutOffers(context, '6');
+        await selectEMIPlanWithoutOffer(context, '2');
+      }
+      await submit(context);
+
+      if (feeBearer) {
+        await handleFeeBearer(context);
+      }
+
+      if (callbackUrl) {
+        await expectRedirectWithCallback(context, { method: 'emi' });
+      } else {
+        await handleEMIValidation(context);
+        await handleMockSuccessDialog(context);
+      }
+    });
+  });
+};
