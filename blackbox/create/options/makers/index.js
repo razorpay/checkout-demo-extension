d8b45123a0c6711983f15782c04a6a4ac65e7363
Baala <baalamurugan.sc@razorpay.com>
2020-03-16 11:41:51 +0530

    payLater Test Cases

--- blackbox/create/options/makers/index.js
+++ blackbox/create/options/makers/index.js
@@ -7,6 +7,7 @@ const cards = require('./cards');
 const emi = require('./emi');
 const tpv = require('./tpv');
 const wallet = require('./wallet');
+const payLater = require('./pay-later');
 
 module.exports = {
   netbanking: netbanking,
@@ -18,4 +19,5 @@ module.exports = {
   emi: emi,
   tpv: tpv,
   wallet: wallet,
+  'pay-later': payLater,
 };
