0c2ffee1ae9e016371b31f3c1d7b7050f181f8a1
Umang Galaiya <umang.galaiya@razorpay.com>
2020-03-16 12:55:08 +0530

    Clean up NewMethodsList.svelte

--- app/modules/ui/tabs/home/NewMethodsList.svelte
+++ app/modules/ui/tabs/home/NewMethodsList.svelte
@@ -1,18 +1,12 @@
 <script>
   // Svelte imports
-  import { createEventDispatcher, onDestroy } from 'svelte';
+  import { onDestroy } from 'svelte';
 
   // UI imports
-  import Method from 'ui/tabs/home/Method.svelte';
   import Instrument from 'ui/tabs/home/instruments/Instrument.svelte';
   import RazorpayCluster from 'ui/tabs/home/RazorpayCluster.svelte';
 
   // Utils imports
-  import { AVAILABLE_METHODS } from 'common/constants';
-  import { shouldSeparateDebitCard } from 'checkoutstore';
-  import { isMethodEnabled } from 'checkoutstore/methods';
-  import { isMobile } from 'common/useragent';
-  import { doesAppExist } from 'common/upi';
   import { showCtaWithDefaultText, hideCta } from 'checkoutstore/cta';
   import Analytics from 'analytics';
   import * as AnalyticsTypes from 'analytics-types';
@@ -20,66 +14,16 @@
 
   // Store
   import {
-    contact,
     selectedInstrument,
     selectedInstrumentId,
     blocks,
-    instruments,
   } from 'checkoutstore/screens/home';
 
   onDestroy(() => {
     deselectInstrument();
   });
 
-  const dispatch = createEventDispatcher();
   const session = getSession();
-
-  let visibleMethods = [];
-
-  function setMethods(methods) {
-    let available = _Arr.filter(AVAILABLE_METHODS, isMethodEnabled);
-
-    /**
-     * Cardless EMI and EMI are the same payment option.
-     * When we click EMI, it should take to Cardless EMI if
-     * cardless_emi is an available method.
-     */
-    if (
-      _Arr.contains(available, 'cardless_emi') &&
-      _Arr.contains(available, 'emi')
-    ) {
-      available = _Arr.remove(available, 'emi');
-    }
-
-    /**
-     * We do not want to show QR in the primary list
-     * of payment options anymore
-     */
-    available = _Arr.remove(available, 'qr');
-
-    // TODO: Filter based on amount
-
-    // Separate out debit and credit cards
-    if (shouldSeparateDebitCard()) {
-      available = _Arr.remove(available, 'card');
-      available = ['credit_card', 'debit_card'].concat(available);
-    }
-
-    visibleMethods = available;
-
-    const _methods = {};
-
-    _Arr.loop(visibleMethods, method => {
-      _methods[method] = true;
-    });
-
-    Analytics.track('methods:list', {
-      type: AnalyticsTypes.BEHAV,
-      data: {
-        methods: _methods,
-      },
-    });
-  }
 
   $: {
     if (session.screen === '') {
@@ -103,12 +47,6 @@
 
   function deselectInstrument() {
     $selectedInstrumentId = null;
-  }
-
-  setMethods();
-
-  function selectMethod(event) {
-    dispatch('selectMethod', event.detail);
   }
 </script>
 
