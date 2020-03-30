5c8bc4f574713b552317c00a8af40447e3fc4f73
Umang Galaiya <umang.galaiya@razorpay.com>
2020-03-30 16:35:25 +0530

    Use Promises instead of for loops with await

--- blackbox/tests/homescreen/checkout-config/config-utils.js
+++ blackbox/tests/homescreen/checkout-config/config-utils.js
@@ -8,33 +8,42 @@ const { innerText } = require('../../../util');
  */
 async function parseBlocksFromHomescreen(context) {
   const blockElements = await context.page.$$('.home-methods .methods-block');
-  let blocks = [];
+  let blocks = await Promise.all(
+    blockElements.map(
+      blockElement =>
+        new Promise(async resolve => {
+          const title = await innerText(
+            await blockElement.$(':scope > .title')
+          );
+          const subItemElements = await blockElement.$$(
+            ':scope > [role=list] > *'
+          );
+          let items = await Promise.all(
+            subItemElements.map(
+              subItemElement =>
+                new Promise(async resolve => {
+                  const itemTitle = await innerText(
+                    await subItemElement.$('[slot=title]')
+                  );
+                  const itemDescription = await innerText(
+                    await subItemElement.$('[slot=subtitle]')
+                  );
 
-  for (let i = 0; i < blockElements.length; i++) {
-    const blockElement = blockElements[i];
+                  resolve({
+                    title: itemTitle,
+                    description: itemDescription,
+                  });
+                })
+            )
+          );
 
-    const title = await innerText(await blockElement.$(':scope > .title'));
-    const subItemElements = await blockElement.$$(':scope > [role=list] > *');
-    let items = [];
-
-    for (let j = 0; j < subItemElements.length; j++) {
-      const subItemElement = subItemElements[j];
-      const itemTitle = await innerText(await subItemElement.$('[slot=title]'));
-      const itemDescription = await innerText(
-        await subItemElement.$('[slot=subtitle]')
-      );
-
-      items.push({
-        title: itemTitle,
-        description: itemDescription,
-      });
-    }
-
-    blocks.push({
-      title,
-      items,
-    });
-  }
+          resolve({
+            title,
+            items,
+          });
+        })
+    )
+  );
 
   return blocks;
 }
