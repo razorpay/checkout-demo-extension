39169ce8a108464b8e03a4199aad7f54feaeef5b
Chintan Acharya <chintan.acharya@razorpay.com>
2020-04-02 00:16:40 +0530

    Move field values to store

---
+++ app/modules/checkoutstore/screens/emandate.js
@@ -0,0 +1,6 @@
+import { writable } from 'svelte/store';
+
+export const accountNumber = writable('');
+export const bankName = writable('');
+export const ifsc = writable('');
+export const accountType = writable('');
--- app/modules/ui/tabs/emandate/index.svelte
+++ app/modules/ui/tabs/emandate/index.svelte
@@ -6,6 +6,14 @@
   import IfscField from 'ui/elements/fields/emandate/IfscField.svelte';
   import NameField from 'ui/elements/fields/emandate/NameField.svelte';
 
+  // Store imports
+  import {
+    accountNumber,
+    bankName,
+    ifsc,
+    accountType,
+  } from 'checkoutstore/screens/emandate';
+
   const session = getSession();
 
   // Prefill
@@ -16,21 +24,22 @@
   const prefilledName = session.get('prefill.bank_account[name]');
   const prefilledIfsc = session.get('prefill.bank_account[ifsc]');
 
-  var account_type = session.get('prefill.bank_account[account_type]');
+  var prefilledAccountType = session.get('prefill.bank_account[account_type]');
   var accountTexts = {
     savings: 'Savings Account',
     current: 'Current Account',
   };
   var accountTypes = _Obj.keys(accountTexts);
 
-  if (!_Arr.contains(accountTypes, account_type)) {
-    account_type = false;
+  if (!_Arr.contains(accountTypes, prefilledAccountType)) {
+    prefilledAccountType = false;
   }
 
-  // State
-  let accountNumber = prefillledBankAccount;
-  let bankName = prefilledName;
-  let ifsc = prefilledIfsc;
+  // Set prefill
+  $accountNumber = prefillledBankAccount;
+  $bankName = prefilledName;
+  $ifsc = prefilledIfsc;
+  $accountType = prefilledAccountType;
 
   const icons = session.themeMeta.icons;
 </script>
@@ -76,27 +85,33 @@
       name="bank_account[account_number]"
       id="nb-acc-no"
       readonly={Boolean(prefillledBankAccount)}
-      bind:value={accountNumber} />
+      bind:value={$accountNumber} />
 
     <IfscField
       id="nb-acc-ifsc"
       name="bank_account[ifsc]"
       readonly={Boolean(prefilledIfsc)}
-      bind:value={ifsc} />
+      bind:value={$ifsc} />
 
     <NameField
       id="nb-acc-name"
       name="bank_account[name]"
       readonly={Boolean(prefilledName)}
-      bind:value={bankName} />
+      bind:value={$bankName} />
 
     <div class="elem-wrap">
-      <div class="elem select" class:readonly={account_type}>
+      <div class="elem select" class:readonly={prefilledAccountType}>
         <i class="select-arrow"></i>
         <div class="help">Please select a bank account type</div>
-        <select name="bank_account[account_type]" required class="input">
-          {#if account_type}
-            <option value={account_type}>{accountTexts[account_type]}</option>
+        <select
+          name="bank_account[account_type]"
+          required
+          class="input"
+          bind:value={$accountType}>
+          {#if prefilledAccountType}
+            <option value={prefilledAccountType}>
+              {accountTexts[prefilledAccountType]}
+            </option>
           {:else}
             <option value="">Type of Bank Account</option>
             {#each accountTypes as type}
