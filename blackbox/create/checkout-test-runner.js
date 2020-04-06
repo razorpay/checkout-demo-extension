8d753c700aeb5be1e132694504936ec7b11fc294
saransh <saranshgupta1995@gmail.com>
2020-04-07 03:36:21 +0530

    Document public methods in runner

--- blackbox/create/checkout-test-runner.js
+++ blackbox/create/checkout-test-runner.js
@@ -16,11 +16,11 @@ class CheckoutTestRunner {
     this.targetFeature = null;
   }
 
-  sortingFunction(x, y) {
+  _sortingFunction(x, y) {
     return x > y ? 1 : -1;
   }
 
-  createTestCombs() {
+  _createTestCombs() {
     let exempted = this.exemptedFeatureCombinations;
     let tests = this.featuresToBeTested;
     let combs = [[]];
@@ -29,7 +29,7 @@ class CheckoutTestRunner {
 
       old_combs.forEach(comb => {
         if (comb.length < this.maxCombinationDepth) {
-          let newComb = [...comb, test].sort(this.sortingFunction);
+          let newComb = [...comb, test].sort(this._sortingFunction);
           if (
             exempted.find(
               combToBeBarred => combToBeBarred + '' === newComb + ''
@@ -44,23 +44,39 @@ class CheckoutTestRunner {
     return this;
   }
 
+  /**
+   * Used to set the list of features to be tests for any payment method
+   * @param {Array} listOfFeatures the list of features that the test runner instance will target
+   */
   setTestFeatures(listOfFeatures) {
-    this.featuresToBeTested = listOfFeatures.sort(this.sortingFunction);
-    return this.createTestCombs();
+    this.featuresToBeTested = listOfFeatures.sort(this._sortingFunction);
+    return this._createTestCombs();
   }
 
+  /**
+   * @description setting a target feature for the instance makes it run only those test combinations which include that feature
+   * @param {String} targetFeature name of the feature that is supposed to be checked
+   */
   setTargetFeature(targetFeature) {
     this.targetFeature = targetFeature;
     return this;
   }
 
+  /**
+   * Used to prevent some test combinations from running
+   * @param {Array} combinations All combinations which are to be exempted from running in the tests
+   */
   setExemptedTestCombinations(combinations) {
     this.exemptedFeatureCombinations = combinations.map(comb =>
-      comb.sort(this.sortingFunction)
+      comb.sort(this._sortingFunction)
     );
-    return this.createTestCombs();
+    return this._createTestCombs();
   }
 
+  /**
+   * Used to run all valid test combinations
+   * @param {Object} paymentMethodTestMaker takes a payment method test creation object on which tests are to be run
+   */
   runOn(paymentMethodTestMaker) {
     let runnables = this.allRunnableCombinations.map(comb =>
       comb.reduce((acc, cV) => {
