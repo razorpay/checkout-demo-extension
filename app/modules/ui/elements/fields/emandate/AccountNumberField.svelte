623b6533125c06dfb3886a92844fff32556ba8ce
Chintan Acharya <chintan.acharya@razorpay.com>
2020-04-13 14:17:55 +0530

    Add bank icon to account number field

--- app/modules/ui/elements/fields/emandate/AccountNumberField.svelte
+++ app/modules/ui/elements/fields/emandate/AccountNumberField.svelte
@@ -1,5 +1,9 @@
 <script>
+  // UI
   import Field from 'ui/components/Field.svelte';
+
+  // Utils
+  import { getBankLogo } from 'common/bank';
 
   export let id;
   export let name;
@@ -16,22 +20,50 @@
   }
 </script>
 
-<Field
-  type="text"
-  {name}
-  {id}
-  {readonly}
-  {value}
-  label="Bank Account Number"
-  helpText="Please enter a valid account number"
-  maxlength="20"
-  required={true}
-  formatter={{ type: 'alphanumeric' }}
-  pattern={PATTERN}
-  spellcheck="false"
-  autocorrect="off"
-  autocapitalize="off"
-  handleBlur
-  handleFocus
-  handleInput
-  on:input={handleInput} />
+<style>
+  .account-number-field {
+    position: relative;
+  }
+
+  .bank-icon {
+    position: absolute;
+    right: 0;
+    top: 24px;
+    width: 18px;
+    height: 18px;
+  }
+
+  .bank-icon img {
+    max-width: 100%;
+    max-height: 100%;
+  }
+</style>
+
+<div class="account-number-field">
+  {#if bankCode}
+    <div class="bank-icon">
+      <img src={getBankLogo(bankCode)} />
+    </div>
+  {/if}
+
+  <Field
+    type="text"
+    {name}
+    {id}
+    {readonly}
+    {value}
+    label="Bank Account Number"
+    helpText="Please enter a valid account number"
+    maxlength="20"
+    required={true}
+    formatter={{ type: 'alphanumeric' }}
+    pattern={PATTERN}
+    spellcheck="false"
+    autocorrect="off"
+    autocapitalize="off"
+    handleBlur
+    handleFocus
+    handleInput
+    on:input={handleInput} />
+
+</div>
