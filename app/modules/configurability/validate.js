686911f273357af394a967614de4a5c1de3128b1
Umang Galaiya <umang.galaiya@razorpay.com>
2020-04-20 17:17:15 +0530

    Add methods to validate instruments

---
+++ app/modules/configurability/validate.js
@@ -0,0 +1,51 @@
+const PAYMENT_VALIDATORS = {
+}
+
+/**
+ * Checks if the instrument is valid for the payment payload
+ * @param {Instrument} instrument
+ * @param {Object} payment Payment payload
+ *
+ * @returns {Promise<boolean>}
+ */
+export function isInstrumentValidForPayment(instrument, payment) {
+  const validator = PAYMENT_VALIDATORS[instrument.method];
+
+  if (!validator) {
+    return Promise.resolve(false); // Should reject but we'd need to polyfill Promise.allSettled for getValidPaymentInstruments
+  }
+
+  const validated = validator(payment, instrument);
+
+  return Promise.resolve(validated);
+}
+
+/**
+ * Returns the valid payment instruments
+ * @param {Object} payment Payment payload
+ * @param {Array<Instrument>} instruments
+ *
+ * @returns {Promise<Array<Instrument>>}
+ */
+export function getValidPaymentInstruments(payment, instruments) {
+  const methodInstruments = _Arr.filter(
+    instruments,
+    instrument => instrument.method === payment.method
+  );
+
+  const promises = _Arr.map(methodInstruments, instrument =>
+    isInstrumentValidForPayment(instrument, payment)
+  );
+
+  return Promise.all(promises).then(instrumentValidities => {
+    let validInstruments = [];
+
+    _Arr.loop(instrumentValidities, (valid, index) => {
+      if (valid) {
+        validInstruments.push(methodInstruments[index]);
+      }
+
+      return validInstruments;
+    });
+  });
+}
