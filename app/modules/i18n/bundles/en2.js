f489765e8c6b8867214573bcb8c3cc45d422e8e3
Chintan Acharya <chintan.acharya@razorpay.com>
2020-04-29 04:16:10 +0530

    Add additional language with overlay loader

---
+++ app/modules/i18n/bundles/en2.js
@@ -0,0 +1,6 @@
+export default {
+  netbanking: {
+    select_label: 'Select a different bank2',
+    select_help: 'Please select a bank2',
+  },
+};
--- app/modules/i18n/index.js
+++ app/modules/i18n/index.js
@@ -1,12 +1,39 @@
-import { addMessages, init as initSvelteI18n } from 'svelte-i18n';
+import {
+  addMessages,
+  init as initSvelteI18n,
+  register,
+  isLoading,
+} from 'svelte-i18n';
 
 import en from './bundles/en';
+import en2 from './bundles/en2';
+
+import { getSession } from 'sessionmanager';
+
+function fetchBundle(locale) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(en2), 3000);
+  });
+}
 
 export function init() {
   // Add bundled messages
   addMessages('en', en);
+  register('en2', () => fetchBundle('en2'));
 
-  // TODO: register for more languages
+  const session = getSession();
+
+  isLoading.subscribe(value => {
+    if (value) {
+      // TODO: lock overlay and prevent dismissal
+      session.showLoadError('Loading');
+    } else {
+      // TODO: fix this and remove try/catch
+      try {
+        session.hideOverlayMessage();
+      } catch (e) {}
+    }
+  });
 
   initSvelteI18n({
     fallbackLocale: 'en',
