74b1c8d577d306de1544b3b3f47dd94a814f893f
saransh <saranshgupta1995@gmail.com>
2020-05-07 12:46:03 +0530

    introduce formatted text svelte element

---
+++ app/modules/ui/elements/FormattedText.svelte
@@ -0,0 +1,46 @@
+<script>
+  export let text = '';
+  const formatters = [
+    {
+      flag: '**',
+      tag: 'bold',
+    },
+    {
+      flag: '__',
+      tag: 'italic',
+    },
+  ];
+
+  let tokenized = [
+    {
+      text: text,
+      type: 'plain',
+    },
+  ];
+
+  const tokenizeTextForFormatter = (tokenText, formatter) => {
+    return tokenText.split(formatter.flag).map((text, i) => {
+      return { text: text, type: i % 2 ? formatter.tag : 'plain' };
+    });
+  };
+
+  formatters.forEach(formatter => {
+    tokenized = tokenized.reduce((pV, cV) => {
+      if (cV.type === 'plain') {
+        return [...pV, ...tokenizeTextForFormatter(cV.text, formatter)];
+      }
+      pV.push(cV);
+      return pV;
+    }, []);
+  });
+</script>
+
+{#each tokenized as token}
+  {#if token.type === 'plain'}
+    {token.text}
+  {:else if token.type === 'bold'}
+    <strong>{token.text}</strong>
+  {:else if token.type === 'italic'}
+    <i>{token.text}</i>
+  {/if}
+{/each}
