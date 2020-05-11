290cdb28ca56a74f3ae6c01081727c247fa88257
saransh <saranshgupta1995@gmail.com>
2020-05-11 15:59:37 +0530

    use deepEqual

--- test/FormattedText/index.js
+++ test/FormattedText/index.js
@@ -9,25 +9,39 @@ test('Module: FormattedText', t => {
 
       textToBeFormatted = 'This __might__ work';
 
-      t.equal(
-        JSON.stringify(tokenizeTextForFormatters(textToBeFormatted)),
-        '[{"text":"This ","type":"plain"},{"text":"might","type":"italic"},{"text":" work","type":"plain"}]',
+      t.deepEqual(
+        tokenizeTextForFormatters(textToBeFormatted),
+        [
+          { text: 'This ', type: 'plain' },
+          { text: 'might', type: 'italic' },
+          { text: ' work', type: 'plain' },
+        ],
         'works for just italic in string'
       );
 
       textToBeFormatted = 'This **might** work';
 
-      t.equal(
-        JSON.stringify(tokenizeTextForFormatters(textToBeFormatted)),
-        '[{"text":"This ","type":"plain"},{"text":"might","type":"bold"},{"text":" work","type":"plain"}]',
+      t.deepEqual(
+        tokenizeTextForFormatters(textToBeFormatted),
+        [
+          { text: 'This ', type: 'plain' },
+          { text: 'might', type: 'bold' },
+          { text: ' work', type: 'plain' },
+        ],
         'works for just bold in string'
       );
 
       textToBeFormatted = 'This **might** __work__';
 
-      t.equal(
-        JSON.stringify(tokenizeTextForFormatters(textToBeFormatted)),
-        '[{"text":"This ","type":"plain"},{"text":"might","type":"bold"},{"text":" ","type":"plain"},{"text":"work","type":"italic"},{"text":"","type":"plain"}]',
+      t.deepEqual(
+        tokenizeTextForFormatters(textToBeFormatted),
+        [
+          { text: 'This ', type: 'plain' },
+          { text: 'might', type: 'bold' },
+          { text: ' ', type: 'plain' },
+          { text: 'work', type: 'italic' },
+          { text: '', type: 'plain' },
+        ],
         'works for both bold and italic in string'
       );
 
