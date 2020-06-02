9a00fb24a5bfcd00a688a1d794c9513fc70ee7dd
rajat-chowdhary-rzp <56256133+rajat-chowdhary-rzp@users.noreply.github.com>
2020-06-02 14:14:31 +0530

    Ce 1411/poweredby (#1672)
    * PoweredBy.svelte
    
    * destroy powered by on modal close
    
    * remove session.onClose
    
    * fix import
    
    * fix warning
    
    Co-authored-by: Pranav Gupta <pranavguptarulz@gmail.com>

--- app/js/checkout-frame/session.js
+++ app/js/checkout-frame/session.js
@@ -62,8 +62,6 @@ var strings = {
   otp_resent: 'OTP resent',
 };
 
-var fontTimeout;
-
 /**
  * Temp stores for Cardless EMI & PayLater.
  * Will move to Svelte Store upon migration.
@@ -909,10 +907,6 @@ Session.prototype = {
       classes.push('test');
     }
 
-    if (this.fontLoaded) {
-      classes.push('font-loaded');
-    }
-
     if (getter('theme.hide_topbar')) {
       classes.push('notopbar');
     }
@@ -950,7 +944,6 @@ Session.prototype = {
         cta: storeGetter(Cta.getStore()),
       });
       this.el = div.firstChild;
-      this.applyFont(this.el.querySelector('#powered-link'));
       document.body.appendChild(this.el);
 
       this.el.appendChild(styleEl);
@@ -1195,6 +1188,7 @@ Session.prototype = {
     this.setFormatting();
     this.improvisePaymentOptions();
     this.improvisePrefill();
+    discreet.es6components.render();
     this.setSvelteComponents();
     this.fillData();
     this.setEMI();
@@ -2050,39 +2044,6 @@ Session.prototype = {
     discreet.Theme.setThemeColor(this.r.themeMeta.color);
 
     this.themeMeta = discreet.Theme.getThemeMeta();
-  },
-
-  applyFont: function(anchor, retryCount) {
-    if (!retryCount) {
-      retryCount = 0;
-    }
-    if (anchor.offsetWidth / anchor.offsetHeight > 3) {
-      $(this.el).addClass('font-loaded');
-      this.fontLoaded = true;
-      this.applyPartnershipBranding();
-    } else if (retryCount < 25) {
-      var self = this;
-      fontTimeout = setTimeout(function() {
-        self.applyFont(anchor, ++retryCount);
-      }, 120 + retryCount * 50);
-    }
-  },
-
-  /**
-   * Applies the In Partnership With branding
-   */
-  applyPartnershipBranding: function() {
-    var brand_logo = this.get('partnership_logo');
-
-    if (brand_logo) {
-      var elem = _Doc.querySelector('#powered-by');
-
-      _El.addClass(elem, 'branded');
-      _El.setContents(
-        elem,
-        'In partnership with<br><img src="' + brand_logo + '">'
-      );
-    }
   },
 
   hideErrorMessage: function(confirmedCancel) {
@@ -5095,8 +5056,8 @@ Session.prototype = {
       abortAjax(this.ajax);
       this.clearRequest(cancelReason);
       this.isOpen = false;
-      clearTimeout(fontTimeout);
 
+      discreet.es6components.destroy();
       this.cleanUpSvelteComponents();
 
       try {
---
+++ app/modules/ui/components/PoweredBy.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { getOption } from 'checkoutstore';
+  import { onMount } from 'svelte';
+
+  const customLogo = getOption('partnership_logo');
+  let fontLoaded = false;
+  let fontTimeout = null;
+  let ref;
+
+  onMount(() => {
+    applyFont(ref, 0);
+  });
+
+  function applyFont(anchor, retryCount) {
+    if (anchor.offsetWidth / anchor.offsetHeight > 3) {
+      _El.addClass(_Doc.querySelector('#container'), 'font-loaded');
+      fontLoaded = true;
+    } else if (retryCount < 25) {
+      fontTimeout = setTimeout(
+        () => applyFont(anchor, ++retryCount),
+        120 + retryCount * 50
+      );
+    }
+  }
+</script>
+
+<i id="powered-by" class:branded={customLogo && fontLoaded}>
+  {#if customLogo && fontLoaded}
+    In partnership with
+    <br />
+    <img alt="Logo" src={customLogo} />
+  {:else}
+    <a
+      bind:this={ref}
+      href="https://razorpay.com?ref=org-in-chk"
+      target="_blank"
+      tabindex="-1">
+      &#xe608;
+    </a>
+  {/if}
+</i>
