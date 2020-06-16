07e15b45e62c1a5c44985050ce6cb688d6519e80
Chintan Acharya <chintan.acharya@razorpay.com>
2020-06-17 04:11:43 +0530

    Fix float issues

--- app/modules/ui/elements/LanguageSelection.svelte
+++ app/modules/ui/elements/LanguageSelection.svelte
@@ -32,10 +32,11 @@
 
 <style>
   .outer {
-    margin: 16px -24px -24px;
+    margin: 0 -24px -24px;
     padding: 6px 24px;
     height: 20px;
     background: rgba(0, 0, 0, 0.1);
+    clear: both;
   }
 
   .selected {
