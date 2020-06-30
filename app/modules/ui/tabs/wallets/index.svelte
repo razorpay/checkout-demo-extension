f08a2d27ebfcaa1bd6fda31e4b9990af874c88eb
maskedTitan <harshamarri18@gmail.com>
2020-06-30 09:25:01 +0530

    moved function sendWalletBalanceEvents from session.js to wallets UI component

--- app/modules/ui/tabs/wallets/index.svelte
+++ app/modules/ui/tabs/wallets/index.svelte
@@ -111,6 +111,7 @@
         scrollIntoView(walletReferences[selectedWallet]);
       }, 200);
     }
+    sendWalletBalanceEvents();
   }
 
   // Called when the user presses the pay button
@@ -118,6 +119,23 @@
     const payload = {
       wallet: selectedWallet,
     };
+
+    function sendWalletBalanceEvents() {
+      Analytics.track('wallet:balance:add', {
+        type: AnalyticsTypes.BEHAV,
+        data: {
+          wallet: this.payload && this.payload.wallet,
+        },
+      });
+      this.otpView.setTextView('loading');
+      this.otpView.updateScreen({
+        action: false,
+        loading: true,
+        addFunds: false,
+      });
+      this.powerwallet = false;
+      this.r.topupWallet();
+    }
 
     /**
      * Wallets might need to go through intent flow too
