9abac77bb38c8ec02c96d368c4e4a59cdbd97a5b
Umang Galaiya <umang.galaiya@razorpay.com>
2020-07-07 12:20:44 +0530

    Use minified script

--- app/modules/payment/popup/template.js
+++ app/modules/payment/popup/template.js
@@ -16,6 +16,106 @@ const makeTrackingScript = ({ checkout_id, live, library }) => {
   }
 
   return `
+    <script>
+      var events={page:"checkout_popup",props:{checkout_id:"${checkout_id}",library:"${library}"},load:!0,unload:!0};!function(e){e.track=Boolean;try{if("object"!=typeof e.events)return;var n,t=e.events.props,o=e.events,a="https://lumberjack.razorpay.com/v1/track",r="ZmY5N2M0YzVkN2JiYzkyMWM1ZmVmYWJk",c="function"==typeof navigator.sendBeacon,s=Date.now(),i=[{name:"ua_parser",input_key:"user_agent",output_key:"user_agent_parsed"}];function p(e,p){(p=p||{}).beacon=c,p.time_since_render=Date.now()-s,p.url=location.href,function(e,n){if(e&&n)Object.keys(n).forEach(function(t){e[t]=n[t]})}(p,t);var u={addons:i,events:[{event:o.page+":"+e,properties:p,timestamp:Date.now()}]},d=encodeURIComponent(btoa(unescape(encodeURIComponent(JSON.stringify(u))))),f=JSON.stringify({key:r,data:d});c?navigator.sendBeacon(a,f):((n=new XMLHttpRequest).open("post",a,!0),n.send(f))}p("load"),e.addEventListener("beforeunload",function(){p("unload")}),e.addEventListener("error",function(e){p("js_error",{message:e.message,line:e.line,col:e.col,stack:e.error&&e.error.stack})})}catch(e){}e.track=p}(window);
+    </script>
+  `;
+};
+
+export default function popupTemplate(_) {
+  var get = _.r.get;
+  var method = _.data && _.data.method === 'wallet' ? 'wallet' : 'bank';
+  var color = get('theme.color') || '#3594E2';
+  var highlightColor = _.r.themeMeta.highlightColor;
+  var title =
+    get('name') || get('description') || 'Redirecting...'
+    |> sanitizeHtmlEntities;
+  var amount = displayAmount(
+    _.r,
+    _.data && _.data.amount,
+    _.data && _.data.currency
+  );
+
+  var dccCurrency = _.data && _.data.dcc_currency;
+  if (dccCurrency) {
+    var dccAmount = getConvertedAmount(_.data.amount, dccCurrency);
+    amount = displayAmount(_.r, dccAmount, dccCurrency);
+  }
+
+  var hideAmount =
+    _.data && _.data.method === 'emandate' ? 'display: none;' : '';
+
+  var image = get('image');
+  image = image
+    ? `<div id="logo"><img src="${image.replace(/"/g, '')}"/></div>`
+    : '';
+
+  var message =
+    _.message ||
+      'Please wait while we redirect you to your ' + method + ' page.'
+    |> sanitizeHtmlEntities;
+
+  return `<!doctype html><html style="height:100%;width:100%;"><head>
+<title>Processing, Please Wait...</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="theme-color" content="${color}">
+<style>${css}#ldr:after{background:${highlightColor}}#bg{background:${color}}
+@media(max-height:580px),(max-width:420px){body{background:${color}}}
+</style>
+</head><body><div id='bg'></div><div id='cntnt'>
+<div id="hdr">${image}
+  <div id='name'>${title}</div>
+  <div id="amt" style="${hideAmount}">
+    <div style="font-size:12px;color:#757575;line-height:15px;margin-bottom:5px;text-align:right">PAYING</div>
+    <div dir="ltr" style="font-size:20px;line-height:24px;">${amount}</div>
+  </div>
+</div>
+<div id="ldr"></div>
+<div id="txt">
+  <div style="display:inline-block;vertical-align:middle;white-space:normal;">
+    <h2 id='title'>Loading ${method} page…</h2><p id='msg'>${message}</p>
+  </div>
+  <div style="display:inline-block;vertical-align:middle;height:100%"></div>
+</div>
+<div id='ftr'>
+  <div style="display:inline-block;">Secured by
+    <img style="vertical-align:middle;margin-bottom:5px;" height="20px" src="https://cdn.razorpay.com/logo.svg">
+  </div>
+  <div style="display:inline-block;vertical-align:middle;height:100%"></div>
+</div>
+</div>
+<div style="display:inline-block;vertical-align:middle;height:100%"></div>
+<script>
+var doc = document;
+var gel = doc.getElementById.bind(doc);
+setTimeout(function(){doc.body.className='loaded'}, 10);
+setTimeout(function(){
+  gel('title').innerHTML = 'Still trying to load...';
+  gel('msg').innerHTML = 'The bank page is taking time to load. You can either wait or <span id="cncl">change the payment method</span>.';
+  gel('cncl').onclick = function(){
+    if(window.confirm("Do you want to cancel the ongoing payment?")){
+      window.close();
+      if (CheckoutBridge && CheckoutBridge.oncomplete) {
+        CheckoutBridge.oncomplete('${cancelError}');
+      }
+    }
+  };
+},1e4)
+</script>
+<form></form>
+${makeTrackingScript({
+  live: _.r.isLiveMode(),
+  checkout_id: _.r.id,
+  library: Track.props.library,
+})}
+</body>
+</html>`;
+}
+
+/**
+
+Script used in string: Minify when being used.
+
 <script>
 var events = {
   page: 'checkout_popup',
@@ -109,95 +209,6 @@ var events = {
   } catch (e) {}
   window.track = track;
 })(window);
-</script>`;
-};
+</script>`
 
-export default function popupTemplate(_) {
-  var get = _.r.get;
-  var method = _.data && _.data.method === 'wallet' ? 'wallet' : 'bank';
-  var color = get('theme.color') || '#3594E2';
-  var highlightColor = _.r.themeMeta.highlightColor;
-  var title =
-    get('name') || get('description') || 'Redirecting...'
-    |> sanitizeHtmlEntities;
-  var amount = displayAmount(
-    _.r,
-    _.data && _.data.amount,
-    _.data && _.data.currency
-  );
-
-  var dccCurrency = _.data && _.data.dcc_currency;
-  if (dccCurrency) {
-    var dccAmount = getConvertedAmount(_.data.amount, dccCurrency);
-    amount = displayAmount(_.r, dccAmount, dccCurrency);
-  }
-
-  var hideAmount =
-    _.data && _.data.method === 'emandate' ? 'display: none;' : '';
-
-  var image = get('image');
-  image = image
-    ? `<div id="logo"><img src="${image.replace(/"/g, '')}"/></div>`
-    : '';
-
-  var message =
-    _.message ||
-      'Please wait while we redirect you to your ' + method + ' page.'
-    |> sanitizeHtmlEntities;
-
-  return `<!doctype html><html style="height:100%;width:100%;"><head>
-<title>Processing, Please Wait...</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="theme-color" content="${color}">
-<style>${css}#ldr:after{background:${highlightColor}}#bg{background:${color}}
-@media(max-height:580px),(max-width:420px){body{background:${color}}}
-</style>
-</head><body><div id='bg'></div><div id='cntnt'>
-<div id="hdr">${image}
-  <div id='name'>${title}</div>
-  <div id="amt" style="${hideAmount}">
-    <div style="font-size:12px;color:#757575;line-height:15px;margin-bottom:5px;text-align:right">PAYING</div>
-    <div dir="ltr" style="font-size:20px;line-height:24px;">${amount}</div>
-  </div>
-</div>
-<div id="ldr"></div>
-<div id="txt">
-  <div style="display:inline-block;vertical-align:middle;white-space:normal;">
-    <h2 id='title'>Loading ${method} page…</h2><p id='msg'>${message}</p>
-  </div>
-  <div style="display:inline-block;vertical-align:middle;height:100%"></div>
-</div>
-<div id='ftr'>
-  <div style="display:inline-block;">Secured by
-    <img style="vertical-align:middle;margin-bottom:5px;" height="20px" src="https://cdn.razorpay.com/logo.svg">
-  </div>
-  <div style="display:inline-block;vertical-align:middle;height:100%"></div>
-</div>
-</div>
-<div style="display:inline-block;vertical-align:middle;height:100%"></div>
-<script>
-var doc = document;
-var gel = doc.getElementById.bind(doc);
-setTimeout(function(){doc.body.className='loaded'}, 10);
-setTimeout(function(){
-  gel('title').innerHTML = 'Still trying to load...';
-  gel('msg').innerHTML = 'The bank page is taking time to load. You can either wait or <span id="cncl">change the payment method</span>.';
-  gel('cncl').onclick = function(){
-    if(window.confirm("Do you want to cancel the ongoing payment?")){
-      window.close();
-      if (CheckoutBridge && CheckoutBridge.oncomplete) {
-        CheckoutBridge.oncomplete('${cancelError}');
-      }
-    }
-  };
-},1e4)
-</script>
-<form></form>
-${makeTrackingScript({
-  live: _.r.isLiveMode(),
-  checkout_id: _.r.id,
-  library: Track.props.library,
-})}
-</body>
-</html>`;
-}
+ */
