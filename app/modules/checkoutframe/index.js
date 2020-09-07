873c8ec59b4c9d808bf20a06dc2771458d7c2ea9
saransh <saranshgupta1995@gmail.com>
2020-09-07 10:43:42 +0530

    filter duplicates for preferences as well

--- app/modules/checkoutframe/index.js
+++ app/modules/checkoutframe/index.js
@@ -14,6 +14,7 @@ import showTimer from 'checkoutframe/timer';
 import {
   setInstrumentsForCustomer,
   trackP13nMeta,
+  removeDuplicateApiInstruments,
 } from 'checkoutframe/personalization/api';
 import { setHistoryAndListenForBackPresses } from 'bridge/back';
 
@@ -377,7 +378,7 @@ function updatePreferredMethods(preferences) {
           {
             contact,
           },
-          instruments
+          removeDuplicateApiInstruments(instruments)
         );
       }
     });
