4dccec43e9f5d833f54afb96f2152f6212731e72
saransh <saranshgupta1995@gmail.com>
2020-09-23 14:52:19 +0530

    remove redundant run

--- blackbox/tests/homescreen/cardlessEmi/zestmoney/runner.test.js
+++ blackbox/tests/homescreen/cardlessEmi/zestmoney/runner.test.js
@@ -1,6 +1,4 @@
 const createCardlessEMITest = require('../../../../create/cardless-emi');
-
-createCardlessEMITest();
 const testRunner = require('../../../../create/checkout-test-runner');
 
 testRunner
