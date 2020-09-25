54c8e9b8a32487d6ce27dd71c86e5c1e48822ef7
saransh <saranshgupta1995@gmail.com>
2020-09-25 12:30:44 +0530

    run all combinations

--- blackbox/tests/homescreen/upiWebPayments/runner.test.js
+++ blackbox/tests/homescreen/upiWebPayments/runner.test.js
@@ -2,7 +2,7 @@ const createWebPaymentsTests = require('../../../create/upi-web-payments');
 const testRunner = require('../../../create/checkout-test-runner');
 
 testRunner
-  .setTestFeatures([])
+  .setTestFeatures(testRunner.globalFeatureList)
   .setExemptedTestCombinations([
     ['offers', 'partialPayment'],
     ['downtimeHigh', 'downtimeLow'],
