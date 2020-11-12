5149d2e2c5fb858a68121fc0d093ab58b336d069
Mayank Singhal <mayanks3005@gmail.com>
2020-11-12 16:57:45 +0530

    partnership logo changes

--- app/modules/payment/index.js
+++ app/modules/payment/index.js
@@ -33,6 +33,7 @@ import { getCardEntityFromPayload, getCardFeatures } from 'common/card';
  * the same domain as the configured API
  */
 const isRazorpayFrame = () => {
+  return false;
   return _Str.startsWith(
     RazorpayConfig.api,
     `${location.protocol}//${location.hostname}`
