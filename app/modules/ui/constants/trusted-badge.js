a0c96dab9148d59cfb882ab9e26337f9688a42cd
Mayank Singhal <mayanks3005@gmail.com>
2020-12-04 18:59:40 +0530

    added prod merchants

--- app/modules/ui/constants/trusted-badge.js
+++ app/modules/ui/constants/trusted-badge.js
@@ -1,4 +1,19 @@
 const badges_text = {
+  FjZg37JOiJDI0K: {
+    list: getList('50+', '3 months', '3 months'),
+  },
+  FnsHdtMjq1a7j7: {
+    list: getList('150+', '3 months', '3 months'),
+  },
+  FhZlzY1EMcTImZ: {
+    list: getList('300+', '3 months', '3 months'),
+  },
+  FFsK7Ojsv5B8cb: {
+    list: getList('400+', '4 months', '4 months'),
+  },
+  CDa7YMwXiWqCZz: {
+    list: getList('600+', '20 months', '20 months'),
+  },
   '10000000000000': {
     list: getList('10k+', '2+ years', '3 months'),
   },
