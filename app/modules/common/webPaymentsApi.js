2a637d7a723d18e6ee7644e5252d74dc2affd012
Chintan Acharya <chintan.acharya@razorpay.com>
2020-12-10 13:27:49 +0530

    Remove session dependency

--- app/modules/common/webPaymentsApi.js
+++ app/modules/common/webPaymentsApi.js
@@ -2,7 +2,7 @@ import { getSession } from 'sessionmanager';
 import * as Bridge from 'bridge';
 import { GOOGLE_PAY_PACKAGE_NAME, PHONE_PE_PACKAGE_NAME } from 'common/upi';
 import { setUpiApps, getUPIIntentApps } from 'checkoutstore/native';
-import { phonepeSupportedMethods } from 'payment/adapters';
+import { checkPaymentAdapter, phonepeSupportedMethods } from 'payment/adapters';
 
 export const appsThatSupportWebPayments = [
   GOOGLE_PAY_PACKAGE_NAME,
@@ -30,14 +30,12 @@ export function isWebPaymentsApiAvailable(app) {
 }
 
 export const checkWebPaymentsForApp = app => {
-  const session = getSession();
-
   /* disable Web payments API for SDK as we have native intent there */
   if (Bridge.checkout.exists()) {
     return;
   }
 
-  session.r.checkPaymentAdapter(app).then(() => {
+  checkPaymentAdapter(app).then(() => {
     webPaymentsApps[app] = true;
     setUpiApps(
       _Arr.mergeWith(getUPIIntentApps().all, [
