9a3785f399b9e3973f31dc6f231d42a2445159cd
Mayank Singhal <mayanks3005@gmail.com>
2020-12-11 12:18:11 +0530

    added params to the rewards req in test

--- blackbox/actions/rewards.js
+++ blackbox/actions/rewards.js
@@ -14,19 +14,19 @@ async function sendRewards({ expectRequest, respondJSON, options, rewards }) {
   await expectRequest(({ URL, params }) => {
     expect(URL.pathname).toEqual('/v1/rewards');
 
-    // if (options.key) {
-    //   expect(options.key).toEqual(params.key_id);
-    // } else {
-    //   expect(params).not.toHaveProperty('key_id');
-    // }
+    if (options.key) {
+      expect(options.key).toEqual(params.key_id);
+    } else {
+      expect(params).not.toHaveProperty('key_id');
+    }
 
-    // preferencesParams.forEach(param => {
-    //   if (options[param]) {
-    //     expect(options[param]).toEqual(params[param]);
-    //   } else {
-    //     expect(params).not.toHaveProperty(param);
-    //   }
-    // });
+    preferencesParams.forEach(param => {
+      if (options[param]) {
+        expect(options[param]).toEqual(params[param]);
+      } else {
+        expect(params).not.toHaveProperty(param);
+      }
+    });
   });
   await respondJSON(makeRewards());
 }
