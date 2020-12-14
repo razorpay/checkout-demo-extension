3adc6a67d5635a81fa7745117d28a6c2f4fde384
Mayank Singhal <mayanks3005@gmail.com>
2020-12-14 17:43:39 +0530

    table ui completed

--- app/modules/ui/tabs/bank-transfer/NeftPrintView.svelte
+++ app/modules/ui/tabs/bank-transfer/NeftPrintView.svelte
@@ -5,51 +5,155 @@
   import { getCustomerDetails } from 'checkoutstore/screens/home';
   import { onMount } from 'svelte';
 
+  import { t } from 'svelte-i18n';
+
+  const HEADER = 'For RTGS/NEFT/Funds Transfer';
+  const ROW_HEADERS = {
+    row1: 'Beneficiary Name',
+    row2: 'Account No.',
+    row3: 'IFSC Code',
+    row4: 'Bank',
+    row5: 'Branch',
+    row6: 'Amount',
+    row7: 'Customer Name',
+    row8: 'Customer Email ID',
+    row9: 'Customer Mobile No',
+    row10: 'Description',
+    row11: 'Merchant Order ID',
+    row12: 'Disclaimers',
+  };
+  const OFFICE_USE = {
+    header: '(FOR BANK USE ONLY)',
+    list: ['Amount (Rs.)', 'Debit A/C No.', 'UTR No.'],
+  };
+  const DISCLAIMERS = [
+    'This challan can be used only for electronic funds transfer to provided account no. fromHDFC Bank and Other Banks through intra bank Funds Transfer or RTGS / NEFT.',
+    "It is remitter's responsibility to remit the funds to correct account no. with correctamount as provided above. Incase of any mismatch in account no or amount, the transaction willbe rejected and funds will be refunded back to remitter account by next working day.",
+    'IMPS transactions are not allowed into provided account no. Bank shall not be liable incaseof IMPS transactions are not updated successfully or not refunded back to remitter. (This isrequired as solution for refund of IMPS rejections is yet to be finalized)',
+    'This challan is valid for only one transaction hence Remitter has to generate new challanfor every payment.',
+  ];
+  const DISCLAIMER_LABEL = 'Disclaimer';
+  const DIPOSITOR_SIGN_LABEL = 'Signature of Depositor';
+  const AUTH_SIGN_LABEL = 'Authorised Signatory';
+  const BRANCH_LABEL = 'Branch Stamp';
+
   let neftView;
   export let customerDetails = getCustomerDetails();
   export let neftDetails;
-  export let amount = getAmount();
-  // export let { contact, email } = customerDetails;
-  export let description = getOption('description');
-  export let orderId = getOption('order_id');
+  const { contact, email, name } = customerDetails;
+  let description = getOption('description');
+  let orderId = getOption('order_id');
 
   onMount(() => {
     const currentWindow = neftView.ownerDocument.defaultView;
-    currentWindow.print();
-    currentWindow.close();
+    // currentWindow.print();
+    // currentWindow.close();
   });
+  const tableDetails = {
+    [ROW_HEADERS.row1]: neftDetails.name,
+    [ROW_HEADERS.row2]: neftDetails.account_number,
+    [ROW_HEADERS.row3]: neftDetails.ifsc,
+    [ROW_HEADERS.row4]: neftDetails.bank_name,
+    [ROW_HEADERS.row5]: neftDetails.branch,
+    [ROW_HEADERS.row6]: getAmount(),
+    [ROW_HEADERS.row7]: name,
+    [ROW_HEADERS.row8]: email,
+    [ROW_HEADERS.row9]: contact,
+    [ROW_HEADERS.row10]: description,
+    [ROW_HEADERS.row11]: orderId,
+  };
 </script>
 
 <div class="neft-print-view" bind:this={neftView}>
-  <h2>Challan Payment Details</h2>
-
-  <footer>
-    <p>
-      <b>Note:</b>
-      Please pay the exact amount to the last paisa. Incorrect amount and Bank
-      Account details will lead to rejection of deposit.
-    </p>
-    <p>Do not make multiple transfers using the same challan.</p>
-    <p>
-      The contents of this challan should not be modified when presented to any
-      bank for remittance of funds.
-    </p>
-    <p>
-      You may also add this account number/IFSC code as beneficiary in your
-      Netbanking account and initiate NEFT / RTGS / IMPS funds transfer.
-    </p>
-    <p>Please use your own Challan details while doing Transaction.</p>
-    <p>
-      NEFT needs to be deposited against the account number and for the exact
-      amount mentioned above only.
-    </p>
-    <p>
-      Kindly ensure the transaction is made online or via branch well before the
-      Challan expiry date mentioned above.
-    </p>
-    <p>
-      In case you fail to deposit the NEFT within Challan expiry date, please
-      regenerate the Challan.
-    </p>
-  </footer>
+  <style>
+    .neft-print-view,
+    div,
+    p,
+    table {
+      font-family: 'PT Mono', 'Monospace', 'Consolas', 'sans-serif';
+      font-size: 14px;
+    }
+    .text-center {
+      text-align: center;
+    }
+    .text-left {
+      text-align: left;
+    }
+    .text-right {
+      text-align: right;
+    }
+    .bank-transfer-table {
+      width: 100%;
+      border-collapse: collapse;
+    }
+    td,
+    th {
+      padding: 5px;
+      border: 1px solid;
+    }
+    .footer-logo {
+      margin-top: 5px;
+    }
+  </style>
+  <!-- <img /> Logo -->
+  <table class="bank-transfer-table">
+    <tr>
+      <td colspan="2" class="text-center">{HEADER}</td>
+    </tr>
+    <tr>
+      <td colspan="2" class="text-left">Date: 12/12/2020</td>
+    </tr>
+    {#each Object.keys(tableDetails) as key}
+      <tr>
+        <th class="text-left" width="30%">{key}:</th>
+        <td class="text-left">{tableDetails[key] || ''}</td>
+      </tr>
+    {/each}
+    <tr>
+      <th colspan="2" class="text-left">{DISCLAIMER_LABEL}:</th>
+    </tr>
+    <tr>
+      <td colspan="2">&nbsp;</td>
+    </tr>
+    {#each DISCLAIMERS as dis, ind}
+      <tr>
+        <td colspan="2">{`${ind + 1}.) ${dis}`}</td>
+      </tr>
+    {/each}
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <th colspan="2" class="text-right">
+        <br /><br /><br />{DIPOSITOR_SIGN_LABEL}
+      </th>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td colspan="2">{OFFICE_USE.header}</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    {#each OFFICE_USE.list as off}
+      <tr>
+        <th class="text-left">{off}</th>
+        <td>&nbsp;</td>
+      </tr>
+    {/each}
+    <tr>
+      <th class="text-right" colspan="2">{AUTH_SIGN_LABEL}</th>
+    </tr>
+    <tr>
+      <th class="text-right" colspan="2">{BRANCH_LABEL}</th>
+    </tr>
+  </table>
+  <div class="text-right footer-logo">
+    <img src="https://cdn.razorpay.com/static/assets/secured_by_razorpay.svg" />
+  </div>
 </div>
