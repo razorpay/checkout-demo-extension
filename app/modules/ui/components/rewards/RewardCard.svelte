86ed41486f6cf6c7849fc94e03f75f20c02cbd18
Mayank Singhal <mayanks3005@gmail.com>
2020-12-16 13:13:33 +0530

    design changes

--- app/modules/ui/components/rewards/RewardCard.svelte
+++ app/modules/ui/components/rewards/RewardCard.svelte
@@ -6,6 +6,7 @@
   // props
   export let logo;
   export let reward_id;
+  export let name;
 
   function handleCardClick() {
     Analytics.track('rewards:icon:show', {
@@ -30,14 +31,27 @@
     box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.18);
     border-radius: 5px;
     flex-shrink: 0;
+    flex-wrap: wrap;
   }
   :global(.mobile) .reward-card {
     height: 177px;
     width: 189px;
     margin-right: 20px;
   }
+  .full-width {
+    width: 100%;
+  }
+  :global(.mobile) .reward-logo {
+    width: 75px;
+  }
+  :global(.mobile) .reward-text {
+    font-size: 16px;
+  }
 </style>
 
 <div class="carousel-slide reward-card" on:click={handleCardClick}>
-  <img src={logo} alt="Reward Logo" />
+  <div class="full-width">
+    <img class="reward-logo" src={logo} alt="Reward Logo" />
+  </div>
+  <div class="full-width reward-text"><b>{name}</b></div>
 </div>
