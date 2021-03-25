19e0d834fd15020775b68bc94c2b90be46ba0d88
Mayank Singhal <mayanks3005@gmail.com>
2021-03-25 17:46:27 +0530

    dummy commit

--- app/modules/ui/components/rewards/Animation.svelte
+++ app/modules/ui/components/rewards/Animation.svelte
@@ -24,6 +24,7 @@ technique
 
 	confetti.start = startConfetti;
 	confetti.stop = stopConfetti;
+
 	const supportsAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame;
 	const colors = ["rgba(30,144,255,", "rgba(107,142,35,", "rgba(255,215,0,", "rgba(255,192,203,", "rgba(106,90,205,", "rgba(173,216,230,", "rgba(238,130,238,", "rgba(152,251,152,", "rgba(70,130,180,", "rgba(244,164,96,", "rgba(210,105,30,", "rgba(220,20,60,"];
 	const particles = [];
