b9a1c6872afb43aeda424d7a5f79a86e52aaea8d
Mayank Singhal <mayanks3005@gmail.com>
2021-03-25 17:23:32 +0530

    lint fixes

--- app/modules/ui/components/rewards/Animation.svelte
+++ app/modules/ui/components/rewards/Animation.svelte
@@ -24,13 +24,14 @@ technique
 
 	confetti.start = startConfetti;
 	confetti.stop = stopConfetti;
-	var supportsAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame;
-	var colors = ["rgba(30,144,255,", "rgba(107,142,35,", "rgba(255,215,0,", "rgba(255,192,203,", "rgba(106,90,205,", "rgba(173,216,230,", "rgba(238,130,238,", "rgba(152,251,152,", "rgba(70,130,180,", "rgba(244,164,96,", "rgba(210,105,30,", "rgba(220,20,60,"];
-	var streamingConfetti = false;
-	var lastFrameTime = Date.now();
-	var particles = [];
-	var waveAngle = 0;
-	var context = null;
+	const supportsAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame;
+	const colors = ["rgba(30,144,255,", "rgba(107,142,35,", "rgba(255,215,0,", "rgba(255,192,203,", "rgba(106,90,205,", "rgba(173,216,230,", "rgba(238,130,238,", "rgba(152,251,152,", "rgba(70,130,180,", "rgba(244,164,96,", "rgba(210,105,30,", "rgba(220,20,60,"];
+	const particles = [];
+	let streamingConfetti = false;
+	let lastFrameTime = Date.now();
+	let waveAngle = 0;
+	let context = null;
+	let animationTimer;
 
 	function resetParticle(particle, width, height) {
 		particle.color = colors[(Math.random() * colors.length) | 0] + (confetti.alpha + ")");
