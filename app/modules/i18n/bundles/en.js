c1a730f43a9b651734d07d278fe9058cf779899b
Mayank Singhal <mayanks3005@gmail.com>
2021-04-14 17:09:14 +0530

    corrected the key

--- app/modules/i18n/bundles/en.js
+++ app/modules/i18n/bundles/en.js
@@ -628,7 +628,7 @@ export default {
     amount_label: 'Amount',
     breakup_title: 'Fees Breakup',
     continue_action: 'Continue',
-    continue_action: 'Close',
+    close_action: 'Close',
     gateway_charges_label: 'Convenience Charges',
     gst_label: 'GST on {label}',
     loading_message: 'Loading fees breakup...',
