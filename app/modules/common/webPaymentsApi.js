35700fe741c37b17f31dd15126686452f14b5625
Nanda Kishor Jeripothula <jeripothula.kishor@razorpay.com>
2021-05-04 12:32:29 +0530

    feat : phonepe is disabled on mweb to avoid transaction failures. (#2581)

--- app/modules/common/webPaymentsApi.js
+++ app/modules/common/webPaymentsApi.js
@@ -10,7 +10,8 @@ import { getSession } from 'sessionmanager';
 
 export const appsThatSupportWebPayments = [
   { package_name: GOOGLE_PAY_PACKAGE_NAME, method: 'upi' },
-  { package_name: PHONE_PE_PACKAGE_NAME, method: 'upi' },
+  // disabling phonepe due to failures on mweb
+  // { package_name: PHONE_PE_PACKAGE_NAME, method: 'upi' },
   { package_name: CRED_PACKAGE_NAME, method: 'app' },
 ];
 
