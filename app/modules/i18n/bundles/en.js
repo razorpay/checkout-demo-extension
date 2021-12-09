e81e055d7195a88402128c30cfbc63ee57d7ea0a
Parth Anand <parth.anand@razorpay.com>
2021-12-09 17:03:39 +0530

    updating bank name

--- app/modules/i18n/bundles/en.js
+++ app/modules/i18n/bundles/en.js
@@ -1026,7 +1026,7 @@ export default {
       icic: 'ICICI Bank PayLater',
       hdfc: 'FlexiPay by HDFC Bank',
       lazypay: 'LazyPay',
-      kkbk: 'kkbk',
+      kkbk: 'Kotak Mahindra Bank',
     },
     select_option_title: 'Select an option',
   },
