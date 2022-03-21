fb3a4b2b66a11ca291f660a2b56bf13052256607
Suhas More <suhas.more@razorpay.com>
2022-03-21 18:24:31 +0530

    Fix: Updated descriptive comments

--- app/modules/ui/tabs/card/index.svelte
+++ app/modules/ui/tabs/card/index.svelte
@@ -728,8 +728,9 @@
      * recurring callout needs to be displayed when 3 conditions are met
      * a -> user must be domestic user
      * b -> must be a recurring/subscription payment
-     * c -> user focused in card input.
-     * d -> card bin number is NOT supported for recurring payments (card's constants)
+     * c -> SI-HUB should not be enabled
+     * d -> user focused in card input.
+     * e -> card bin number is NOT supported for recurring payments (card's constants)
      *
      * note: b -> toggles on when user clicks on card input field, for subsequent interactions it remains toggled on.
      * note: c -> as user enters the card number, we switch it to false (#onCardInput),
