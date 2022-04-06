57f3b4a23460eb6eece2b16c687078b3dd9de28a
tarun khanna <tarun.khanna@razorpay.com>
2022-04-06 11:58:17 +0530

    feat: adding friction on closing 1cc modal

--- app/modules/one_click_checkout/header/sessionInterface.js
+++ app/modules/one_click_checkout/header/sessionInterface.js
@@ -1,7 +1,21 @@
 import { getSession } from 'sessionmanager';
 
+// i18n imports
+import {
+  CONFIRM_CANCEL_HEADING,
+  CONFIRM_CANCEL_MESSAGE,
+} from 'one_click_checkout/misc/i18n/label';
+import { formatTemplateWithLocale, getCurrentLocale } from 'i18n';
+
 export const handleModalClose = () => {
   const session = getSession();
+  const locale = getCurrentLocale();
 
-  session.closeModal();
+  Confirm.show({
+    heading: formatTemplateWithLocale(CONFIRM_CANCEL_HEADING, {}, locale),
+    message: formatTemplateWithLocale(CONFIRM_CANCEL_MESSAGE, {}, locale),
+    onPositiveClick: function () {
+      session.closeModal();
+    },
+  });
 };
---
+++ app/modules/one_click_checkout/misc/i18n/en.js
@@ -0,0 +1,5 @@
+export default {
+  confirm_cancel_heading: 'Cancel Order?',
+  confirm_cancel_message:
+    'Your payment is ongoing. Are you sure you want to cancel the order?',
+};
