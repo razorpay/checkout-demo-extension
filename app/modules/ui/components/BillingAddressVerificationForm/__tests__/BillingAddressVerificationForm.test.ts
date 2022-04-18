2866fe5500ee76629cd7e993a285c422ffddbda5
Ravi Dhiman <ravi.dhiman@razorpay.com>
2022-04-18 10:34:12 +0530

    Refactor billing address verification component and increase test coverage (#3400)
    * chore: billing address verification form common component created
    
    * chore: get countries and state api integrated
    
    * chore: countries & states api integrated
    
    * chore: Test cases fixed
    
    * chore: Refactored with typescript
    
    * Components refactored with Typescript
    
    * chore: comments removed
    
    * chore: form validations fixed
    
    * chore: Integration issues fixed
    
    * chore: fire input event when country change
    
    * PR comment resolved
    
    * chore: test cases fixed
    
    * PR comment resolved

---
+++ app/modules/ui/components/BillingAddressVerificationForm/__tests__/BillingAddressVerificationForm.test.ts
@@ -0,0 +1,109 @@
+import { render } from '@testing-library/svelte';
+
+// component
+import BillingAddressVerificationForm from '../BillingAddressVerificationForm.svelte';
+
+// constants
+import { FORM_TYPE } from '../types';
+import { FORM_FIELDS_TYPE_MAPPING } from '../constants';
+
+const razorpayInstance = {
+  id: 'id',
+  key: 'rzp_test_key',
+  get: (arg: unknown) => arg,
+  getMode: () => 'test',
+};
+
+jest.mock('sessionmanager', () => {
+  return {
+    getSession: () => ({
+      get: jest.fn(),
+      r: razorpayInstance,
+    }),
+  };
+});
+
+describe('Test BillingAddressVerificationForm', () => {
+  it('should render without breaking', () => {
+    const { component } = render(BillingAddressVerificationForm);
+    expect(component).toBeTruthy();
+  });
+  it('should render input fields for AVS', () => {
+    const { container } = render(BillingAddressVerificationForm);
+    expect(container.querySelectorAll('input[type="text"]')).toHaveLength(6);
+    FORM_FIELDS_TYPE_MAPPING[FORM_TYPE.AVS].forEach((field) => {
+      if (Array.isArray(field)) {
+        field.forEach((subField) => {
+          expect(
+            container.querySelector(
+              `input[name="billing-address-verification-${subField}"]`
+            )
+          ).toBeTruthy();
+        });
+      } else {
+        expect(
+          container.querySelector(
+            `input[name="billing-address-verification-${field}"]`
+          )
+        ).toBeTruthy();
+      }
+    });
+  });
+  it('should render input fields for N_AVS', () => {
+    const { container } = render(BillingAddressVerificationForm, {
+      props: {
+        formType: FORM_TYPE.N_AVS,
+      },
+    });
+    expect(container.querySelectorAll('input[type="text"]')).toHaveLength(8);
+    FORM_FIELDS_TYPE_MAPPING[FORM_TYPE.N_AVS].forEach((field) => {
+      if (Array.isArray(field)) {
+        field.forEach((subField) => {
+          expect(
+            container.querySelector(
+              `input[name="billing-address-verification-${subField}"]`
+            )
+          ).toBeTruthy();
+        });
+      } else {
+        expect(
+          container.querySelector(
+            `input[name="billing-address-verification-${field}"]`
+          )
+        ).toBeTruthy();
+      }
+    });
+  });
+  it('should have default billing address', () => {
+    const defaultValue: { [x in string]: string } = {
+      line1: '123 Main St',
+      line2: 'Apt 1',
+      city: 'San Francisco',
+      postal_code: '94107',
+      country: 'US',
+      state: 'CA',
+    };
+
+    const { container } = render(BillingAddressVerificationForm, {
+      props: {
+        value: defaultValue,
+      },
+    });
+
+    FORM_FIELDS_TYPE_MAPPING[FORM_TYPE.AVS].forEach((field) => {
+      if (Array.isArray(field)) {
+        field.forEach((subField) => {
+          const el = container.querySelector(
+            `input[name="billing-address-verification-${subField}"]`
+          ) as HTMLInputElement;
+          expect(el?.value).toEqual(defaultValue[subField]);
+        });
+      } else {
+        const el = container.querySelector(
+          `input[name="billing-address-verification-${field}"]`
+        ) as HTMLInputElement;
+        expect(el?.value).toEqual(defaultValue[field]);
+      }
+    });
+  });
+});
