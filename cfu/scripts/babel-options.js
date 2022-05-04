8ddc454899374f65cb46ba583b41e866fe5c150a
Arshpreet <75480493+rparsh@users.noreply.github.com>
2022-05-04 20:33:08 +0530

    Revert "remove unused babel plugins (#3686)" (#3721)
    This reverts commit a162eb71112dd7d0678a7b456936ef8efab60891.

--- cfu/scripts/babel-options.js
+++ cfu/scripts/babel-options.js
@@ -1,10 +1,15 @@
-module.exports = {
-  plugins: [
-    [
-      require('@babel/plugin-proposal-pipeline-operator'),
-      { proposal: 'minimal' },
-    ],
+const plugins = [
+  [require('fast-async'), { useRuntimeModule: false }],
+  [
+    require('@babel/plugin-proposal-pipeline-operator'),
+    { proposal: 'minimal' },
   ],
+  [require('@babel/plugin-proposal-optional-chaining'), { loose: true }],
+  [require('@babel/plugin-transform-spread'), { loose: false }],
+];
+
+module.exports = {
+  plugins,
   presets: [
     [
       require('@babel/preset-env'),
