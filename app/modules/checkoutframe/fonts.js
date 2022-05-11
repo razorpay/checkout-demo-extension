87b1c12c4e24d00492e9402f09eb7297ba1bcbec
tarun khanna <tarun.khanna@razorpay.com>
2022-05-11 12:27:09 +0530

    bugfix: missing font-weights added

--- app/modules/checkoutframe/fonts.js
+++ app/modules/checkoutframe/fonts.js
@@ -2,7 +2,7 @@ export function loadInterFont() {
   const head = document.head || document.getElementsByTagName('head')[0];
   const link = document.createElement('link');
   link.href =
-    'https://fonts.googleapis.com/css2?family=Inter:wght@300;500;600;700&display=swap';
+    'https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800&display=swap';
   link.rel = 'stylesheet';
 
   head.appendChild(link);
