36ff1dbab05e7494a49c3dc0969cf58bb9d5556a
Revanth M <revanth.m@razorpay.com>
2022-05-23 14:51:03 +0530

    feat(rtb): Ramp up RTB bottom sheet experiment to 100% (#3816)
    Closes CE-5523

--- app/modules/rtb/experiments.ts
+++ app/modules/rtb/experiments.ts
@@ -2,12 +2,12 @@ import { createExperiment } from 'experiments';
 
 /**
  * Experiment: Show RTB Bottom Sheet On-Click of RTB badge
- * Enabled for 50% of users. To be removed post monitoring.
+ * Enabled for 100% of users. To be removed post monitoring.
  *
  * ToDo - Make the API more user-friendly as the current way of passing the (1-exp_value) is misleading
  */
 const showRTBBottomSheet = createExperiment(
-  'show_rtb_bottom_sheet_rampup',
-  0.5
+  'show_rtb_bottom_sheet_rampup_full',
+  0
 );
 export { showRTBBottomSheet };
