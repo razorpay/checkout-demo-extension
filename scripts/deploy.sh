ddf82169b11cd29885358fc0d3dd8627b18024d2
Pranav Gupta <pronav@users.noreply.github.com>
2022-07-03 23:02:30 +0530

    Add image for POS (checkout.razorpay.com) server (#3996)
    * add nginx images to proxy from cdn
    
    * update public page
    
    * switch to alpine image

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,12 +9,20 @@ DIST_canary="E2RK6LAT9F78JF"
 DIST_baseline="E2C4TJ1SGQAY82"
 DIST_production="E10EFYGUV7S9YQ"
 
+COMMIT=$1
+CANARY=$2
+BASELINE=$3
+PRODUCTION=$4
+
 deploy_to_env() {
   ENV=$1
 
   # make a copy of build folder
   rm -rf envbuild
   cp -r build envbuild
+
+  # put commit_id in build folder
+  echo $COMMIT > envbuild/commit.txt
 
   # put env in copied file
   find envbuild -name '*.js' -exec sed -i -- "s#__S_TRAFFIC_ENV__#$ENV#g" {} \;
@@ -24,7 +32,7 @@ deploy_to_env() {
   BUCKET=${!BUCKET}
 
   # copy result to env specific bucket
-  aws --output text s3 sync envbuild s3://$BUCKET/v1 --cache-control "max-age=120, s-maxage=300"
+  aws --output text s3 sync envbuild s3://$BUCKET/v1 --cache-control "max-age=120,s-maxage=600,stale-while-revalidate=120"
 
   # invalidate
   DIST="DIST_$ENV"
@@ -32,11 +40,6 @@ deploy_to_env() {
 
   aws --output text cloudfront create-invalidation --distribution-id $DIST --paths "/v1/*"
 }
-
-COMMIT=$1
-CANARY=$2
-BASELINE=$3
-PRODUCTION=$4
 
 # copy build folder to disk
 aws --output text s3 sync s3://$BUCKET_production/build/$COMMIT build
--- scripts/server/page.lua
+++ scripts/server/page.lua
@@ -8,21 +8,17 @@ response = [[
 <!DOCTYPE html>
 <html dir="ltr">
 <head>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta charset="utf-8">
   <title>Razorpay Checkout</title>
   <link rel="icon" href="data:;base64,=">
   <meta name="viewport" content="user-scalable=no,width=device-width,initial-scale=1,maximum-scale=1">
-  <meta http-equiv="cache-control" content="no-cache">
   <link rel="stylesheet" href="]] .. base .. [[/css/checkout.css">
 </head>
 <body><div style="font-family:'lato';visibility:hidden;position:absolute;">.</div></body>
 <style>
 @font-face {
   font-family:'lato';font-weight:normal;font-style:normal;
-  src: url("https://cdn.razorpay.com/lato.eot?#iefix") format('embedded-opentype'), url("https://cdn.razorpay.com/lato.woff2") format('woff2'), url("https://cdn.razorpay.com/lato.woff") format('woff'), url("https://cdn.razorpay.com/lato.ttf") format('truetype'), url("https://cdn.razorpay.com/lato.svg#lato") format('svg');
-  font-weight: normal;
-  font-style: normal
+  src: url("https://cdn.razorpay.com/lato.woff2") format('woff2'), url("https://cdn.razorpay.com/lato.woff") format('woff');
 }
 </style>
 <script>
