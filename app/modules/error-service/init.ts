6daee39f736a9adf3ca89309862893c69b9feee8
Pranav Gupta <pronav@users.noreply.github.com>
2022-07-26 15:14:49 +0530

    fix error reporting in checkout-frame (#4128)

--- app/modules/error-service/init.ts
+++ app/modules/error-service/init.ts
@@ -1,8 +1,9 @@
 import { startErrorCapturing } from './error-service';
 (() => {
   if (
-    '__S_TRAFFIC_ENV__'.indexOf('TRAFFIC_ENV') === -1 &&
-    '__S_TRAFFIC_ENV__'
+    ('__S_TRAFFIC_ENV__'.indexOf('TRAFFIC_ENV') === -1 &&
+      '__S_TRAFFIC_ENV__') ||
+    location.search.indexOf('traffic_env=') > 0
   ) {
     startErrorCapturing();
   }
--- scripts/sdk-loader.js
+++ scripts/sdk-loader.js
@@ -1,8 +1,11 @@
 (function () {
-  let prefix = 'https://checkout-static.razorpay.com/build/COMMIT';
+  var prefix = 'https://checkout-static.razorpay.com/build/COMMIT';
+  var env = 'traffic_env=__S_TRAFFIC_ENV__';
+  var s = location.search;
+  history.pushState(null, null, s ? s + '&' + env : '?' + env);
 
   function addJs(retry) {
-    let script = document.createElement('script');
+    var script = document.createElement('script');
     script.src = prefix + '/checkout-frame.js' + (retry ? '?retry' : '');
     if (!retry) {
       script.crossOrigin = 'anonymous';
@@ -14,7 +17,7 @@
   }
 
   function addCss(retry) {
-    let link = document.createElement('link');
+    var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = prefix + '/css/checkout.css' + (retry ? '?retry' : '');
     if (!retry) {
