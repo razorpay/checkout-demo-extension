08fc4204d78ba0fbbec0bfda1d4becae00bd75d2
Kunal Vishnoi <kunal.vishnoi@razorpay.com>
2022-11-30 17:24:15 +0530

    fix(sms): template fix (#4835)

--- app/modules/checkoutframe/sms_template.js
+++ app/modules/checkoutframe/sms_template.js
@@ -2,13 +2,12 @@ import { getSession } from 'sessionmanager';
 import { views } from 'one_click_checkout/routing/constants';
 import { screensHistory } from 'one_click_checkout/routing/History';
 import { OTP_TEMPLATES } from 'otp/constants';
-import { isRedesignV15 } from 'razorpay/helper';
 
 export function getDefaultOtpTemplate() {
   let template = screensHistory.config[views.OTP].props.smsTemplate;
 
   const session = getSession();
-  if (session.tab !== 'home-1cc' && isRedesignV15()) {
+  if (session.tab !== 'home-1cc') {
     if (session.payload && session.payload.save) {
       template = OTP_TEMPLATES.save_card;
     } else {
