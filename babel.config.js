6f95286c49b3a6c927d3958698f38eee17580d2c
Ravi Sharma <ravi.sharma@razorpay.com>
2022-12-07 13:15:07 +0530

    fix: fetch polling callback (#4939)
    * fix: fetch polling callback
    
    * Revert "fix: fetch polling callback"
    
    This reverts commit b8a469b8936caf825f874f2455864dcf1fa640af.
    
    * fix: run babel-ts transform before preset-env
    
    * fix: dev mode ts transpilation

--- babel.config.js
+++ babel.config.js
@@ -2,19 +2,19 @@ module.exports = (api) => {
   api.cache(true);
 
   return {
-    presets: [['@babel/preset-typescript']],
     plugins: [['@babel/plugin-transform-runtime']],
     env: {
       /**
-       * Uncomment below preset if you would like to transpile
+       * Add this preset if you would like to transpile
        * during development - for eg ie11 or older browser testing
        * note: this will increase compilation time
+       * ['@babel/preset-env', { debug: true }]
        */
-      // development: {
-      //   presets: [['@babel/preset-env', { debug: true }]],
-      // },
+      development: {
+        presets: [['@babel/preset-typescript']],
+      },
       production: {
-        presets: [['@babel/preset-env']],
+        presets: [['@babel/preset-env'], ['@babel/preset-typescript']],
       },
       test: {
         plugins: ['svelte-inline-compile'],
