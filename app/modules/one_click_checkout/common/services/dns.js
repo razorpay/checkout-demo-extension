83e6c9b358374a22933e37810522b0c073afb555
Ravi Sharma <ravi.sharma@razorpay.com>
2022-12-15 09:54:35 +0530

    fix: allow mx record api failure (#5040)

--- app/modules/one_click_checkout/common/services/dns.js
+++ app/modules/one_click_checkout/common/services/dns.js
@@ -1,15 +1,18 @@
 import fetch from 'utils/fetch';
+import { isNumber } from 'utils/_';
 
 export function resolveMxRecords(domain) {
   return new Promise((resolve) => {
+    // https://developers.google.com/speed/public-dns/docs/doh/json
     fetch({
       url: `https://dns.google/resolve?name=${domain}&type=MX`,
       callback: (response) => {
-        if (response.Status === 0) {
+        if (isNumber(response.Status)) {
+          resolve(response.Status === 0);
+        } else {
+          // allow emails to pass through if the there's a network error
           resolve(true);
-          return;
         }
-        resolve(false);
       },
     });
   });
